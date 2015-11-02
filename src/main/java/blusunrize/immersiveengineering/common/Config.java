76f1a4f354a6c1bb68743b18b08b66ca0620ae10
BluSunrize <danny.rad10@yahoo.de>
2015-11-02 19:31:32 +0100

    Added a recipe for the HC hemp trolly, closes #558 Changed the default bullet damage

--- src/main/java/blusunrize/immersiveengineering/common/Config.java
+++ src/main/java/blusunrize/immersiveengineering/common/Config.java
@@ -35,9 +35,22 @@ public class Config
 			IELogger.info("The Previous Version of IE was outdated!");
 			if(lastVersion<.6)
 			{
-				IELogger.info("The Config on transfer rates of IE wires will be reset to the default.");
+				IELogger.info("The config on transfer rates of IE wires will be reset to the default.");
 				config.getCategory("general").remove("Cable transfer rates");
 				config.getCategory("general").remove("Cable loss");
+			}
+			if(lastVersion<.61)
+			{
+				IELogger.info("The config on bullet damage will be reset to the default.");
+				config.getCategory("tools").remove("BulletDamage-Casull");
+				config.getCategory("tools").remove("BulletDamage-AP");
+				config.getCategory("tools").remove("BulletDamage-Buck");
+				config.getCategory("tools").remove("BulletDamage-Dragon");
+				config.getCategory("tools").remove("BulletDamage-Homing");
+				config.getCategory("tools").remove("BulletDamage-Wolfpack");
+				config.getCategory("tools").remove("BulletDamage-WolfpackPart");
+				config.getCategory("tools").remove("BulletDamage-Silver");
+				config.getCategory("tools").remove("BulletDamage-Potion");
 			}
 			config.getCategory("general").remove("Show Update News");
 		}
@@ -143,18 +156,19 @@ public class Config
 		setIntArray("oreDimBlacklist", config.get("OreGen", "DimensionBlacklist", new int[]{-1,1}, "A blacklist of dimensions in which IE ores won't spawn. By default this is Nether (-1) and End (1)").getIntList());
 
 		setBoolean("hardmodeBulletRecipes", config.get("Tools", "Bullets: Hardmode Recipes", false, "Enable this to use the old, harder bullet recipes(require one ingot per bullet)").getBoolean());
-		setDouble("BulletDamage-Casull", config.get("Tools", "BulletDamage-Casull", 7d, "The amount of base damage a Casull Cartridge inflicts").getDouble());
-		setDouble("BulletDamage-AP", config.get("Tools", "BulletDamage-AP", 7d, "The amount of base damage an ArmorPiercing Cartridge inflicts").getDouble());
-		setDouble("BulletDamage-Buck", config.get("Tools", "BulletDamage-Buck", 1d, "The amount of base damage a single part of Buckshot inflicts").getDouble());
-		setDouble("BulletDamage-Dragon", config.get("Tools", "BulletDamage-Dragon", 4d, "The amount of base damage a DragonsBreath Cartridge inflicts").getDouble());
-		setDouble("BulletDamage-Homing", config.get("Tools", "BulletDamage-Homing", 8d, "The amount of base damage a Homing Cartridge inflicts").getDouble());
+		setDouble("BulletDamage-Casull", config.get("Tools", "BulletDamage-Casull", 10d, "The amount of base damage a Casull Cartridge inflicts").getDouble());
+		setDouble("BulletDamage-AP", config.get("Tools", "BulletDamage-AP", 10d, "The amount of base damage an ArmorPiercing Cartridge inflicts").getDouble());
+		setDouble("BulletDamage-Buck", config.get("Tools", "BulletDamage-Buck", 2d, "The amount of base damage a single part of Buckshot inflicts").getDouble());
+		setDouble("BulletDamage-Dragon", config.get("Tools", "BulletDamage-Dragon", 3d, "The amount of base damage a DragonsBreath Cartridge inflicts").getDouble());
+		setDouble("BulletDamage-Homing", config.get("Tools", "BulletDamage-Homing", 10d, "The amount of base damage a Homing Cartridge inflicts").getDouble());
 		setDouble("BulletDamage-Wolfpack", config.get("Tools", "BulletDamage-Wolfpack", 6d, "The amount of base damage a Wolfpack Cartridge inflicts").getDouble());
 		setDouble("BulletDamage-WolfpackPart", config.get("Tools", "BulletDamage-WolfpackPart", 4d, "The amount of damage the sub-projectiles of the Wolfpack Cartridge inflict").getDouble());
-		setDouble("BulletDamage-Silver", config.get("Tools", "BulletDamage-Silver", 7d, "The amount of damage a silver bullet inflicts").getDouble());
+		setDouble("BulletDamage-Silver", config.get("Tools", "BulletDamage-Silver", 10d, "The amount of damage a silver bullet inflicts").getDouble());
+		setDouble("BulletDamage-Potion", config.get("Tools", "BulletDamage-Potion", 1d, "The amount of base damage a Phial Cartridge inflicts").getDouble());
 
 		for(String key : IECompatModule.moduleClasses.keySet())
 			setBoolean("compat_"+key, config.get("Compatability", "Enable Compatmodule: "+key, true, "Set this to false to disable IE's built in compatability with "+key).getBoolean());
-		
+
 		config.save();
 	}
 
--- src/main/java/blusunrize/immersiveengineering/common/util/compat/hydcraft/HydCraftHelper.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/hydcraft/HydCraftHelper.java
@@ -1,7 +1,14 @@
 package blusunrize.immersiveengineering.common.util.compat.hydcraft;
 
 import k4unl.minecraft.Hydraulicraft.api.HCApi;
+import k4unl.minecraft.Hydraulicraft.api.recipes.FluidShapedOreRecipe;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import blusunrize.immersiveengineering.common.util.compat.IECompatModule;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class HydCraftHelper extends IECompatModule
 {
@@ -9,7 +16,7 @@ public class HydCraftHelper extends IECompatModule
 	public void preInit()
 	{
 	}
-	
+
 	@Override
 	public void postInit()
 	{
@@ -18,6 +25,15 @@ public class HydCraftHelper extends IECompatModule
 	@Override
 	public void init()
 	{
-		HCApi.getInstance().getTrolleyRegistrar().registerTrolley(new IETrolley());	
+		HCApi.getInstance().getTrolleyRegistrar().registerTrolley(new IETrolley());
+		
+		ItemStack ieTrolly = HCApi.getInstance().getTrolleyRegistrar().getTrolleyItem("ieCrop");
+		ieTrolly.stackSize = 4;
+		Block pressureCore = GameRegistry.findBlock("HydCraft","LPBlockCore");
+	    Block pressureWall = GameRegistry.findBlock("HydCraft","hydraulicPressureWall");
+	    Block hydraulicPiston = GameRegistry.findBlock("HydCraft","hydraulicPiston");
+	    Fluid fLube = FluidRegistry.getFluid("lubricant");
+		if(pressureCore!=null && pressureWall!=null && hydraulicPiston!=null && fLube!=null)
+			HCApi.getInstance().getRecipeHandler().addAssemblerRecipe( new FluidShapedOreRecipe(ieTrolly, new Object[]{true, new Object[]{" P ","WCW","SIS", 'C',new ItemStack(pressureCore,1,1), 'W',pressureWall, 'S',"treatedStick", 'I',"ingotIron", 'P',hydraulicPiston}}).addFluidInput(new FluidStack(fLube, 40)));
 	}
 }
