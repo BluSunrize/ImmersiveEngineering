f17c8f3545b28b66c04beb703ce9e25ec75ce524
malte0811 <malte0811@web.de>
2016-07-01 21:51:36 +0200

    fixed IEObj model caching: 1. a crash in case of a hash collision of blockstates with different properties 2. the cache no longer grows to infinity

--- src/main/java/blusunrize/immersiveengineering/common/blocks/BlockIETileProvider.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/BlockIETileProvider.java
@@ -73,7 +73,7 @@ public abstract class BlockIETileProvider<E extends Enum<E> & BlockIEBase.IBlock
 	public void breakBlock(World world, BlockPos pos, IBlockState state)
 	{
 		TileEntity tile = world.getTileEntity(pos);
-		if (!(tile instanceof ITileDrop)&&tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
+		if (!(tile instanceof ITileDrop)&&tile!=null&&tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
 		{
 			IItemHandler h = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
 			if (h instanceof IEInventoryHandler)
