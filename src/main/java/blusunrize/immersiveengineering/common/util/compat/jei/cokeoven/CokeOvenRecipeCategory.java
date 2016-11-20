e78fb19f744a29eb516b59209960e7a729c3b09f
BluSunrize <danny.rad10@yahoo.de>
2016-11-20 11:10:35 +0100

    Added Icons for all IE machines to JEI

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/cokeoven/CokeOvenRecipeCategory.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/cokeoven/CokeOvenRecipeCategory.java
@@ -1,5 +1,7 @@
 package blusunrize.immersiveengineering.common.util.compat.jei.cokeoven;
 
+import blusunrize.immersiveengineering.common.IEContent;
+import blusunrize.immersiveengineering.common.blocks.stone.BlockTypes_StoneDevices;
 import blusunrize.immersiveengineering.common.util.compat.jei.IERecipeCategory;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
@@ -8,7 +10,9 @@ import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.util.Log;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
 
 public class CokeOvenRecipeCategory extends IERecipeCategory
 {
@@ -17,7 +21,7 @@ public class CokeOvenRecipeCategory extends IERecipeCategory
 	
 	public CokeOvenRecipeCategory(IGuiHelper helper)
 	{
-		super("cokeoven","tile.immersiveengineering.stoneDevice.coke_oven.name", helper.createDrawable(background, 8,13, 142, 60), CokeOvenRecipeWrapper.class);
+		super("cokeoven","tile.immersiveengineering.stoneDevice.coke_oven.name", helper.createDrawable(background, 8,13, 142, 60), CokeOvenRecipeWrapper.class, new ItemStack(IEContent.blockStoneDevice,1,BlockTypes_StoneDevices.COKE_OVEN.getMeta()));
 		tankOverlay = helper.createDrawable(background, 176,31, 16,47, -2,2,-2,2);
 	}
 	
@@ -36,11 +40,11 @@ public class CokeOvenRecipeCategory extends IERecipeCategory
 		if(recipeWrapper instanceof CokeOvenRecipeWrapper)
 		{
 			CokeOvenRecipeWrapper recipe = (CokeOvenRecipeWrapper) recipeWrapper;
-			guiItemStacks.setFromRecipe(0, recipe.getInputs());
-			guiItemStacks.set(1, recipe.getOutputs());
+			guiItemStacks.set(0, ingredients.getInputs(ItemStack.class).get(0));
+			guiItemStacks.set(1, ingredients.getOutputs(ItemStack.class));
 			
 			recipeLayout.getFluidStacks().init(0, false, 121,7, 16,47, 12000, false, tankOverlay);
-			recipeLayout.getFluidStacks().set(0, recipe.getFluidOutputs());
+			recipeLayout.getFluidStacks().set(0, ingredients.getOutputs(FluidStack.class));
 		}
 		else
 			Log.error("Unknown recipe wrapper type: {}", recipeWrapper);
--- src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/fermenter/FermenterRecipeCategory.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/fermenter/FermenterRecipeCategory.java
@@ -1,5 +1,7 @@
 package blusunrize.immersiveengineering.common.util.compat.jei.fermenter;
 
+import blusunrize.immersiveengineering.common.IEContent;
+import blusunrize.immersiveengineering.common.blocks.metal.BlockTypes_MetalMultiblock;
 import blusunrize.immersiveengineering.common.util.compat.jei.IERecipeCategory;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
@@ -8,19 +10,21 @@ import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.util.Log;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
 
 public class FermenterRecipeCategory extends IERecipeCategory
 {
 	public static ResourceLocation background = new ResourceLocation("immersiveengineering:textures/gui/fermenter.png");
 	private final IDrawable tankOverlay;
-	
+
 	public FermenterRecipeCategory(IGuiHelper helper)
 	{
-		super("fermenter","tile.immersiveengineering.metalMultiblock.fermenter.name", helper.createDrawable(background, 6,12, 164,59), FermenterRecipeWrapper.class);
+		super("fermenter","tile.immersiveengineering.metalMultiblock.fermenter.name", helper.createDrawable(background, 6,12, 164,59), FermenterRecipeWrapper.class, new ItemStack(IEContent.blockMetalMultiblock,1,BlockTypes_MetalMultiblock.FERMENTER.getMeta()));
 		tankOverlay = helper.createDrawable(background, 177,31, 16,47, -2,2,-2,2);
 	}
-	
+
 	@Override
 	@Deprecated
 	public void setRecipe(IRecipeLayout recipeLayout, IRecipeWrapper recipeWrapper)
@@ -35,12 +39,11 @@ public class FermenterRecipeCategory extends IERecipeCategory
 		guiItemStacks.init(1, false, 84, 40);
 		if(recipeWrapper instanceof FermenterRecipeWrapper)
 		{
-			FermenterRecipeWrapper recipe = (FermenterRecipeWrapper) recipeWrapper;
-			guiItemStacks.set(0, recipe.recipeInputs[0]);
-			guiItemStacks.set(1, recipe.getOutputs());
-			
+			guiItemStacks.set(0, ingredients.getInputs(ItemStack.class).get(0));
+			guiItemStacks.set(1, ingredients.getOutputs(ItemStack.class));
+
 			recipeLayout.getFluidStacks().init(0, false, 106,9, 16,47, 24000, false, tankOverlay);
-			recipeLayout.getFluidStacks().set(0, recipe.getFluidOutputs());
+			recipeLayout.getFluidStacks().set(0, ingredients.getOutputs(FluidStack.class));
 		}
 		else
 			Log.error("Unknown recipe wrapper type: {}", recipeWrapper);
