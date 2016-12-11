0641eecbfdf2307941b3f48d3f6e11affb90a36d
BluSunrize <danny.rad10@yahoo.de>
2016-12-11 17:17:48 +0100

    Step 1 of adding the Automated Workbench It basically works, has some of the fanciest animation I've ever done and all's well. Just needs some fine tuning ^_^

---
+++ src/main/java/blusunrize/immersiveengineering/client/gui/GuiAutoWorkbench.java
@@ -0,0 +1,176 @@
+package blusunrize.immersiveengineering.client.gui;
+
+import blusunrize.immersiveengineering.ImmersiveEngineering;
+import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
+import blusunrize.immersiveengineering.client.ClientUtils;
+import blusunrize.immersiveengineering.client.gui.elements.GuiButtonItem;
+import blusunrize.immersiveengineering.common.blocks.metal.TileEntityAutoWorkbench;
+import blusunrize.immersiveengineering.common.gui.ContainerAutoWorkbench;
+import blusunrize.immersiveengineering.common.items.ItemEngineersBlueprint;
+import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
+import blusunrize.immersiveengineering.common.util.network.MessageTileSync;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
+import net.minecraft.nbt.NBTTagCompound;
+import org.lwjgl.opengl.GL11;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GuiAutoWorkbench extends GuiContainer
+{
+	TileEntityAutoWorkbench tile;
+	public GuiAutoWorkbench(InventoryPlayer inventoryPlayer, TileEntityAutoWorkbench tile )
+	{
+		super(new ContainerAutoWorkbench(inventoryPlayer, tile));
+		this.tile = tile;
+		this.ySize=184;
+	}
+
+	@Override
+	public void initGui()
+	{
+		this.buttonList.clear();
+		super.initGui();
+		Slot s = inventorySlots.getSlot(0);
+		if(s!=null && s.getHasStack() && s.getStack().getItem() instanceof ItemEngineersBlueprint)
+		{
+			BlueprintCraftingRecipe[] recipes = BlueprintCraftingRecipe.findRecipes(ItemNBTHelper.getString(s.getStack(),"blueprint"));
+			if(recipes!=null && recipes.length>0)
+			{
+				int l = recipes.length;
+				int xx = guiLeft+121;
+				int yy = guiTop+(l>6?59-(l-3)/3*18: l>3?59: 68);
+				for(int i=0; i<l; i++)
+					if(recipes[i]!=null && recipes[i].output!=null)
+					{
+						this.buttonList.add(new GuiButtonItem(i, xx+(i%3)*18,yy+(i/3)*18, recipes[i].output.copy(), i==tile.selectedRecipe));
+					}
+			}
+//			ItemStack stack = s.getStack();
+//			IConfigurableTool tool = ((IConfigurableTool)stack.getItem());
+//			int buttonid = 0;
+//			ToolConfigBoolean[] boolArray = tool.getBooleanOptions(stack);
+//			if(boolArray!=null)
+//				for(ToolConfigBoolean b : boolArray)
+//					this.buttonList.add(new GuiButtonCheckbox(buttonid++, guiLeft+b.x,guiTop+b.y, tool.fomatConfigName(stack,b), b.value));
+//			ToolConfigFloat[] floatArray = tool.getFloatOptions(stack);
+//			if(floatArray!=null)
+//				for(ToolConfigFloat f : floatArray)
+//					this.buttonList.add(new GuiSliderIE(buttonid++, guiLeft+f.x,guiTop+f.y, 80, tool.fomatConfigName(stack,f), f.value));
+		}
+	}
+
+	@Override
+	protected void actionPerformed(GuiButton button) throws IOException
+	{
+		if(button instanceof GuiButtonItem)
+		{
+			if(button.id==tile.selectedRecipe)//disable
+				tile.selectedRecipe = -1;
+			else
+				tile.selectedRecipe = button.id;
+			NBTTagCompound message = new NBTTagCompound();
+			message.setInteger("recipe",tile.selectedRecipe);
+			ImmersiveEngineering.packetHandler.sendToServer(new MessageTileSync(this.tile, message));
+			initGui();
+		}
+	}
+
+	NBTTagCompound lastMessage;
+	@Override
+	protected void mouseReleased(int mouseX, int mouseY, int state)
+	{
+		super.mouseReleased(mouseX, mouseY, state);
+		Slot s = inventorySlots.getSlot(0);
+//		if(s!=null && s.getHasStack() && s.getStack().getItem() instanceof IConfigurableTool)
+//		{
+//			ItemStack stack = s.getStack();
+//			IConfigurableTool tool = ((IConfigurableTool)stack.getItem());
+//			NBTTagCompound message = new NBTTagCompound();
+//			ToolConfigBoolean[] boolArray = tool.getBooleanOptions(stack);
+//			int iBool = 0;
+//			ToolConfigFloat[] floatArray = tool.getFloatOptions(stack);
+//			int iFloat = 0;
+//			for(GuiButton button : this.buttonList)
+//			{
+//				if(button instanceof GuiButtonCheckbox && boolArray!=null)
+//					message.setBoolean("b_"+boolArray[iBool++].name,((GuiButtonCheckbox)button).state);
+//				if(button instanceof GuiSliderIE && floatArray!=null)
+//					message.setFloat("f_"+floatArray[iFloat++].name,(float)((GuiSliderIE)button).sliderValue);
+//			}
+//			if(lastMessage==null || !lastMessage.equals(message))//Only send packets when values have changed
+//				ImmersiveEngineering.packetHandler.sendToServer(new MessageTileSync(this.tile, message));
+//			lastMessage = message;
+//		}
+	}
+
+	@Override
+	public void drawScreen(int mx, int my, float partial)
+	{
+		super.drawScreen(mx, my, partial);
+
+		ArrayList<String> tooltip = new ArrayList<String>();
+		if(mx>guiLeft+80&&mx<guiLeft+87 && my>guiTop+36&&my<guiTop+82)
+			tooltip.add(tile.getEnergyStored(null)+"/"+tile.getMaxEnergyStored(null)+" RF");
+
+		if(!tooltip.isEmpty())
+		{
+			ClientUtils.drawHoveringText(tooltip, mx, my, fontRendererObj, guiLeft+xSize,-1);
+			RenderHelper.enableGUIStandardItemLighting();
+		}
+	}
+
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int mx, int my)
+	{
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		ClientUtils.bindTexture("immersiveengineering:textures/gui/autoWorkbench.png");
+		this.drawTexturedModalRect(guiLeft,guiTop, 0, 0, xSize, ySize);
+
+		int stored = (int)(46*(tile.getEnergyStored(null)/(float)tile.getMaxEnergyStored(null)));
+		ClientUtils.drawGradientRect(guiLeft+80,guiTop+36+(46-stored), guiLeft+87,guiTop+82, 0xffb51500, 0xff600b00);
+
+//		for(int i=0; i<((ContainerAutoWorkbench)inventorySlots).slotCount; i++)
+//		{
+//			Slot s = inventorySlots.getSlot(i);
+//
+//			ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition-1, guiTop+ s.yDisplayPosition-1, 17,1, 0x77222222);
+//			ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition-1, guiTop+ s.yDisplayPosition+0, 1,16, 0x77222222);
+//			ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition+16, guiTop+ s.yDisplayPosition+0, 1,17, 0x77999999);
+//			ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition+0, guiTop+ s.yDisplayPosition+16, 16,1, 0x77999999);
+//			if( !(s instanceof IESlot.BlueprintOutput) || s.getHasStack() || ((IESlot.BlueprintOutput)s).recipe.output==null)
+//				ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition+0, guiTop+ s.yDisplayPosition+0, 16,16, 0x77444444);
+//		}
+//
+//		for(int i=0; i<((ContainerModWorkbench)inventorySlots).slotCount; i++)
+//		{
+//			Slot s = inventorySlots.getSlot(i);
+//			if(s instanceof IESlot.BlueprintOutput && !s.getHasStack())
+//			{
+//				ItemStack ghostStack = ((IESlot.BlueprintOutput)s).recipe.output;
+//				if(ghostStack!=null)
+//				{
+//					this.zLevel = 200.0F;
+//					itemRender.zLevel = 200.0F;
+//					FontRenderer font = ghostStack.getItem().getFontRenderer(ghostStack);
+//					if(font==null)
+//						font = fontRendererObj;
+//					itemRender.renderItemAndEffectIntoGUI(ghostStack, guiLeft+s.xDisplayPosition, guiTop+s.yDisplayPosition);
+//					this.zLevel = 0.0F;
+//					itemRender.zLevel = 0.0F;
+//
+//					GlStateManager.disableLighting();
+//					GlStateManager.disableDepth();
+//					ClientUtils.drawColouredRect(guiLeft+ s.xDisplayPosition+0, guiTop+ s.yDisplayPosition+0, 16,16, 0x77444444);
+//					GlStateManager.enableLighting();
+//					GlStateManager.enableDepth();
+//				}
+//			}
+//		}
+	}
+}
---
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityAutoWorkbench.java
@@ -0,0 +1,339 @@
+package blusunrize.immersiveengineering.common.blocks.metal;
+
+import blusunrize.immersiveengineering.api.Lib;
+import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
+import blusunrize.immersiveengineering.api.crafting.IMultiblockRecipe;
+import blusunrize.immersiveengineering.api.tool.ConveyorHandler.IConveyorAttachable;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IGuiTile;
+import blusunrize.immersiveengineering.common.blocks.multiblocks.MultiblockAutoWorkbench;
+import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
+import blusunrize.immersiveengineering.common.util.Utils;
+import blusunrize.immersiveengineering.common.util.inventory.IEInventoryHandler;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+
+public class TileEntityAutoWorkbench extends TileEntityMultiblockMetal<TileEntityAutoWorkbench,IMultiblockRecipe> implements IGuiTile, IConveyorAttachable// IAdvancedSelectionBounds,IAdvancedCollisionBounds
+{
+	public TileEntityAutoWorkbench()
+	{
+		super(MultiblockAutoWorkbench.instance, new int[]{2,3,3}, 32000, true);
+	}
+
+	public ItemStack[] inventory = new ItemStack[17];
+	public int selectedRecipe = -1;
+
+	@Override
+	public void readCustomNBT(NBTTagCompound nbt, boolean descPacket)
+	{
+		super.readCustomNBT(nbt, descPacket);
+		selectedRecipe = nbt.getInteger("selectedRecipe");
+		if(!descPacket)
+		{
+			inventory = Utils.readInventory(nbt.getTagList("inventory", 10), 17);
+		}
+	}
+	@Override
+	public void writeCustomNBT(NBTTagCompound nbt, boolean descPacket)
+	{
+		super.writeCustomNBT(nbt, descPacket);
+		nbt.setInteger("selectedRecipe",selectedRecipe);
+//		if(!descPacket)
+		{
+			nbt.setTag("inventory", Utils.writeInventory(inventory));
+		}
+	}
+	@Override
+	public void receiveMessageFromClient(NBTTagCompound message)
+	{
+		if(message.hasKey("recipe"))
+		{
+			this.selectedRecipe = message.getInteger("recipe");
+		}
+//		} else if(message.hasKey("patternSync"))
+//		{
+//			int r = message.getInteger("recipe");
+//			NBTTagList list = message.getTagList("patternSync", 10);
+//			CrafterPatternInventory pattern = patterns[r];
+//			for(int i = 0; i < list.tagCount(); i++)
+//			{
+//				NBTTagCompound itemTag = list.getCompoundTagAt(i);
+//				pattern.inv[itemTag.getInteger("slot")] = ItemStack.loadItemStackFromNBT(itemTag);
+//			}
+//		}
+	}
+	@Override
+	public void update()
+	{
+		super.update();
+
+		if(isDummy() || isRSDisabled() || worldObj.isRemote || worldObj.getTotalWorldTime()%16!=((getPos().getX()^getPos().getZ())&15) || inventory[0]==null)
+			return;
+
+		BlueprintCraftingRecipe[] recipes = BlueprintCraftingRecipe.findRecipes(ItemNBTHelper.getString(inventory[0],"blueprint"));
+		if(recipes.length>0 && (this.selectedRecipe>=0 && this.selectedRecipe<recipes.length))
+		{
+			BlueprintCraftingRecipe recipe = recipes[this.selectedRecipe];
+			if(recipe!=null && recipe.output!=null)
+			{
+				ItemStack[] query = new ItemStack[16];
+				System.arraycopy(this.inventory,1, query,0, 16);
+				int crafted = recipe.getMaxCrafted(query);
+				if(crafted>0)
+				{
+					if(this.addProcessToQueue(new MultiblockProcessInWorld(recipe, 0.78f), true))
+					{
+						this.addProcessToQueue(new MultiblockProcessInWorld(recipe, 0.78f, recipe.consumeInputs(query,1)), false);
+						System.arraycopy(query,0, this.inventory,1, 16);
+						this.markDirty();
+						this.markContainingBlockForUpdate(null);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public float[] getBlockBounds()
+	{
+		if(pos<10 || pos==12)
+			return new float[]{0,0,0,1,1,1};
+		if(pos>=13 && pos<=16)
+			return new float[]{0,0,0,1,.125f,1};
+		float xMin = 0;
+		float yMin = 0;
+		float zMin = 0;
+		float xMax = 1;
+		float yMax = 1;
+		float zMax = 1;
+		if(pos==10||pos==11)
+		{
+			yMax = .8125f;
+			if(facing==EnumFacing.NORTH)
+			{
+				zMin = .1875f;
+				if(pos==11)
+					xMax = .875f;
+			}
+			else if(facing==EnumFacing.SOUTH)
+			{
+				zMax = .8125f;
+				if(pos==11)
+					xMin = .125f;
+			}
+			else if(facing==EnumFacing.WEST)
+			{
+				xMin = .1875f;
+				if(pos==11)
+					zMin = .125f;
+			}
+			else if(facing==EnumFacing.EAST)
+			{
+				xMax = .8125f;
+				if(pos==11)
+					zMax = .875f;
+			}
+		}
+		if(pos==17)
+		{
+			yMax = .3125f;
+			if(facing==EnumFacing.NORTH)
+			{
+				zMin = .25f;
+				xMax = .875f;
+			}
+			else if(facing==EnumFacing.SOUTH)
+			{
+				zMax = .75f;
+				xMin = .125f;
+			}
+			else if(facing==EnumFacing.WEST)
+			{
+				xMin = .25f;
+				zMin = .125f;
+			}
+			else if(facing==EnumFacing.EAST)
+			{
+				xMax = .75f;
+				zMax = .875f;
+			}
+		}
+		return new float[]{xMin,yMin,zMin, xMax,yMax,zMax};
+	}
+
+	@Override
+	public int[] getEnergyPos()
+	{
+		return new int[]{9};
+	}
+	@Override
+	public int[] getRedstonePos()
+	{
+		return new int[]{1};
+	}
+
+	@Override
+	public boolean isInWorldProcessingMachine()
+	{
+		return true;
+	}
+	@Override
+	public boolean additionalCanProcessCheck(MultiblockProcess<IMultiblockRecipe> process)
+	{
+		return true;
+	}
+	@Override
+	public void doProcessOutput(ItemStack output)
+	{
+		BlockPos pos = getPos().offset(facing.rotateY(),2);
+		TileEntity inventoryTile = this.worldObj.getTileEntity(pos);
+		if(inventoryTile!=null)
+			output = Utils.insertStackIntoInventory(inventoryTile, output, facing.getOpposite());
+		if(output!=null)
+			Utils.dropStackAtPos(worldObj, pos, output, facing);
+	}
+	@Override
+	public void doProcessFluidOutput(FluidStack output)
+	{
+	}
+	@Override
+	public void onProcessFinish(MultiblockProcess<IMultiblockRecipe> process)
+	{
+	}
+	@Override
+	public int getMaxProcessPerTick()
+	{
+		return 3;
+	}
+	@Override
+	public int getProcessQueueMaxLength()
+	{
+		return 3;
+	}
+	@Override
+	public float getMinProcessDistance(MultiblockProcess<IMultiblockRecipe> process)
+	{
+		return .33f;
+	}
+
+
+	@Override
+	public ItemStack[] getInventory()
+	{
+		return this.inventory;
+	}
+	@Override
+	public boolean isStackValid(int slot, ItemStack stack)
+	{
+		return true;
+	}
+	@Override
+	public int getSlotLimit(int slot)
+	{
+		return 64;
+	}
+	@Override
+	public int[] getOutputSlots()
+	{
+		return null;
+	}
+	@Override
+	public int[] getOutputTanks()
+	{
+		return new int[0];
+	}
+	@Override
+	public FluidTank[] getInternalTanks()
+	{
+		return null;
+	}
+	@Override
+	public void doGraphicalUpdates(int slot)
+	{
+		this.markDirty();
+		this.markContainingBlockForUpdate(null);
+	}
+
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+	{
+		if(pos==9&&capability==CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+			return true;
+		return super.hasCapability(capability, facing);
+	}
+	IItemHandler insertionHandler = new IEInventoryHandler(16, this, 0, true, false);
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+	{
+		if(pos==9&&capability==CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+		{
+			TileEntityAutoWorkbench master = master();
+			if(master==null)
+				return null;
+			return (T)master.insertionHandler;
+		}
+		return super.getCapability(capability, facing);
+	}
+
+
+	@Override
+	public IMultiblockRecipe findRecipeForInsertion(ItemStack inserting)
+	{
+		return null;
+	}
+	@Override
+	protected IMultiblockRecipe readRecipeFromNBT(NBTTagCompound tag)
+	{
+		return BlueprintCraftingRecipe.loadFromNBT(tag);
+	}
+
+
+	@Override
+	public boolean canOpenGui()
+	{
+		return formed;
+	}
+	@Override
+	public int getGuiID()
+	{
+		return Lib.GUIID_AutoWorkbench;
+	}
+	@Override
+	public TileEntity getGuiMaster()
+	{
+		return master();
+	}
+
+
+	@Override
+	protected FluidTank[] getAccessibleFluidTanks(EnumFacing side)
+	{
+		return new FluidTank[0];
+	}
+	@Override
+	protected boolean canFillTankFrom(int iTank, EnumFacing side, FluidStack resource)
+	{
+		return true;
+	}
+	@Override
+	protected boolean canDrainTankFrom(int iTank, EnumFacing side)
+	{
+		return true;
+	}
+
+	@Override
+	public EnumFacing[] sigOutputDirections()
+	{
+		if(pos==14)
+			return new EnumFacing[]{this.facing.rotateY()};
+		return new EnumFacing[0];
+	}
+}
