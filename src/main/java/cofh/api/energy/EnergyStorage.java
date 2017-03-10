8227be4a23434da89bd6dcdf4841ca3c79dae12a
BluSunrize <danny.rad10@yahoo.de>
2017-03-10 22:52:27 +0100

    Updated CoFH API, just to make sure nothing goes wonky.

--- src/main/java/cofh/api/energy/EnergyStorage.java
+++ src/main/java/cofh/api/energy/EnergyStorage.java
@@ -6,7 +6,6 @@ import net.minecraft.nbt.NBTTagCompound;
  * Reference implementation of {@link IEnergyStorage}. Use/extend this or implement your own.
  *
  * @author King Lemming
- *
  */
 public class EnergyStorage implements IEnergyStorage {
 
@@ -91,8 +90,7 @@ public class EnergyStorage implements IEnergyStorage {
 	}
 
 	/**
-	 * This function is included to allow for server to client sync. Do not call this externally to the containing Tile Entity, as not all IEnergyHandlers
-	 * are guaranteed to have it.
+	 * This function is included to allow for server to client sync. Do not call this externally to the containing Tile Entity, as not all IEnergyHandlers are guaranteed to have it.
 	 *
 	 * @param energy
 	 */
@@ -108,8 +106,7 @@ public class EnergyStorage implements IEnergyStorage {
 	}
 
 	/**
-	 * This function is included to allow the containing tile to directly and efficiently modify the energy contained in the EnergyStorage. Do not rely on this
-	 * externally, as not all IEnergyHandlers are guaranteed to have it.
+	 * This function is included to allow the containing tile to directly and efficiently modify the energy contained in the EnergyStorage. Do not rely on this externally, as not all IEnergyHandlers are guaranteed to have it.
 	 *
 	 * @param energy
 	 */
--- src/main/java/cofh/api/energy/package-info.java
+++ src/main/java/cofh/api/energy/package-info.java
@@ -1,10 +1,9 @@
 /**
- * (C) 2014-2016 Team CoFH / CoFH / Cult of the Full Hub
+ * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
  * http://www.teamcofh.com
  */
-@API(apiVersion = CoFHAPIProps.VERSION, owner = "CoFHAPI", provides = "CoFHAPI|energy")
+@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhapi", provides = "cofhapi|energy")
 package cofh.api.energy;
 
-import net.minecraftforge.fml.common.API;
 import cofh.api.CoFHAPIProps;
-
+import net.minecraftforge.fml.common.API;
