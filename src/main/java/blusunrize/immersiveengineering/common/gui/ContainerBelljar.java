15d7d54995863565115e789dec3e498976c98f68
BluSunrize <danny.rad10@yahoo.de>
2017-03-18 17:20:57 +0100

    Belljar Container

--- src/main/java/blusunrize/immersiveengineering/common/gui/ContainerBelljar.java
+++ src/main/java/blusunrize/immersiveengineering/common/gui/ContainerBelljar.java
@@ -9,32 +9,12 @@ public class ContainerBelljar extends ContainerIEBase
 	public ContainerBelljar(InventoryPlayer inventoryPlayer, TileEntityBelljar tile)
 	{
 		super(inventoryPlayer, tile);
-//		for(int i=0; i<tile.getInventory().length; i++)
-//			this.addSlotToContainer(new Slot(this.inv, i, 8+(i%9)*18, 18+(i/9)*18){
-//				@Override
-//				public boolean isItemValid(ItemStack stack)
-//				{
-//					if(OreDictionary.itemMatches(new ItemStack(IEContent.blockWoodenDevice0,1,0), stack, true))
-//						return false;
-//					return !OreDictionary.itemMatches(new ItemStack(IEContent.blockWoodenDevice0, 1, 5), stack, true);
-//				}
-//			});
-		this.addSlotToContainer(new Slot(this.inv, 0, 44, 54){
-			@Override
-			public int getSlotStackLimit()
-			{
-				return 1;
-			}
-		});
-		this.addSlotToContainer(new Slot(this.inv, 1, 44, 34){
-			@Override
-			public int getSlotStackLimit()
-			{
-				return 1;
-			}
-		});
+		this.addSlotToContainer(new IESlot.Belljar(0, this, this.inv, 0, 62, 54));
+		this.addSlotToContainer(new IESlot.Belljar(1, this, this.inv, 1, 62, 34));
+		this.addSlotToContainer(new IESlot.Belljar(2, this, this.inv, 2, 8, 59));
+
 		for(int i=0; i<4; i++)
-			this.addSlotToContainer(new IESlot.Output(this, this.inv, 2+i, 116+i%2*18, 34+i/2*18));
+			this.addSlotToContainer(new IESlot.Output(this, this.inv, 3+i, 116+i%2*18, 34+i/2*18));
 
 		this.slotCount=6;
 		this.tile = tile;
