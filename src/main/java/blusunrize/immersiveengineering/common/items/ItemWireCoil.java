BluSunrize/ImmersiveEngineering

commit 7ce2220c86d4518ff602550d798c44121a060769

Misc fixes (#1979)
fixed balloons and redstone connectors being invisible in the end, closes #1953
fixed invalid connections being possible, fixes #1975, re-fixes #919
misc transformer fixes:
  -fixed bounding boxes and wire offsets (for LV on the HV transformer)
  -HV transformers no longer accept LV on one and MV on the other connector
  -removed the method for TESR render size since wires aren't TESR any more
  -probably some that I forgot about?
fixed fluid potions having missing textures, see #1974
maybe fixed #1968? Still need to test this on a dedicated server
changed the mixer to work even if the internal tank is full (the amount of fluid isn't changed during mixing)

src/main/java/blusunrize/immersiveengineering/common/items/ItemWireCoil.java
@@ -15,6 +15,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -101,7 +102,9 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 				if(!ItemNBTHelper.hasKey(stack, "linkingPos"))
 				{
 					ItemNBTHelper.setIntArray(stack, "linkingPos", new int[]{world.provider.getDimension(),masterPos.getX(),masterPos.getY(),masterPos.getZ()});
-					target.writeToNBT(stack.getTagCompound());
+					NBTTagCompound targetNbt = new NBTTagCompound();
+					target.writeToNBT(targetNbt);
+					ItemNBTHelper.setTagCompound(stack, "targettingInfo", targetNbt);
 				}
 				else
 				{
@@ -116,53 +119,56 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"sameConnection"));
 					else if( distanceSq > (type.getMaxLength()*type.getMaxLength()))
 						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"tooFar"));
-					else if(!(tileEntityLinkingPos instanceof IImmersiveConnectable))
-						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"invalidPoint"));
 					else
 					{
-						IImmersiveConnectable nodeHere = (IImmersiveConnectable)tileEntity;
-						IImmersiveConnectable nodeLink = (IImmersiveConnectable)tileEntityLinkingPos;
-						boolean connectionExists = false;
-						Set<Connection> outputs = ImmersiveNetHandler.INSTANCE.getConnections(world, Utils.toCC(nodeHere));
-						if(outputs!=null)
-							for(Connection con : outputs)
-							{
-								if(con.end.equals(Utils.toCC(nodeLink)))
-									connectionExists = true;
-							}
-						if(connectionExists)
-							player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"connectionExists"));
+						TargetingInfo targetLink = TargetingInfo.readFromNBT(ItemNBTHelper.getTagCompound(stack, "targettingInfo"));
+						if(!(tileEntityLinkingPos instanceof IImmersiveConnectable)||!((IImmersiveConnectable) tileEntityLinkingPos).canConnectCable(wire, targetLink))
+							player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"invalidPoint"));
 						else
 						{
-							Vec3d rtOff0 = nodeHere.getRaytraceOffset(nodeLink).addVector(masterPos.getX(), masterPos.getY(), masterPos.getZ());
-							Vec3d rtOff1 = nodeLink.getRaytraceOffset(nodeHere).addVector(linkPos.getX(), linkPos.getY(), linkPos.getZ());
-							Set<BlockPos> ignore = new HashSet<>();
-							ignore.addAll(nodeHere.getIgnored(nodeLink));
-							ignore.addAll(nodeLink.getIgnored(nodeHere));
-							boolean canSee = Utils.rayTraceForFirst(rtOff0, rtOff1, world, ignore)==null;
-							if(canSee)
-							{
-								TargetingInfo targetLink = TargetingInfo.readFromNBT(stack.getTagCompound());
-								ImmersiveNetHandler.INSTANCE.addConnection(world, Utils.toCC(nodeHere), Utils.toCC(nodeLink), (int)Math.sqrt(distanceSq), type);
-
-								nodeHere.connectCable(type, target, nodeLink);
-								nodeLink.connectCable(type, targetLink, nodeHere);
-								IESaveData.setDirty(world.provider.getDimension());
-								player.addStat(IEAchievements.connectWire);
-
-								if(!player.capabilities.isCreativeMode)
-									stack.stackSize--;
-								((TileEntity)nodeHere).markDirty();
-								world.addBlockEvent(masterPos, ((TileEntity) nodeHere).getBlockType(), -1, 0);
-								IBlockState state = world.getBlockState(masterPos);
-								world.notifyBlockUpdate(masterPos, state,state, 3);
-								((TileEntity)nodeLink).markDirty();
-								world.addBlockEvent(linkPos, ((TileEntity) nodeLink).getBlockType(), -1, 0);
-								state = world.getBlockState(linkPos);
-								world.notifyBlockUpdate(linkPos, state,state, 3);
-							}
+							IImmersiveConnectable nodeHere = (IImmersiveConnectable)tileEntity;
+							IImmersiveConnectable nodeLink = (IImmersiveConnectable)tileEntityLinkingPos;
+							boolean connectionExists = false;
+							Set<Connection> outputs = ImmersiveNetHandler.INSTANCE.getConnections(world, Utils.toCC(nodeHere));
+							if(outputs!=null)
+								for(Connection con : outputs)
+								{
+									if(con.end.equals(Utils.toCC(nodeLink)))
+										connectionExists = true;
+								}
+							if(connectionExists)
+								player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"connectionExists"));
 							else
-								player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"cantSee"));
+							{
+								Vec3d rtOff0 = nodeHere.getRaytraceOffset(nodeLink).addVector(masterPos.getX(), masterPos.getY(), masterPos.getZ());
+								Vec3d rtOff1 = nodeLink.getRaytraceOffset(nodeHere).addVector(linkPos.getX(), linkPos.getY(), linkPos.getZ());
+								Set<BlockPos> ignore = new HashSet<>();
+								ignore.addAll(nodeHere.getIgnored(nodeLink));
+								ignore.addAll(nodeLink.getIgnored(nodeHere));
+								boolean canSee = Utils.rayTraceForFirst(rtOff0, rtOff1, world, ignore)==null;
+								if(canSee)
+								{
+									ImmersiveNetHandler.INSTANCE.addConnection(world, Utils.toCC(nodeHere), Utils.toCC(nodeLink), (int)Math.sqrt(distanceSq), type);
+
+									nodeHere.connectCable(type, target, nodeLink);
+									nodeLink.connectCable(type, targetLink, nodeHere);
+									IESaveData.setDirty(world.provider.getDimension());
+									player.addStat(IEAchievements.connectWire);
+
+									if(!player.capabilities.isCreativeMode)
+										stack.stackSize--;
+									((TileEntity)nodeHere).markDirty();
+									world.addBlockEvent(masterPos, ((TileEntity) nodeHere).getBlockType(), -1, 0);
+									IBlockState state = world.getBlockState(masterPos);
+									world.notifyBlockUpdate(masterPos, state,state, 3);
+									((TileEntity)nodeLink).markDirty();
+									world.addBlockEvent(linkPos, ((TileEntity) nodeLink).getBlockType(), -1, 0);
+									state = world.getBlockState(linkPos);
+									world.notifyBlockUpdate(linkPos, state,state, 3);
+								}
+								else
+									player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"cantSee"));
+							}
 						}
 					}
 					ItemNBTHelper.remove(stack, "linkingPos");

src/main/java/blusunrize/immersiveengineering/client/ClientProxy.java
@@ -1073,14 +1073,8 @@ public class ClientProxy extends CommonProxy
 		ApiUtils.getRegisterSprite(event.getMap(), ConveyorSplit.texture_on);
 		ApiUtils.getRegisterSprite(event.getMap(), ConveyorSplit.texture_casing);
 
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidCreosote.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidCreosote.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPlantoil.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPlantoil.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidEthanol.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidEthanol.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidBiodiesel.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidBiodiesel.getFlowing());
+		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPotion.getStill());
+		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPotion.getFlowing());
 		ApiUtils.getRegisterSprite(event.getMap(), "immersiveengineering:items/shader_slot");
 	}
 

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityMixer.java
@@ -31,6 +31,7 @@ import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import java.util.*;
 
@@ -99,7 +100,7 @@ public class TileEntityMixer extends TileEntityMultiblockMetal<TileEntityMixer,M
 			if(energyStorage.getEnergyStored()>0 && processQueue.size()<this.getProcessQueueMaxLength())
 			{
 				int tankAmount = tank.getFluidAmount();
-				if(tankAmount>0 && tankAmount<tank.getCapacity())
+				if(tankAmount>0)
 				{
 					Set<Integer> usedInvSlots = new HashSet<Integer>();
 					for(MultiblockProcess<MixerRecipe> process : processQueue)
@@ -493,11 +494,12 @@ public class TileEntityMixer extends TileEntityMultiblockMetal<TileEntityMixer,M
 		}
 
 		@Override
-		public boolean canProcess(TileEntityMultiblockMetal multiblock)
-		{
-			if(!super.canProcess(multiblock))
+		public boolean canProcess(TileEntityMultiblockMetal multiblock) {
+			if (!(multiblock instanceof TileEntityMixer))
 				return false;
-			return ((TileEntityMixer)multiblock).tank.drain(Utils.copyFluidStackWithAmount(recipe.fluidInput,1,false),false)!=null;
+			TileEntityMixer mixer = (TileEntityMixer) multiblock;
+			// we don't need to check filling since after draining 1 mB of input fluid there will be space for 1 mB of output fluid
+			return mixer.energyStorage.extractEnergy(energyPerTick, true) == energyPerTick && mixer.tank.drain(Utils.copyFluidStackWithAmount(recipe.fluidInput, 1, false), false) != null;
 		}
 
 		@Override

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityTransformer.java
@@ -22,8 +22,6 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -106,22 +104,27 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 	{
 		if(cableType==WireType.STEEL&&!canTakeHV())
 			return false;
-		if(dummy!=0)
-		{
-			TileEntity master = worldObj.getTileEntity(getPos().add(0,-dummy,0));
-			if(master instanceof TileEntityTransformer)
-				return ((TileEntityTransformer)master).canConnectCable(cableType, target);
-			return false;
+		if(dummy!=0) {
+			TileEntity master = worldObj.getTileEntity(getPos().add(0, -dummy, 0));
+			return master instanceof TileEntityTransformer && ((TileEntityTransformer) master).canConnectCable(cableType, target);
 		}
 		int tc = getTargetedConnector(target);
 		switch(tc)
 		{
 		case 0:
-			return limitType==null && secondCable!=cableType;
+			return canAttach(cableType, limitType, secondCable);
 		case 1:
-			return secondCable==null && limitType!=cableType;
+			return canAttach(cableType, secondCable, limitType);
 		}
 		return false;
+	}
+	private boolean canAttach(WireType toAttach, WireType atConn, WireType other) {
+		if (atConn!=null)
+			return false;
+		if (other==null)
+			return true;
+		WireType higher = this instanceof TileEntityTransformerHV?WireType.STEEL:WireType.ELECTRUM;
+		return toAttach==higher||other==higher;
 	}
 	@Override
 	public void connectCable(WireType cableType, TargetingInfo target, IImmersiveConnectable other)
@@ -194,15 +197,15 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 		else
 		{
 			double conRadius = con.cableType.getRenderDiameter()/2;
-			double offset = con.cableType==WireType.COPPER?-.0625: con.cableType==WireType.ELECTRUM?.0625: .25; 
+			double offset = getIsMirrored()^b?getLowerOffset():getHigherOffset();
 			if(facing==EnumFacing.NORTH)
-				return new Vec3d(b?.8125:.1875, 2.5+offset-conRadius, .5);
+				return new Vec3d(b?.8125:.1875, 2+offset-conRadius, .5);
 			if(facing==EnumFacing.SOUTH)
-				return new Vec3d(b?.1875:.8125, 2.5+offset-conRadius, .5);
+				return new Vec3d(b?.1875:.8125, 2+offset-conRadius, .5);
 			if(facing==EnumFacing.WEST)
-				return new Vec3d(.5, 2.5+offset-conRadius, b?.1875:.8125);
+				return new Vec3d(.5, 2+offset-conRadius, b?.1875:.8125);
 			if(facing==EnumFacing.EAST)
-				return new Vec3d(.5, 2.5+offset-conRadius, b?.8125:.1875);
+				return new Vec3d(.5, 2+offset-conRadius, b?.8125:.1875);
 		}
 		return new Vec3d(.5,.5,.5);
 	}
@@ -249,24 +252,6 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 		return secondCable;
 	}
 
-	@SideOnly(Side.CLIENT)
-	private AxisAlignedBB renderAABB;
-	@SideOnly(Side.CLIENT)
-	@Override
-	public AxisAlignedBB getRenderBoundingBox()
-	{
-		//		if(renderAABB==null)
-		//		{
-		//			if(Config.getBoolean("increasedRenderboxes"))
-		//				renderAABB = AxisAlignedBB.getBoundingBox(xCoord-16, yCoord-16, zCoord-16, xCoord+17, yCoord+17, zCoord+17);
-		//			else if(!dummy)
-		//				renderAABB = AxisAlignedBB.getBoundingBox(xCoord, yCoord-1, zCoord, xCoord+1, yCoord+1.5, zCoord+1);
-		//			else
-		//				renderAABB = AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, xCoord, yCoord, zCoord);
-		//		}
-		return new AxisAlignedBB(getPos().getX()-16,getPos().getY()-16,getPos().getZ()-16, getPos().getX()+16,getPos().getY()+16,getPos().getZ()+16);
-	}
-
 	@Override
 	public PropertyBoolInverted getBoolProperty(Class<? extends IUsesBooleanProperty> inf)
 	{
@@ -277,9 +262,18 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 	{
 		if (onPost)
 			return false;
-		WireType lower = this instanceof TileEntityTransformerHV?WireType.ELECTRUM:WireType.COPPER;
-		boolean b = (limitType!=null&&lower.equals(limitType))||(secondCable!=null&&!lower.equals(secondCable));
-		return !b;
+		if (dummy!=0) {
+			TileEntity master = worldObj.getTileEntity(pos.down(dummy));
+			return master instanceof TileEntityTransformer && ((TileEntityTransformer) master).getIsMirrored();
+		}
+		else
+		{
+			if (limitType==null&&secondCable==null)
+				return true;
+			WireType higher = this instanceof TileEntityTransformerHV ? WireType.STEEL : WireType.ELECTRUM;
+			boolean b = (limitType != null && higher.equals(limitType)) || (secondCable != null && !higher.equals(secondCable));
+			return b;
+		}
 	}
 
 	@Override
@@ -362,27 +356,29 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 					facing.getAxis()==Axis.X?.75F:facing==EnumFacing.SOUTH?1.375F:.3125F};
 		return null;
 	}
-
+	boolean cachedMirrored = false;
+	private List<AxisAlignedBB> advSelectionBoxes = null;
 	@Override
 	public List<AxisAlignedBB> getAdvancedSelectionBounds()
 	{
-		if(dummy==2)
+		boolean mirrored = getIsMirrored();
+		if(dummy==2&&(advSelectionBoxes==null||cachedMirrored!=mirrored))
 		{
-			WireType lower = this instanceof TileEntityTransformerHV?WireType.ELECTRUM:WireType.COPPER;
-			WireType higher = this instanceof TileEntityTransformerHV?WireType.STEEL:WireType.ELECTRUM;
-			boolean b = (limitType!=null&&lower.equals(limitType))||(secondCable!=null&&!lower.equals(secondCable));
-			double offsetL = lower==WireType.COPPER?0: lower==WireType.ELECTRUM?.0625: .25; 
-			double offsetH = higher==WireType.COPPER?0: higher==WireType.ELECTRUM?.0625: .25; 
+			double offsetA = mirrored?getHigherOffset():getLowerOffset();
+			double offsetB = mirrored?getLowerOffset():getHigherOffset();
 			if(facing==EnumFacing.NORTH)
-				return Lists.newArrayList(new AxisAlignedBB(0,0,.3125, .375,.5+(b?offsetH:offsetL),.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.625,0,.3125, 1,.5+(b?offsetL:offsetH),.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
+				advSelectionBoxes = Lists.newArrayList(new AxisAlignedBB(0,0,.3125, .375, offsetB,.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.625,0,.3125, 1, offsetA,.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
 			if(facing==EnumFacing.SOUTH)
-				return Lists.newArrayList(new AxisAlignedBB(0,0,.3125, .375,.5+(b?offsetL:offsetH),.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.625,0,.3125, 1,.5+(b?offsetH:offsetL),.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
+				advSelectionBoxes = Lists.newArrayList(new AxisAlignedBB(0,0,.3125, .375, offsetA,.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.625,0,.3125, 1, offsetB,.6875).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
 			if(facing==EnumFacing.WEST)
-				return Lists.newArrayList(new AxisAlignedBB(.3125,0,0, .6875,.5+(b?offsetL:offsetH),.375).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.3125,0,.625, .6875,.5+(b?offsetH:offsetL),1).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
+				advSelectionBoxes = Lists.newArrayList(new AxisAlignedBB(.3125,0,0, .6875, offsetA,.375).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.3125,0,.625, .6875, offsetB,1).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
 			if(facing==EnumFacing.EAST)
-				return Lists.newArrayList(new AxisAlignedBB(.3125,0,0, .6875,.5+(b?offsetH:offsetL),.375).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.3125,0,.625, .6875,.5+(b?offsetL:offsetH),1).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
+				advSelectionBoxes = Lists.newArrayList(new AxisAlignedBB(.3125,0,0, .6875, offsetB,.375).offset(getPos().getX(),getPos().getY(),getPos().getZ()), new AxisAlignedBB(.3125,0,.625, .6875, offsetA,1).offset(getPos().getX(),getPos().getY(),getPos().getZ()));
+			cachedMirrored = mirrored;
+		} else if (dummy!=2) {
+			advSelectionBoxes = null;
 		}
-		return null;
+		return advSelectionBoxes;
 	}
 
 	@Override
@@ -402,5 +398,13 @@ public class TileEntityTransformer extends TileEntityImmersiveConnectable implem
 	public boolean getIsSecondState()
 	{
 		return onPost;
+	}
+
+	protected float getLowerOffset() {
+		return .5F;
+	}
+
+	protected float getHigherOffset() {
+		return .5625F;
 	}
 }

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityTransformerHV.java
@@ -12,4 +12,14 @@ public class TileEntityTransformerHV extends TileEntityTransformer
 	{
 		return false;
 	}
+
+	@Override
+	protected float getLowerOffset() {
+		return super.getHigherOffset();
+	}
+
+	@Override
+	protected float getHigherOffset() {
+		return .75F;
+	}
 }

src/main/java/blusunrize/immersiveengineering/common/util/network/MessageMineralListSync.java
@@ -4,6 +4,7 @@ import blusunrize.immersiveengineering.api.tool.ExcavatorHandler;
 import blusunrize.immersiveengineering.api.tool.ExcavatorHandler.MineralMix;
 import blusunrize.immersiveengineering.client.ClientProxy;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -55,11 +56,15 @@ public class MessageMineralListSync implements IMessage
 		@Override
 		public IMessage onMessage(MessageMineralListSync message, MessageContext ctx)
 		{
+			Minecraft.getMinecraft().addScheduledTask(()->onMessageMain(message));
+			return null;
+		}
+		private void onMessageMain(MessageMineralListSync message)
+		{
 			ExcavatorHandler.mineralList.clear();
 			for(MineralMix min : message.map.keySet())
 				ExcavatorHandler.mineralList.put(min, message.map.get(min));
 			ClientProxy.handleMineralManual();
-			return null;
 		}
 	}
 }

commit a459f882f811181b911f6d2235535815adf3a432

Fixed wire coils not stacking after being used for linking, caused by #1979

src/main/java/blusunrize/immersiveengineering/common/items/ItemWireCoil.java
@@ -172,10 +172,7 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 						}
 					}
 					ItemNBTHelper.remove(stack, "linkingPos");
-					ItemNBTHelper.remove(stack, "side");
-					ItemNBTHelper.remove(stack, "hitX");
-					ItemNBTHelper.remove(stack, "hitY");
-					ItemNBTHelper.remove(stack, "hitZ");
+					ItemNBTHelper.remove(stack, "targettingInfo");
 				}
 				return EnumActionResult.SUCCESS;
 			}