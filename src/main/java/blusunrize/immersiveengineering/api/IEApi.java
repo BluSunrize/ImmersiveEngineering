BluSunrize/ImmersiveEngineering

commit 7ce2220c86d4518ff602550d798c44121a060769

Misc fixes (#1979)
fixed balloons and redstone connectors being invisible in the end, closes #1953
fixed invalid connections being possible, fixes #1975, re-fixes #919
misc transformer fixes:
  -fixed bounding boxes and wire offsets (for LV on the HV transformer)
  -HV transformers no longer accept LV on one and MV on the other connector
  -removed the method for TESR render size since wires aren't TESR any more
  -probably some that I forgot about?
fixed fluid potions having missing textures, see #1974
maybe fixed #1968? Still need to test this on a dedicated server
changed the mixer to work even if the internal tank is full (the amount of fluid isn't changed during mixing)

src/main/java/blusunrize/immersiveengineering/client/ClientProxy.java
@@ -1073,14 +1073,8 @@ public class ClientProxy extends CommonProxy
 		ApiUtils.getRegisterSprite(event.getMap(), ConveyorSplit.texture_on);
 		ApiUtils.getRegisterSprite(event.getMap(), ConveyorSplit.texture_casing);
 
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidCreosote.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidCreosote.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPlantoil.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPlantoil.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidEthanol.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidEthanol.getFlowing());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidBiodiesel.getStill());
-		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidBiodiesel.getFlowing());
+		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPotion.getStill());
+		ApiUtils.getRegisterSprite(event.getMap(), IEContent.fluidPotion.getFlowing());
 		ApiUtils.getRegisterSprite(event.getMap(), "immersiveengineering:items/shader_slot");
 	}
 

src/main/java/blusunrize/immersiveengineering/common/items/ItemWireCoil.java
@@ -15,6 +15,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -101,7 +102,9 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 				if(!ItemNBTHelper.hasKey(stack, "linkingPos"))
 				{
 					ItemNBTHelper.setIntArray(stack, "linkingPos", new int[]{world.provider.getDimension(),masterPos.getX(),masterPos.getY(),masterPos.getZ()});
-					target.writeToNBT(stack.getTagCompound());
+					NBTTagCompound targetNbt = new NBTTagCompound();
+					target.writeToNBT(targetNbt);
+					ItemNBTHelper.setTagCompound(stack, "targettingInfo", targetNbt);
 				}
 				else
 				{
@@ -116,53 +119,56 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"sameConnection"));
 					else if( distanceSq > (type.getMaxLength()*type.getMaxLength()))
 						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"tooFar"));
-					else if(!(tileEntityLinkingPos instanceof IImmersiveConnectable))
-						player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"invalidPoint"));
 					else
 					{
-						IImmersiveConnectable nodeHere = (IImmersiveConnectable)tileEntity;
-						IImmersiveConnectable nodeLink = (IImmersiveConnectable)tileEntityLinkingPos;
-						boolean connectionExists = false;
-						Set<Connection> outputs = ImmersiveNetHandler.INSTANCE.getConnections(world, Utils.toCC(nodeHere));
-						if(outputs!=null)
-							for(Connection con : outputs)
-							{
-								if(con.end.equals(Utils.toCC(nodeLink)))
-									connectionExists = true;
-							}
-						if(connectionExists)
-							player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"connectionExists"));
+						TargetingInfo targetLink = TargetingInfo.readFromNBT(ItemNBTHelper.getTagCompound(stack, "targettingInfo"));
+						if(!(tileEntityLinkingPos instanceof IImmersiveConnectable)||!((IImmersiveConnectable) tileEntityLinkingPos).canConnectCable(wire, targetLink))
+							player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"invalidPoint"));
 						else
 						{
-							Vec3d rtOff0 = nodeHere.getRaytraceOffset(nodeLink).addVector(masterPos.getX(), masterPos.getY(), masterPos.getZ());
-							Vec3d rtOff1 = nodeLink.getRaytraceOffset(nodeHere).addVector(linkPos.getX(), linkPos.getY(), linkPos.getZ());
-							Set<BlockPos> ignore = new HashSet<>();
-							ignore.addAll(nodeHere.getIgnored(nodeLink));
-							ignore.addAll(nodeLink.getIgnored(nodeHere));
-							boolean canSee = Utils.rayTraceForFirst(rtOff0, rtOff1, world, ignore)==null;
-							if(canSee)
-							{
-								TargetingInfo targetLink = TargetingInfo.readFromNBT(stack.getTagCompound());
-								ImmersiveNetHandler.INSTANCE.addConnection(world, Utils.toCC(nodeHere), Utils.toCC(nodeLink), (int)Math.sqrt(distanceSq), type);
-
-								nodeHere.connectCable(type, target, nodeLink);
-								nodeLink.connectCable(type, targetLink, nodeHere);
-								IESaveData.setDirty(world.provider.getDimension());
-								player.addStat(IEAchievements.connectWire);
-
-								if(!player.capabilities.isCreativeMode)
-									stack.stackSize--;
-								((TileEntity)nodeHere).markDirty();
-								world.addBlockEvent(masterPos, ((TileEntity) nodeHere).getBlockType(), -1, 0);
-								IBlockState state = world.getBlockState(masterPos);
-								world.notifyBlockUpdate(masterPos, state,state, 3);
-								((TileEntity)nodeLink).markDirty();
-								world.addBlockEvent(linkPos, ((TileEntity) nodeLink).getBlockType(), -1, 0);
-								state = world.getBlockState(linkPos);
-								world.notifyBlockUpdate(linkPos, state,state, 3);
-							}
+							IImmersiveConnectable nodeHere = (IImmersiveConnectable)tileEntity;
+							IImmersiveConnectable nodeLink = (IImmersiveConnectable)tileEntityLinkingPos;
+							boolean connectionExists = false;
+							Set<Connection> outputs = ImmersiveNetHandler.INSTANCE.getConnections(world, Utils.toCC(nodeHere));
+							if(outputs!=null)
+								for(Connection con : outputs)
+								{
+									if(con.end.equals(Utils.toCC(nodeLink)))
+										connectionExists = true;
+								}
+							if(connectionExists)
+								player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"connectionExists"));
 							else
-								player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"cantSee"));
+							{
+								Vec3d rtOff0 = nodeHere.getRaytraceOffset(nodeLink).addVector(masterPos.getX(), masterPos.getY(), masterPos.getZ());
+								Vec3d rtOff1 = nodeLink.getRaytraceOffset(nodeHere).addVector(linkPos.getX(), linkPos.getY(), linkPos.getZ());
+								Set<BlockPos> ignore = new HashSet<>();
+								ignore.addAll(nodeHere.getIgnored(nodeLink));
+								ignore.addAll(nodeLink.getIgnored(nodeHere));
+								boolean canSee = Utils.rayTraceForFirst(rtOff0, rtOff1, world, ignore)==null;
+								if(canSee)
+								{
+									ImmersiveNetHandler.INSTANCE.addConnection(world, Utils.toCC(nodeHere), Utils.toCC(nodeLink), (int)Math.sqrt(distanceSq), type);
+
+									nodeHere.connectCable(type, target, nodeLink);
+									nodeLink.connectCable(type, targetLink, nodeHere);
+									IESaveData.setDirty(world.provider.getDimension());
+									player.addStat(IEAchievements.connectWire);
+
+									if(!player.capabilities.isCreativeMode)
+										stack.stackSize--;
+									((TileEntity)nodeHere).markDirty();
+									world.addBlockEvent(masterPos, ((TileEntity) nodeHere).getBlockType(), -1, 0);
+									IBlockState state = world.getBlockState(masterPos);
+									world.notifyBlockUpdate(masterPos, state,state, 3);
+									((TileEntity)nodeLink).markDirty();
+									world.addBlockEvent(linkPos, ((TileEntity) nodeLink).getBlockType(), -1, 0);
+									state = world.getBlockState(linkPos);
+									world.notifyBlockUpdate(linkPos, state,state, 3);
+								}
+								else
+									player.addChatMessage(new TextComponentTranslation(Lib.CHAT_WARN+"cantSee"));
+							}
 						}
 					}
 					ItemNBTHelper.remove(stack, "linkingPos");

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityMixer.java
@@ -31,6 +31,7 @@ import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import java.util.*;
 
@@ -99,7 +100,7 @@ public class TileEntityMixer extends TileEntityMultiblockMetal<TileEntityMixer,M
 			if(energyStorage.getEnergyStored()>0 && processQueue.size()<this.getProcessQueueMaxLength())
 			{
 				int tankAmount = tank.getFluidAmount();
-				if(tankAmount>0 && tankAmount<tank.getCapacity())
+				if(tankAmount>0)
 				{
 					Set<Integer> usedInvSlots = new HashSet<Integer>();
 					for(MultiblockProcess<MixerRecipe> process : processQueue)
@@ -493,11 +494,12 @@ public class TileEntityMixer extends TileEntityMultiblockMetal<TileEntityMixer,M
 		}
 
 		@Override
-		public boolean canProcess(TileEntityMultiblockMetal multiblock)
-		{
-			if(!super.canProcess(multiblock))
+		public boolean canProcess(TileEntityMultiblockMetal multiblock) {
+			if (!(multiblock instanceof TileEntityMixer))
 				return false;
-			return ((TileEntityMixer)multiblock).tank.drain(Utils.copyFluidStackWithAmount(recipe.fluidInput,1,false),false)!=null;
+			TileEntityMixer mixer = (TileEntityMixer) multiblock;
+			// we don't need to check filling since after draining 1 mB of input fluid there will be space for 1 mB of output fluid
+			return mixer.energyStorage.extractEnergy(energyPerTick, true) == energyPerTick && mixer.tank.drain(Utils.copyFluidStackWithAmount(recipe.fluidInput, 1, false), false) != null;
 		}
 
 		@Override

commit 188a5c0895fb503a27d83ac3a9422cf615a21d4b

Misc fixes for build 58 (#2064)
* fixed stuttering on the metal press and the watermill
re-enabled quad-caching on the metal press

* Stopped relays from connecting to conduits/cables/etc
To help prevent further DireDerps :) (not necessarily by him)

* fixed some TESR models not updating on resource reload
(more to the point, on changing mipmap level, because that changes the UV's)

* Probably fixed a crash when shooting a mob from Lycanite's Mobs with a turret, closes #2039
Fixed #2042 and then got sidetracked fixing a couple of other revolver GUI bugs

* Fixed the blast furnace being able to make steel blocks faster than intended, closes #2058

* The skyhook no longer is a ridiculously powerful weapon

* Fixed wire coils not stacking after being used for linking, caused by #1979

* Added a list to the API to allow addons to have their own render caches cleared by "/cie resetrender"

* Fixed redstone wires resetting their values on world reload and initialy not transmitting the signals when merging 2 networks

* Prevented beds from being rotated (I did that by accident when trying to sleep in my survival world)
Added a bunch of commented out rotation "preventions" for things that break in a non-gamebreaking manner when rotated

src/main/java/blusunrize/immersiveengineering/api/IEApi.java
@@ -6,6 +6,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameData;
 import net.minecraftforge.oredict.OreDictionary;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -44,6 +45,12 @@ public class IEApi
 	 * 2: conductive, increases flux damage done to the target (CoFH/RedstoneArsenal compat)<br>
 	 */
 	public static Potion[] potions;
+
+	/**
+	 * Each element of this list will be called once when the command "/cie resetrender" is run.
+	 * These elements could be something like modelCache::clear.
+	 */
+	public static List<Runnable> renderCacheClearers = new ArrayList<>();
 
 	public static ItemStack getPreferredOreStack(String oreName)
 	{

src/main/java/blusunrize/immersiveengineering/api/energy/wires/redstone/RedstoneWireNetwork.java
@@ -36,6 +36,7 @@ public class RedstoneWireNetwork
 			if(connector != null)
 				connector.onChange();
 		}
+		updateValues();
 	}
 
 	public void removeFromNetwork(IRedstoneConnector removedConnector)

src/main/java/blusunrize/immersiveengineering/client/ClientProxy.java
@@ -1505,15 +1505,23 @@ public class ClientProxy extends CommonProxy
 		ConnModelReal.cache.clear();
 	}
 
+	static
+	{
+		IEApi.renderCacheClearers.add(IESmartObjModel.modelCache::clear);
+		IEApi.renderCacheClearers.add(IESmartObjModel.modelCache::clear);
+		IEApi.renderCacheClearers.add(ConnModelReal.cache::clear);
+		IEApi.renderCacheClearers.add(ModelConveyor.modelCache::clear);
+		IEApi.renderCacheClearers.add(ModelConfigurableSides.modelCache::clear);
+		IEApi.renderCacheClearers.add(TileEntityFluidPipe.cachedOBJStates::clear);
+		IEApi.renderCacheClearers.add(TileRenderBelljar::reset);
+		IEApi.renderCacheClearers.add(TileRenderWatermill::reset);
+		IEApi.renderCacheClearers.add(TileRenderWindmill::reset);
+	}
 	@Override
 	public void clearRenderCaches()
 	{
-		IESmartObjModel.modelCache.clear();
-		IESmartObjModel.cachedBakedItemModels.clear();
-		ConnModelReal.cache.clear();
-		ModelConveyor.modelCache.clear();
-		ModelConfigurableSides.modelCache.clear();
-		TileEntityFluidPipe.cachedOBJStates.clear();
+		for (Runnable r:IEApi.renderCacheClearers)
+			r.run();
 	}
 	private static void mapFluidState(Block block, Fluid fluid)
 	{

src/main/java/blusunrize/immersiveengineering/client/render/TileRenderBelljar.java
@@ -25,8 +25,8 @@ import java.util.List;
 
 public class TileRenderBelljar extends TileEntitySpecialRenderer<TileEntityBelljar>
 {
-	private HashMap<EnumFacing, List<BakedQuad>> quads = new HashMap<>();
-	private HashMap<IBlockState, List<BakedQuad>> plantQuads = new HashMap<>();
+	private static HashMap<EnumFacing, List<BakedQuad>> quads = new HashMap<>();
+	private static HashMap<IBlockState, List<BakedQuad>> plantQuads = new HashMap<>();
 
 	@Override
 	public void renderTileEntityAt(TileEntityBelljar tile, double x, double y, double z, float partialTicks, int destroyStage)
@@ -79,14 +79,14 @@ public class TileRenderBelljar extends TileEntitySpecialRenderer<TileEntityBellj
 					return;
 				for(IBlockState s : states)
 				{
-					List<BakedQuad> plantQuadList = this.plantQuads.get(s);
-//					if(plantQuadList==null)
+					List<BakedQuad> plantQuadList = plantQuads.get(s);
+					if(plantQuadList==null)
 					{
 						IBakedModel plantModel = blockRenderer.getModelForState(s);
 						plantQuadList = plantModel.getQuads(s,null,0);
 						for(EnumFacing f : EnumFacing.values())
 							plantQuadList.addAll(plantModel.getQuads(s,f,0));
-						this.plantQuads.put(s, plantQuadList);
+						plantQuads.put(s, plantQuadList);
 					}
 					if(plantQuadList!=null)
 					{
@@ -112,5 +112,10 @@ public class TileRenderBelljar extends TileEntitySpecialRenderer<TileEntityBellj
 
 		GlStateManager.popMatrix();
 		RenderHelper.enableStandardItemLighting();
+	}
+	public static void reset()
+	{
+		quads.clear();
+		plantQuads.clear();
 	}
 }

src/main/java/blusunrize/immersiveengineering/client/render/TileRenderMetalPress.java
@@ -52,7 +52,7 @@ public class TileRenderMetalPress extends TileEntitySpecialRenderer<TileEntityMe
 				continue;
 			float transportTime = 52.5f/(float)process.maxTicks;
 			float pressTime = 3.75f/(float)process.maxTicks;
-			float fProcess = process.processTick/(float)process.maxTicks;
+			float fProcess = (process.processTick+(te.shouldRenderAsActive()?partialTicks:0))/(float)process.maxTicks;
 			if(fProcess<transportTime)
 				shift[i] = fProcess/transportTime*.5f;
 			else if(fProcess<(1-transportTime))

src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWatermill.java
@@ -57,4 +57,8 @@ public class TileRenderWatermill extends TileEntitySpecialRenderer<TileEntityWat
 		GlStateManager.disableBlend();
 		GlStateManager.enableCull();
 	}
+	public static void reset()
+	{
+		quads = null;
+	}
 }

src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWindmill.java
@@ -17,10 +17,15 @@ import net.minecraft.util.math.BlockPos;
 import org.lwjgl.opengl.GL11;
 
 import java.util.List;
+import java.util.WeakHashMap;
 
 public class TileRenderWindmill extends TileEntitySpecialRenderer<TileEntityWindmill>
 {
 	private List<BakedQuad> quads;
+	private static WeakHashMap<TileRenderWindmill, Boolean> instances = new WeakHashMap<>();
+	{
+		instances.put(this, true);
+	}
 	@Override
 	public void renderTileEntityAt(TileEntityWindmill tile, double x, double y, double z, float partialTicks, int destroyStage)
 	//	public void renderTileEntityFast(TileEntityWindmill tile, double x, double y, double z, float partialTicks, int destroyStage, VertexBuffer vertexBuffer)
@@ -63,5 +68,10 @@ public class TileRenderWindmill extends TileEntitySpecialRenderer<TileEntityWind
 	    RenderHelper.enableStandardItemLighting();
 		GlStateManager.disableBlend();
 		GlStateManager.enableCull();
+	}
+	public static void reset()
+	{
+		for (TileRenderWindmill r:instances.keySet())
+			r.quads = null;
 	}
 }

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityConnectorLV.java
@@ -238,25 +238,11 @@ public class TileEntityConnectorLV extends TileEntityImmersiveConnectable implem
 	@Override
 	public IEForgeEnergyWrapper getCapabilityWrapper(EnumFacing facing)
 	{
-		if(facing!=this.facing)
+		if(facing!=this.facing || isRelay())
 			return null;
 		if(energyWrapper==null || energyWrapper.side!=this.facing)
 			energyWrapper = new IEForgeEnergyWrapper(this, this.facing);
 		return energyWrapper;
-	}
-	@Override
-	public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing)
-	{
-		if(capability == CapabilityEnergy.ENERGY && facing==this.facing)
-			return true;
-		return super.hasCapability(capability, facing);
-	}
-	@Override
-	public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing)
-	{
-		if(capability == CapabilityEnergy.ENERGY && facing==this.facing)
-			return (T)getCapabilityWrapper(this.facing);
-		return super.getCapability(capability, facing);
 	}
 
 	@Override

src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityTurret.java
@@ -161,9 +161,12 @@ public abstract class TileEntityTurret extends TileEntityIEBase implements ITick
 			if(whitelist ^ isListedName(targetList, entity.getName()))
 				continue;
 			//Same as above but for the owner of the pet, to prevent shooting wolves
-			if(entity instanceof IEntityOwnable && ((IEntityOwnable)entity).getOwner()!=null)
-				if(whitelist ^ isListedName(targetList, ((IEntityOwnable)entity).getOwner().getName()))
+			if(entity instanceof IEntityOwnable)
+			{
+				Entity entityOwner = ((IEntityOwnable)entity).getOwner();
+				if(entityOwner!=null&&(whitelist ^ isListedName(targetList, entityOwner.getName())))
 					continue;
+			}
 
 			if(entity instanceof EntityAnimal && !attackAnimals)
 				continue;

src/main/java/blusunrize/immersiveengineering/common/blocks/stone/TileEntityBlastFurnace.java
@@ -97,9 +97,18 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 					}
 					else
 					{
-						process-=processSpeed;
-						if(!active)
-							active=true;
+						BlastFurnaceRecipe recipe = getRecipe();
+						if (recipe!=null&&recipe.time!=processMax) {
+							processMax = 0;
+							process = 0;
+							active = false;
+						}
+						else
+						{
+							process -= processSpeed;
+							if (!active)
+								active = true;
+						}
 					}
 					burnTime-=processSpeed;
 					markContainingBlockForUpdate(null);

src/main/java/blusunrize/immersiveengineering/common/blocks/wooden/TileEntityWatermill.java
@@ -51,9 +51,9 @@ public class TileEntityWatermill extends TileEntityIEBase implements ITickable, 
 			return;
 		}
 		else
-			canTurn=getRotationVec().lengthVector()!=0;
+			canTurn = multiblock||getRotationVec().lengthVector() != 0;
 
-		if(worldObj.getTotalWorldTime()%256==((getPos().getX()^getPos().getZ())&255)) 
+		if(worldObj.getTotalWorldTime()%64==((getPos().getX()^getPos().getZ())&63))
 		{
 			rotationVec=null;
 		}
@@ -73,7 +73,7 @@ public class TileEntityWatermill extends TileEntityIEBase implements ITickable, 
 				tileEntity = worldObj.getTileEntity(getPos().offset(facing, l));
 			}
 
-			double perTick = 1f/1440 * power/l;
+			perTick = 1f/1440 * power/l;
 			canTurn = perTick!=0;
 			rotation += perTick;
 			rotation %= 1;
@@ -84,6 +84,7 @@ public class TileEntityWatermill extends TileEntityIEBase implements ITickable, 
 				{
 					((TileEntityWatermill)tileEntity).rotation = rotation;
 					((TileEntityWatermill)tileEntity).canTurn = canTurn;
+					((TileEntityWatermill)tileEntity).perTick = perTick;
 					((TileEntityWatermill)tileEntity).multiblock = true;
 				}
 			}

src/main/java/blusunrize/immersiveengineering/common/gui/ContainerInternalStorageItem.java
@@ -29,7 +29,7 @@ public abstract class ContainerInternalStorageItem extends Container
 		this.blockedSlot = (iinventory.currentItem + 27 + internalSlots);
 
 
-		if (!world.isRemote)
+		//if (!world.isRemote)
 			try {
 				((InventoryStorageItem)this.input).stackList = ((IInternalStorageItem)this.heldItem.getItem()).getContainedItems(this.heldItem);
 			} catch(Exception e)
@@ -44,13 +44,13 @@ public abstract class ContainerInternalStorageItem extends Container
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int slot)
 	{
-		ItemStack stack = null;
+		ItemStack oldStackInSlot = null;
 		Slot slotObject = inventorySlots.get(slot);
 
 		if(slotObject != null && slotObject.getHasStack())
 		{
 			ItemStack stackInSlot = slotObject.getStack();
-			stack = stackInSlot.copy();
+			oldStackInSlot = stackInSlot.copy();
 
 			if(slot < internalSlots)
 			{
@@ -78,9 +78,7 @@ public abstract class ContainerInternalStorageItem extends Container
 						if(this.mergeItemStack(insert, i, i + 1, true))
 						{
 							b = false;
-							break;
-						} else
-							continue;
+						}
 					}
 				}
 				if(b)
@@ -91,13 +89,14 @@ public abstract class ContainerInternalStorageItem extends Container
 				slotObject.putStack(null);
 			else
 				slotObject.onSlotChanged();
-
-			if (stackInSlot.stackSize == stack.stackSize)
+			slotObject.inventory.markDirty();
+			if (stackInSlot.stackSize == oldStackInSlot.stackSize)
 				return null;
-			slotObject.onPickupFromSlot(player, stack);
+			slotObject.onPickupFromSlot(player, oldStackInSlot);
 			updatePlayerItem();
+			detectAndSendChanges();
 		}
-		return stack;
+		return oldStackInSlot;
 	}
 
 	@Override

src/main/java/blusunrize/immersiveengineering/common/items/ItemInternalStorage.java
@@ -42,7 +42,18 @@ public abstract class ItemInternalStorage extends ItemIEBase implements IInterna
 				stackList[i].writeToNBT(tag);
 				inv.appendTag(tag);
 			}
-		if(!stack.hasTagCompound())
+		if(stack.hasTagCompound())
+		{
+			NBTTagList invExisting = stack.getTagCompound().getTagList("Inv",10);
+			for (int i=0; i<invExisting.tagCount(); i++)
+			{
+				NBTTagCompound tag = invExisting.getCompoundTagAt(i);
+				int slot = tag.getByte("Slot") & 0xFF;
+				if ((slot >= stackList.length) && (slot < stackList.length))
+					inv.appendTag(tag);
+			}
+		}
+		else
 			stack.setTagCompound(new NBTTagCompound());
 		stack.getTagCompound().setTag("Inv",inv);
 	}

src/main/java/blusunrize/immersiveengineering/common/items/ItemRevolver.java
@@ -61,7 +61,7 @@ public class ItemRevolver extends ItemUpgradeableTool implements IOBJModelCallba
 		super("revolver", 1, "REVOLVER", "normal","speedloader");
 	}
 	public static UUID speedModUUID = Utils.generateNewUUID();
-	public HashMap<String, TextureAtlasSprite> revolverIcons = new HashMap();
+	public HashMap<String, TextureAtlasSprite> revolverIcons = new HashMap<>();
 	public TextureAtlasSprite revolverDefaultTexture;
 	public void stichRevolverTextures(TextureMap map)
 	{
@@ -356,6 +356,8 @@ public class ItemRevolver extends ItemUpgradeableTool implements IOBJModelCallba
 		ItemStack[] stackList = this.getContainedItems(revolver);
 		for(int i=0; i<bullets.length; i++)
 			stackList[i] = bullets[i];
+		for (int i = bullets.length;i<getBulletSlotAmount(revolver);i++)
+			stackList[i] = null;
 		this.setContainedItems(revolver, stackList);
 	}
 	public int getBulletSlotAmount(ItemStack revolver)

src/main/java/blusunrize/immersiveengineering/common/items/ItemSkyhook.java
@@ -8,15 +8,11 @@ import blusunrize.immersiveengineering.common.gui.IESlot;
 import blusunrize.immersiveengineering.common.util.IEAchievements;
 import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
 import blusunrize.immersiveengineering.common.util.SkylineHelper;
-import com.google.common.collect.Multimap;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -52,7 +48,7 @@ public class ItemSkyhook extends ItemUpgradeableTool implements ITool
 			ItemNBTHelper.setFloat(stack, "fallDamageBoost", dmg);
 		}
 	}
-	@Override
+	/*@Override
 	public Multimap getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack)
 	{
 		Multimap multimap = super.getAttributeModifiers(slot, stack);
@@ -62,7 +58,7 @@ public class ItemSkyhook extends ItemUpgradeableTool implements ITool
 			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", dmg, 0));
 		}
 		return multimap;
-	}
+	}*/
 
 	public static HashMap<String, EntitySkylineHook> existingHooks = new HashMap<String, EntitySkylineHook>();
 

src/main/java/blusunrize/immersiveengineering/common/items/ItemWireCoil.java
@@ -172,10 +172,7 @@ public class ItemWireCoil extends ItemIEBase implements IWireCoil
 						}
 					}
 					ItemNBTHelper.remove(stack, "linkingPos");
-					ItemNBTHelper.remove(stack, "side");
-					ItemNBTHelper.remove(stack, "hitX");
-					ItemNBTHelper.remove(stack, "hitY");
-					ItemNBTHelper.remove(stack, "hitZ");
+					ItemNBTHelper.remove(stack, "targettingInfo");
 				}
 				return EnumActionResult.SUCCESS;
 			}

src/main/java/blusunrize/immersiveengineering/common/util/RotationUtil.java
@@ -25,6 +25,20 @@ public class RotationUtil
 			return !((state.getBlock()==Blocks.PISTON||state.getBlock()==Blocks.STICKY_PISTON)&&state.getValue(BlockPistonBase.EXTENDED));
 		});
 		permittedRotation.add(state -> {
+			//beds don't like being rotated piecewise
+			return state.getBlock()!=Blocks.BED;
+		});
+		/*permittedRotation.add(state -> {
+			//A lot of the RS stuff breaks when rotated
+			Block b = state.getBlock();
+			return b!=Blocks.TRIPWIRE_HOOK&&b!=Blocks.STONE_BUTTON&&b!=Blocks.WOODEN_BUTTON&&b!=Blocks.LEVER&&b!=Blocks.REDSTONE_TORCH;
+		});
+		permittedRotation.add(state -> {
+			//misc things don't like floating in the air...
+			Block b = state.getBlock();
+			return b!=Blocks.TORCH&&b!=Blocks.LADDER&&b!=Blocks.WALL_SIGN&&b!=Blocks.WALL_BANNER;
+		});*/
+		permittedRotation.add(state -> {
 			//preventing endportals, skulls from rotating
 			return !(state.getBlock()==Blocks.END_PORTAL_FRAME||state.getBlock()==Blocks.SKULL);
 		});