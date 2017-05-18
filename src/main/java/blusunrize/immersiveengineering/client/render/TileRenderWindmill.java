a1be77b2e3e768b434d4efbf99f2041bcea5c581
BluSunrize <danny.rad10@yahoo.de>
2017-05-18 18:58:24 +0200

    Changed the windmills! The advanced windmill is no longer accessible, instead the normal windmill is upgraded in world through use of sails (missing texture + changed recipe atm) Height-scaling has been disabled to encourage builds focusing on design rather than poweroutput.

--- src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWindmill.java
+++ src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWindmill.java
@@ -7,6 +7,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -14,14 +15,18 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.EnumFacing.AxisDirection;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.client.model.obj.OBJModel.OBJState;
+import net.minecraftforge.common.property.IExtendedBlockState;
+import net.minecraftforge.common.property.Properties;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.WeakHashMap;
 
 public class TileRenderWindmill extends TileEntitySpecialRenderer<TileEntityWindmill>
 {
-	private List<BakedQuad> quads;
+	private List<BakedQuad>[] quads = new List[9];
 	private static WeakHashMap<TileRenderWindmill, Boolean> instances = new WeakHashMap<>();
 	{
 		instances.put(this, true);
@@ -30,16 +35,25 @@ public class TileRenderWindmill extends TileEntitySpecialRenderer<TileEntityWind
 	public void renderTileEntityAt(TileEntityWindmill tile, double x, double y, double z, float partialTicks, int destroyStage)
 	//	public void renderTileEntityFast(TileEntityWindmill tile, double x, double y, double z, float partialTicks, int destroyStage, VertexBuffer vertexBuffer)
 	{
-		if (!tile.getWorld().isBlockLoaded(tile.getPos(), false))
+		if(!tile.getWorld().isBlockLoaded(tile.getPos(), false))
 			return;
 		final BlockRendererDispatcher blockRenderer = Minecraft.getMinecraft().getBlockRendererDispatcher();
 		BlockPos blockPos = tile.getPos();
-		if (quads==null)
+		if(quads[tile.sails]==null)
 		{
 			IBlockState state = getWorld().getBlockState(blockPos);
 			state = state.getActualState(getWorld(), blockPos);
 			state = state.withProperty(IEProperties.FACING_ALL, EnumFacing.NORTH);
-			quads = blockRenderer.getBlockModelShapes().getModelForState(state).getQuads(state, null, 0);
+			IBakedModel model = blockRenderer.getBlockModelShapes().getModelForState(state);
+			if(state instanceof IExtendedBlockState)
+			{
+				List<String> parts = new ArrayList<>();
+				parts.add("base");
+				for(int i=1; i<=tile.sails; i++)
+					parts.add("sail_"+i);
+				state = ((IExtendedBlockState)state).withProperty(Properties.AnimationProperty, new OBJState(parts, true));
+			}
+			quads[tile.sails] = model.getQuads(state, null, 0);
 		}
 		Tessellator tessellator = Tessellator.getInstance();
 		GlStateManager.blendFunc(770, 771);
@@ -55,23 +69,24 @@ public class TileRenderWindmill extends TileEntitySpecialRenderer<TileEntityWind
 
 		GlStateManager.rotate(rot, tile.facing.getAxis() == Axis.X ? 1 : 0, 0, tile.facing.getAxis() == Axis.Z ? 1 : 0);
 		GlStateManager.rotate(dir, 0, 1, 0);
-		
+
 		RenderHelper.disableStandardItemLighting();
 		Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 		VertexBuffer worldRenderer = tessellator.getBuffer();
 		worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
 		worldRenderer.setTranslation(-.5, -.5, -.5);
-		ClientUtils.renderModelTESRFast(quads, worldRenderer, tile.getWorld(), blockPos);
+		ClientUtils.renderModelTESRFast(quads[tile.sails], worldRenderer, tile.getWorld(), blockPos);
 		worldRenderer.setTranslation(0, 0, 0);
 		tessellator.draw();
 		GlStateManager.popMatrix();
-	    RenderHelper.enableStandardItemLighting();
+		RenderHelper.enableStandardItemLighting();
 		GlStateManager.disableBlend();
 		GlStateManager.enableCull();
 	}
 	public static void reset()
 	{
 		for (TileRenderWindmill r:instances.keySet())
-			r.quads = null;
+			for(int i=0; i<r.quads.length; i++)
+				r.quads[i] = null;
 	}
 }
