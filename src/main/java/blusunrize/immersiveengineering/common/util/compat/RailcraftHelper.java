914dc8478603d2a8345ecb1f0379c70d8960bd71
BluSunrize <danny.rad10@yahoo.de>
2017-06-17 17:06:56 +0200

    Buffed Railgun damage by about 50%, to compensate for it's reduced energy storage, and to offset it more from the revolver

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/RailcraftHelper.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/RailcraftHelper.java
@@ -31,11 +31,11 @@ public class RailcraftHelper extends IECompatModule
 	public void init()
 	{
 		Item itemRail = Item.REGISTRY.getObject(new ResourceLocation("railcraft:rail"));
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,0), 7,1.25).setColourMap(new int[][]{{0xa4a4a4,0x686868}});
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,1), 6,1.375).setColourMap(new int[][]{{0xa4a4a4,0xa4a4a4,0x686868, 0xddb82c,0xc9901f}, {0xa4a4a4,0xa4a4a4,0x686868, 0xf5cc2d,0xddb82c},{0xa4a4a4,0xa4a4a4,0x686868, 0xf5cc2d,0xddb82c}, {0xa4a4a4,0xa4a4a4,0x686868, 0xddb82c,0xc9901f}});
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,3), 7,1).setColourMap(new int[][]{{0x999999,0xa4a4a4,0xa4a4a4, 0xc9901f,0xc9901f,0xba851d}});
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,4), 8,1.375).setColourMap(new int[][]{{0x686868,0x808080,0x808080, 0x3e2e60,0x3e2e60,0x31254d}});
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,5), 7,1).setColourMap(new int[][]{{0x999999,0xa4a4a4,0xa4a4a4, 0x9a6033,0x9a6033,0xa86938}});
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,0), 10,1.25).setColourMap(new int[][]{{0xa4a4a4,0x686868}});
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,1), 9,1.375).setColourMap(new int[][]{{0xa4a4a4,0xa4a4a4,0x686868, 0xddb82c,0xc9901f}, {0xa4a4a4,0xa4a4a4,0x686868, 0xf5cc2d,0xddb82c},{0xa4a4a4,0xa4a4a4,0x686868, 0xf5cc2d,0xddb82c}, {0xa4a4a4,0xa4a4a4,0x686868, 0xddb82c,0xc9901f}});
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,3), 10,1).setColourMap(new int[][]{{0x999999,0xa4a4a4,0xa4a4a4, 0xc9901f,0xc9901f,0xba851d}});
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,4), 12,1.375).setColourMap(new int[][]{{0x686868,0x808080,0x808080, 0x3e2e60,0x3e2e60,0x31254d}});
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRail,1,5), 10,1).setColourMap(new int[][]{{0x999999,0xa4a4a4,0xa4a4a4, 0x9a6033,0x9a6033,0xa86938}});
 
 		Item itemRebar = Item.REGISTRY.getObject(new ResourceLocation("railcraft:rebar"));
 		int[][] rebarColourMap = new int[8*3+1][];
@@ -46,7 +46,7 @@ public class RailcraftHelper extends IECompatModule
 			rebarColourMap[1+ i*3] = rebarColourMap[1+ i*3+1] = rebarColourMap[1];
 			rebarColourMap[1+ i*3+2] = rebarColourMap[0];
 		}
-		RailgunHandler.registerProjectileProperties(new ItemStack(itemRebar), 7,1.25).setColourMap(rebarColourMap);
+		RailgunHandler.registerProjectileProperties(new ItemStack(itemRebar), 10,1.25).setColourMap(rebarColourMap);
 		Config.manual_bool.put("literalRailGun", true);
 	}
 
