615d884716ecdb2e9154eb374c73747bc446d0ff
BluSunrize <danny.rad10@yahoo.de>
2017-06-29 12:43:51 +0200

    Fixed speed of example rotation in IngredientStack

--- src/main/java/blusunrize/immersiveengineering/api/crafting/IngredientStack.java
+++ src/main/java/blusunrize/immersiveengineering/api/crafting/IngredientStack.java
@@ -143,12 +143,12 @@ public class IngredientStack
 	{
 		ItemStack ret = stack;
 		if(ret.isEmpty()&&stackList!=null&&stackList.size()>0)
-			ret = stackList.get((int)(rand / 20 % stackList.size()));
+			ret = stackList.get((int)(rand / 20) % stackList.size());
 		if(ret.isEmpty()&&oreName!=null)
 		{
 			List<ItemStack> ores = OreDictionary.getOres(oreName);
 			if(ores!=null&&ores.size()>0)
-				ret = ores.get((int)(rand / 20 % ores.size()));
+				ret = ores.get((int)(rand / 20) % ores.size());
 		}
 		if(ret.isEmpty()&&fluid!=null&&ForgeModContainer.getInstance().universalBucket!=null)
 			ret = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, fluid.getFluid());
