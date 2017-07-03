d22f7d619f65cb3f0a5c398853f0be80008f551c
BluSunrize <danny.rad10@yahoo.de>
2017-07-03 15:17:33 +0200

    Update to 1.12: Save Data & Remapper Remapper is borked atm

--- src/main/java/blusunrize/immersiveengineering/common/IESaveData.java
+++ src/main/java/blusunrize/immersiveengineering/common/IESaveData.java
@@ -10,8 +10,7 @@ import blusunrize.immersiveengineering.api.tool.ExcavatorHandler.MineralWorldInf
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldSavedData;
+import net.minecraft.world.storage.WorldSavedData;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 
