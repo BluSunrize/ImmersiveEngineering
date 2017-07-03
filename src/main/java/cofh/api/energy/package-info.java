5d49a77ddf9dbed5adf911021d446aad0b4abcc5
BluSunrize <danny.rad10@yahoo.de>
2017-07-03 19:59:40 +0200

    Update to 1.12: Removed RF API

--- src/main/java/cofh/api/energy/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
-@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhapi", provides = "cofhapi|energy")
-package cofh.api.energy;
-
-import cofh.api.CoFHAPIProps;
-import net.minecraftforge.fml.common.API;
--- src/main/java/cofh/api/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
-@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhlib", provides = "cofhapi")
-package cofh.api;
-
-import net.minecraftforge.fml.common.API;
