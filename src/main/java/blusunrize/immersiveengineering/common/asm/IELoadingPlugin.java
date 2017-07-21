631185f3517058a329775b33b5a174bc38961f1f
BluSunrize <danny.rad10@yahoo.de>
2017-07-21 23:55:27 +0200

    Fixed the build error. Fucking imports >_>

--- src/main/java/blusunrize/immersiveengineering/common/asm/IELoadingPlugin.java
+++ src/main/java/blusunrize/immersiveengineering/common/asm/IELoadingPlugin.java
@@ -1,6 +1,5 @@
 package blusunrize.immersiveengineering.common.asm;
 
-import blusunrize.immersiveengineering.common.util.IELogger;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import javax.annotation.Nullable;
@@ -9,13 +8,11 @@ import java.util.Map;
 /**
  * @author BluSunrize - 20.07.2017
  */
-//@IFMLLoadingPlugin.SortingIndex(1001)
 public class IELoadingPlugin implements IFMLLoadingPlugin
 {
 	@Override
 	public String[] getASMTransformerClass()
 	{
-		IELogger.info("IE~ get transformer");
 		return new String[]{"blusunrize.immersiveengineering.common.asm.IEClassTransformer"};
 	}
 
