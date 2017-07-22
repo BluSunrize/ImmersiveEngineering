a50980410b029546d9b234fa17e29eaeab3f4770
BluSunrize <danny.rad10@yahoo.de>
2017-07-22 13:56:12 +0200

    Added Fertilizer compat for AA, BwM and IC2, see #2345

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/ActuallyAdditionsHelper.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/ActuallyAdditionsHelper.java
@@ -31,27 +31,31 @@ public class ActuallyAdditionsHelper extends IECompatModule
 	public void postInit()
 	{
 
-		Item coffeeSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_coffee_seed"));
-		Item coffeeBeans = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_coffee_beans"));
-		Block coffeeBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions:block_coffee"));
+		Item coffeeSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_coffee_seed"));
+		Item coffeeBeans = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_coffee_beans"));
+		Block coffeeBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions","block_coffee"));
 		if(coffeeSeeds!=null && coffeeBeans!=null && coffeeBlock!=null)
 			BelljarHandler.cropHandler.register(new ItemStack(coffeeSeeds), new ItemStack[]{new ItemStack(coffeeBeans,3), new ItemStack(coffeeSeeds)}, new ItemStack(Blocks.DIRT), coffeeBlock.getDefaultState());
 
-		Item riceSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_rice_seed"));
-		Item food = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_food"));
-		Block riceBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions:block_rice"));
+		Item riceSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_rice_seed"));
+		Item food = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_food"));
+		Block riceBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions","block_rice"));
 		if(riceSeeds!=null && food!=null && riceBlock!=null)
 			BelljarHandler.cropHandler.register(new ItemStack(riceSeeds), new ItemStack[]{new ItemStack(food,2,16), new ItemStack(riceSeeds)}, new ItemStack(Blocks.DIRT), riceBlock.getDefaultState());
 
-		Item canolaSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_canola_seed"));
-		Item misc = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_misc"));
-		Block canolaBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions:block_canola"));
+		Item canolaSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_canola_seed"));
+		Item misc = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_misc"));
+		Block canolaBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions","block_canola"));
 		if(canolaSeeds!=null && misc!=null && canolaBlock!=null)
 			BelljarHandler.cropHandler.register(new ItemStack(canolaSeeds), new ItemStack[]{new ItemStack(misc,3,13), new ItemStack(canolaSeeds)}, new ItemStack(Blocks.DIRT), canolaBlock.getDefaultState());
 
-		Item flaxSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions:item_flax_seed"));
-		Block flaxBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions:block_flax"));
+		Item flaxSeeds = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_flax_seed"));
+		Block flaxBlock = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions","block_flax"));
 		if(flaxSeeds!=null && flaxBlock!=null)
 			BelljarHandler.cropHandler.register(new ItemStack(flaxSeeds), new ItemStack[]{new ItemStack(Items.STRING,4), new ItemStack(flaxSeeds)}, new ItemStack(Blocks.DIRT), flaxBlock.getDefaultState());
+
+		Item fertilizer = Item.REGISTRY.getObject(new ResourceLocation("actuallyadditions","item_fertilizer"));
+		if(fertilizer!=null)
+			BelljarHandler.registerBasicItemFertilizer(new ItemStack(fertilizer), 1.25f);
 	}
 }
