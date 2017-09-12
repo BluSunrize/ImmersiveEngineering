34687d320bc34d7fca54a7ef70c8dea1fd675d91
berinhardt <darta@web2mil.com.uy>
2017-09-12 17:58:33 -0300

    Added rs toggle to metal barrel to use with levers as fluid valves (#2487)
    * Added rs toggle to metak barrel to use with levers as fluid valves (Issue #1328)
    
    * rs toggled barrel mentioned on manual

--- src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityMetalBarrel.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityMetalBarrel.java
@@ -5,6 +5,11 @@ import net.minecraftforge.fluids.FluidStack;
 
 public class TileEntityMetalBarrel extends TileEntityWoodenBarrel
 {
+    @Override
+	public void update() {
+        if (world.isRemote || world.isBlockIndirectlyGettingPowered(getPos())>0) return;
+        else super.update();
+    }
 	@Override
 	public boolean isFluidValid(FluidStack fluid)
 	{
