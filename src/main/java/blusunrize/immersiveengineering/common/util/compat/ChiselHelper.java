b80c43222f6d6a9848f167d9e2ef9bd28030ac93
BluSunrize <danny.rad10@yahoo.de>
2017-11-12 16:11:09 +0100

    Made Chisel compat use the correct key, closes #2584

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/ChiselHelper.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/ChiselHelper.java
@@ -20,15 +20,15 @@ public class ChiselHelper extends IECompatModule
 	@Override
 	public void init()
 	{
-		FMLInterModComms.sendMessage("chisel", "variation:add", "treated_wood|ImmersiveEngineering:treatedWood|0");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "treated_wood|ImmersiveEngineering:treatedWood|1");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "treated_wood|ImmersiveEngineering:treatedWood|2");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "steel_scaffold|ImmersiveEngineering:metalDecoration1|1");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "steel_scaffold|ImmersiveEngineering:metalDecoration1|2");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "steel_scaffold|ImmersiveEngineering:metalDecoration1|3");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|5");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|6");
-		FMLInterModComms.sendMessage("chisel", "variation:add", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|7");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "treated_wood|ImmersiveEngineering:treatedWood|0");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "treated_wood|ImmersiveEngineering:treatedWood|1");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "treated_wood|ImmersiveEngineering:treatedWood|2");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "steel_scaffold|ImmersiveEngineering:metalDecoration1|1");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "steel_scaffold|ImmersiveEngineering:metalDecoration1|2");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "steel_scaffold|ImmersiveEngineering:metalDecoration1|3");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|5");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|6");
+		FMLInterModComms.sendMessage("chisel", "add_variation", "aluminum_scaffold|ImmersiveEngineering:metalDecoration1|7");
 	}
 
 	@Override
