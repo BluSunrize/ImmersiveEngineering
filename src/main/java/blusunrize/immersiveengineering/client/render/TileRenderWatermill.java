aaf6d61ac678d684db83a6d83c919275fe4ca20f
malte0811 <malte0811@web.de>
2017-11-13 16:48:40 +0100

    Fix waterwheel rotation, closes #2610

--- src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWatermill.java
+++ src/main/java/blusunrize/immersiveengineering/client/render/TileRenderWatermill.java
@@ -8,6 +8,7 @@
 
 package blusunrize.immersiveengineering.client.render;
 
+import blusunrize.immersiveengineering.api.IEProperties;
 import blusunrize.immersiveengineering.client.ClientUtils;
 import blusunrize.immersiveengineering.common.blocks.wooden.TileEntityWatermill;
 import net.minecraft.block.state.IBlockState;
@@ -18,7 +19,6 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumFacing.AxisDirection;
 import org.lwjgl.opengl.GL11;
 
 import java.util.List;
@@ -35,23 +35,20 @@ public class TileRenderWatermill extends TileEntitySpecialRenderer<TileEntityWat
 		{
 			final BlockRendererDispatcher blockRenderer = Minecraft.getMinecraft().getBlockRendererDispatcher();
 			IBlockState state = tile.getWorld().getBlockState(tile.getPos());
+			state = state.withProperty(IEProperties.FACING_ALL, EnumFacing.NORTH);
 			quads = blockRenderer.getModelForState(state).getQuads(state, null, 0);
 		}
 		Tessellator tessellator = Tessellator.getInstance();
 		GlStateManager.pushMatrix();
 
 		GlStateManager.translate(x + .5, y + .5, z + .5);
-		GlStateManager.rotate(90, 1, 0, 0);
 		GlStateManager.blendFunc(770, 771);
 		GlStateManager.enableBlend();
 		GlStateManager.disableCull();
-		final float dir = tile.facing == EnumFacing.NORTH ? 180 : tile.facing == EnumFacing.SOUTH ? 0 : tile.facing == EnumFacing.WEST ? 90 : -90;
+		final float dir = (tile.facing.getHorizontalAngle()+180)%180;
 		float wheelRotation = 360 * (tile.rotation + (!tile.canTurn || tile.rotation == 0 ? 0 : partialTicks)*(float)tile.perTick);
-		if(tile.facing.getAxisDirection() == AxisDirection.NEGATIVE)
-			wheelRotation *= -1;
-		final float rot = wheelRotation;
-		GlStateManager.rotate(dir, 0, 0, 1);
-		GlStateManager.rotate(rot, 0, 1, 0);
+		GlStateManager.rotate(dir, 0, 1, 0);
+		GlStateManager.rotate(wheelRotation, 0, 0, 1);
 		RenderHelper.disableStandardItemLighting();
 		Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 		BufferBuilder worldRenderer = tessellator.getBuffer();
