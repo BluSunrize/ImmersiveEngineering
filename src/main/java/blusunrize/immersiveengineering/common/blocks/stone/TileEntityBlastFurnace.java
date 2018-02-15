BluSunrize/ImmersiveEngineering

commit 5e078d5be908dd512dc7045e4469b91bd5426d58

Changed Alloy Kiln and Blastfurnace to decrement their burntimers even when not in use, made them render active through then too, closes #2457

src/main/java/blusunrize/immersiveengineering/common/blocks/stone/TileEntityBlastFurnace.java
@@ -91,9 +91,10 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 
 			if(burnTime>0)
 			{
+				int processSpeed = 1;
 				if(process>0)
 				{
-					int processSpeed = getProcessSpeed();
+					processSpeed = getProcessSpeed();
 					if(inventory.get(0).isEmpty())
 					{
 						process=0;
@@ -115,13 +116,13 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 								active = true;
 						}
 					}
-					burnTime-=processSpeed;
 					markContainingBlockForUpdate(null);
 				}
+				burnTime-=processSpeed;
 
 				if(process<=0)
 				{
-					if(active)
+					if(processMax>0)
 					{
 						BlastFurnaceRecipe recipe = getRecipe();
 						if(recipe!=null)
@@ -140,7 +141,6 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 							}
 						}
 						processMax=0;
-						active=false;
 						burnTime -= process;
 					}
 					BlastFurnaceRecipe recipe = getRecipe();

src/main/java/blusunrize/immersiveengineering/common/blocks/stone/TileEntityAlloySmelter.java
@@ -114,11 +114,13 @@ public class TileEntityAlloySmelter extends TileEntityMultiblockPart<TileEntityA
 								active = true;
 						}
 					}
+					markContainingBlockForUpdate(null);
 				}
+				burnTime--;
 
 				if(process<=0)
 				{
-					if(active)
+					if(processMax>0)
 					{
 						AlloyRecipe recipe = getRecipe();
 						if(recipe!=null)
@@ -133,7 +135,6 @@ public class TileEntityAlloySmelter extends TileEntityMultiblockPart<TileEntityA
 								inventory.set(3, recipe.output.copy());
 						}
 						processMax=0;
-						active=false;
 					}
 					AlloyRecipe recipe = getRecipe();
 					if(recipe!=null)
@@ -143,8 +144,6 @@ public class TileEntityAlloySmelter extends TileEntityMultiblockPart<TileEntityA
 						this.active=true;
 					}
 				}
-				burnTime--;
-				markContainingBlockForUpdate(null);
 			}
 			else
 			{

commit 04828fee67aaff733407896ebb73b3c81c66ad76

Fixed blast furnaces using slightly more than 1 coke per steel, re-fixes #2021 (probably broken by 5e078d5be908dd512dc7045e4469b91bd5426d58)

src/main/java/blusunrize/immersiveengineering/common/blocks/stone/TileEntityBlastFurnace.java
@@ -103,8 +103,10 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 			{
 				int processSpeed = 1;
 				if(process>0)
-				{
 					processSpeed = getProcessSpeed();
+				burnTime-=processSpeed;
+				if(process>0)
+				{
 					if(inventory.get(0).isEmpty())
 					{
 						process=0;
@@ -122,13 +124,13 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 						else
 						{
 							process -= processSpeed;
+							processSpeed = 0;//Process speed is "used up"
 							if (!active)
 								active = true;
 						}
 					}
 					markContainingBlockForUpdate(null);
 				}
-				burnTime-=processSpeed;
 
 				if(process<=0)
 				{
@@ -156,7 +158,7 @@ public class TileEntityBlastFurnace extends TileEntityMultiblockPart<TileEntityB
 					BlastFurnaceRecipe recipe = getRecipe();
 					if(recipe!=null)
 					{
-						this.process=recipe.time;
+						this.process=recipe.time - processSpeed;
 						this.processMax=process;
 						this.active=true;
 					}