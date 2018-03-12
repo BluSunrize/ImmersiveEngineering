5b8acf0bbdea8b5e51701f255b10d20179192351
malte0811 <malte0811@web.de>
2018-03-12 17:11:19 +0100

    Fix the original cause of #2857 (windmills ending up facing down)

--- src/main/java/blusunrize/immersiveengineering/common/blocks/IEBlockInterfaces.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/IEBlockInterfaces.java
@@ -155,7 +155,7 @@ public class IEBlockInterfaces
 				}
 			}
 			else if(limit == 6)
-				f = side.getAxis()!=Axis.Y? side.getOpposite(): EnumFacing.getDirectionFromEntityLiving(pos, placer).getOpposite();
+				f = side.getAxis()!=Axis.Y? side.getOpposite():placer.getHorizontalFacing();
 
 			return mirrorFacingOnPlacement(placer)?f.getOpposite():f;
 		}
