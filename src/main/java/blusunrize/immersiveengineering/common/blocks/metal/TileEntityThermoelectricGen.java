26fcb6f8aa988ed5c023a949d8f3fc221f46ec7e
BluSunrize <danny.rad10@yahoo.de>
2018-04-22 12:19:34 +0200

    Changed Thermoelectric Gen to cahche its output and recalculate only on block updates or every 1024 ticks, fixes #2930

--- src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityThermoelectricGen.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/metal/TileEntityThermoelectricGen.java
@@ -11,6 +11,7 @@ package blusunrize.immersiveengineering.common.blocks.metal;
 import blusunrize.immersiveengineering.api.IEEnums.SideConfig;
 import blusunrize.immersiveengineering.api.energy.ThermoelectricHandler;
 import blusunrize.immersiveengineering.common.Config.IEConfig;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.INeighbourChangeTile;
 import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
 import blusunrize.immersiveengineering.common.util.EnergyHelper;
 import blusunrize.immersiveengineering.common.util.EnergyHelper.IEForgeEnergyWrapper;
@@ -33,38 +34,50 @@ import net.minecraftforge.fluids.IFluidBlock;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class TileEntityThermoelectricGen extends TileEntityIEBase implements ITickable, IIEInternalFluxConnector
+public class TileEntityThermoelectricGen extends TileEntityIEBase implements ITickable, INeighbourChangeTile, IIEInternalFluxConnector
 {
+	private int energyOutput = -1;
+
 	@Override
 	public void update()
 	{
-		if(!world.isRemote)
-		{
-			int energy = 0;
-			for(EnumFacing fd : new EnumFacing[]{EnumFacing.DOWN,EnumFacing.NORTH,EnumFacing.WEST})
-				if(!world.isAirBlock(getPos().offset(fd)) && !world.isAirBlock(getPos().offset(fd.getOpposite())))
-				{
-					int temp0 = getTemperature(getPos().offset(fd));
-					int temp1 = getTemperature(getPos().offset(fd.getOpposite()));
-					if(temp0>-1&&temp1>-1)
-					{
-						int diff = Math.abs(temp0-temp1);
-						energy += (int) (Math.sqrt(diff)/2 * IEConfig.Machines.thermoelectric_output);
-					}
-				}
-			outputEnergy(energy);
-		}
+		if(world.getTotalWorldTime()%1024==((getPos().getX()^getPos().getZ())&1023))
+			recalculateEnergyOutput();
+		if(!world.isRemote&&this.energyOutput > 0)
+			outputEnergy(this.energyOutput);
 	}
 
 	public void outputEnergy(int amount)
 	{
-		for (EnumFacing fd : EnumFacing.VALUES)
+		for(EnumFacing fd : EnumFacing.VALUES)
 		{
 			TileEntity te = Utils.getExistingTileEntity(world, getPos().offset(fd));
 			amount -= EnergyHelper.insertFlux(te, fd.getOpposite(), amount, false);
 		}
 	}
 
+	@Override
+	public void onNeighborBlockChange(BlockPos pos)
+	{
+		recalculateEnergyOutput();
+	}
+
+	private void recalculateEnergyOutput()
+	{
+		int energy = 0;
+		for(EnumFacing fd : new EnumFacing[]{EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.WEST})
+			if(!world.isAirBlock(getPos().offset(fd))&&!world.isAirBlock(getPos().offset(fd.getOpposite())))
+			{
+				int temp0 = getTemperature(getPos().offset(fd));
+				int temp1 = getTemperature(getPos().offset(fd.getOpposite()));
+				if(temp0 > -1&&temp1 > -1)
+				{
+					int diff = Math.abs(temp0-temp1);
+					energy += (int)(Math.sqrt(diff)/2*IEConfig.Machines.thermoelectric_output);
+				}
+			}
+		this.energyOutput = energy==0?-1: energy;
+	}
 
 	int getTemperature(BlockPos pos)
 	{
@@ -74,19 +87,20 @@ public class TileEntityThermoelectricGen extends TileEntityIEBase implements ITi
 		IBlockState state = world.getBlockState(pos);
 		return ThermoelectricHandler.getTemperature(state.getBlock(), state.getBlock().getMetaFromState(state));
 	}
+
 	Fluid getFluid(BlockPos pos)
 	{
 		IBlockState state = world.getBlockState(pos);
 		Block b = state.getBlock();
 		Fluid f = FluidRegistry.lookupFluidForBlock(b);
-		if(f==null && b instanceof BlockDynamicLiquid && b.getMetaFromState(state)==0)
+		if(f==null&&b instanceof BlockDynamicLiquid&&b.getMetaFromState(state)==0)
 			if(state.getMaterial().equals(Material.WATER))
 				f = FluidRegistry.WATER;
 			else if(state.getMaterial().equals(Material.LAVA))
 				f = FluidRegistry.LAVA;
-		if(b instanceof IFluidBlock && !((IFluidBlock)b).canDrain(world, pos))
+		if(b instanceof IFluidBlock&&!((IFluidBlock)b).canDrain(world, pos))
 			return null;
-		if(b instanceof BlockStaticLiquid && b.getMetaFromState(state)!=0)
+		if(b instanceof BlockStaticLiquid&&b.getMetaFromState(state)!=0)
 			return null;
 		if(f==null)
 			return null;
@@ -96,10 +110,13 @@ public class TileEntityThermoelectricGen extends TileEntityIEBase implements ITi
 	@Override
 	public void readCustomNBT(NBTTagCompound nbt, boolean descPacket)
 	{
+		this.energyOutput = nbt.getInteger("enegyOutput");
 	}
+
 	@Override
 	public void writeCustomNBT(NBTTagCompound nbt, boolean descPacket)
 	{
+		nbt.setInteger("enegyOutput", this.energyOutput);
 	}
 
 
@@ -109,12 +126,15 @@ public class TileEntityThermoelectricGen extends TileEntityIEBase implements ITi
 	{
 		return SideConfig.OUTPUT;
 	}
+
 	@Override
 	public boolean canConnectEnergy(EnumFacing from)
 	{
 		return true;
 	}
-	IEForgeEnergyWrapper wrapper = new IEForgeEnergyWrapper(this,null);
+
+	IEForgeEnergyWrapper wrapper = new IEForgeEnergyWrapper(this, null);
+
 	@Override
 	public IEForgeEnergyWrapper getCapabilityWrapper(EnumFacing facing)
 	{
