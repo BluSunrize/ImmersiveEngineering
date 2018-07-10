74f347881a44bf94e4bd9743eddba68126eea729
Shadows_of_Fire <Bward7864@gmail.com>
2018-07-10 02:23:17 -0400

    Fix AD2 support

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/IECompatModule.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/IECompatModule.java
@@ -37,7 +37,7 @@ public abstract class IECompatModule
 	{
 		moduleClasses.put("actuallyadditions", ActuallyAdditionsHelper.class);
 		moduleClasses.put("albedo", AlbedoHelper.class);
-		moduleClasses.put("attaineddrops", AttainedDropsHelper.class);
+		moduleClasses.put("attaineddrops2", AttainedDropsHelper.class);
 		moduleClasses.put("baubles", BaublesHelper.class);
 		moduleClasses.put("betterwithmods", BetterWithModsHelper.class);
 		moduleClasses.put("bloodmagic", BloodMagicHelper.class);
