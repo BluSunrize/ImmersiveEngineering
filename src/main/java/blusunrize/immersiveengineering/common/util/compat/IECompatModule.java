1115143c234f6eefc89166f895608bed4fca50c3
BluSunrize <danny.rad10@yahoo.de>
2018-08-27 00:13:41 +0200

    Adjusted changed modid for IC2Classic, fixes #3187

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/IECompatModule.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/IECompatModule.java
@@ -83,7 +83,7 @@ public abstract class IECompatModule
 				try
 				{
 					//IC2 Classic is not supported.
-					if("IC2".equals(e.getKey())&&Loader.isModLoaded("IC2-Classic-Spmod"))
+					if("ic2".equals(e.getKey())&&Loader.isModLoaded("ic2-classic-spmod"))
 						continue;
 
 					Boolean enabled = Config.IEConfig.compat.get(e.getKey());
