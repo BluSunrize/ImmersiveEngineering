53a34315979d9c51114f6a2df466eeb4af9a2556
malte0811 <malte0811@web.de>
2018-09-05 09:45:09 +0200

    Cleaner/more stable fix for #3121 (upgrades vanishing in the workbench, probably only in singleplayer) This appears to be caused by item stacks using the same NBTTagCompound instance on the integrated server and client, so modifying the tag on the client and then replacing it with a different one causes issues.

--- src/main/java/blusunrize/immersiveengineering/common/items/ItemUpgradeableTool.java
+++ src/main/java/blusunrize/immersiveengineering/common/items/ItemUpgradeableTool.java
@@ -16,6 +16,7 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.oredict.OreDictionary;
@@ -45,9 +46,7 @@ public abstract class ItemUpgradeableTool extends ItemInternalStorage implements
 	@Override
 	public void clearUpgrades(ItemStack stack)
 	{
-		NBTTagCompound nbt = stack.getTagCompound();
-		if(nbt!=null)
-			nbt.removeTag("upgrades");
+		ItemNBTHelper.remove(stack, "upgrades");
 	}
 
 	@Override
@@ -58,6 +57,8 @@ public abstract class ItemUpgradeableTool extends ItemInternalStorage implements
 	@Override
 	public void recalculateUpgrades(ItemStack stack)
 	{
+		if(FMLCommonHandler.instance().getEffectiveSide().isClient())
+			return;
 		clearUpgrades(stack);
 		IItemHandler inv = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
 		NBTTagCompound upgradeTag = getUpgradeBase(stack).copy();
