959ef65517726264839b163616ed6d1b79a1a8e9
malte0811 <malte0811@web.de>
2018-09-07 12:22:58 +0200

    Moved connector models to JSON

--- src/main/java/blusunrize/immersiveengineering/client/models/smart/ConnLoader.java
+++ src/main/java/blusunrize/immersiveengineering/client/models/smart/ConnLoader.java
@@ -9,9 +9,12 @@
 package blusunrize.immersiveengineering.client.models.smart;
 
 import blusunrize.immersiveengineering.ImmersiveEngineering;
-import blusunrize.immersiveengineering.api.energy.wires.WireApi;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
@@ -20,82 +23,21 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.client.model.obj.OBJModel;
 import net.minecraftforge.common.model.IModelState;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.function.Function;
 
 public class ConnLoader implements ICustomModelLoader
 {
 	public static final String RESOURCE_LOCATION = "models/block/smartmodel/conn_";
+	public static final ResourceLocation DATA_BASED_LOC = new ResourceLocation(ImmersiveEngineering.MODID, "models/block/smartmodel/connector");
 	//Do not manually write to these in IE 0.12-77+. Use blusunrize.immersiveengineering.api.energy.wires.WireApi.registerConnectorForRender()
 	public static Map<String, ImmutableMap<String, String>> textureReplacements = new HashMap<>();
 	public static Map<String, ResourceLocation> baseModels = new HashMap<>();
-
-	static
-	{
-		WireApi.registerConnectorForRender("conn_lv", new ResourceLocation("immersiveengineering:block/connector/connector_lv.obj"), null);
-		WireApi.registerConnectorForRender("rel_lv", new ResourceLocation("immersiveengineering:block/connector/connector_lv.obj"),
-				ImmutableMap.of("#immersiveengineering:blocks/connector_connector_lv",
-						"immersiveengineering:blocks/connector_relay_lv"));
-
-		WireApi.registerConnectorForRender("conn_mv", new ResourceLocation("immersiveengineering:block/connector/connector_mv.obj"), null);
-		WireApi.registerConnectorForRender("rel_mv", new ResourceLocation("immersiveengineering:block/connector/connector_mv.obj"),
-				ImmutableMap.of("#immersiveengineering:blocks/connector_connector_mv",
-						"immersiveengineering:blocks/connector_relay_mv"));
-
-		WireApi.registerConnectorForRender("conn_hv", new ResourceLocation("immersiveengineering:block/connector/connector_hv.obj"), null);
-		WireApi.registerConnectorForRender("rel_hv", new ResourceLocation("immersiveengineering:block/connector/relay_hv.obj"), null);
-
-		WireApi.registerConnectorForRender("conn_struct",
-				new ResourceLocation("immersiveengineering:block/connector/connector_structural.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("conn_redstone", new ResourceLocation("immersiveengineering:block/connector/connector_redstone.obj.ie"), null);
-		WireApi.registerConnectorForRender("conn_probe", new ResourceLocation("immersiveengineering:block/connector/connector_probe.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("breaker_off",
-				new ResourceLocation("immersiveengineering:block/connector/breaker_switch_off.obj.ie"), null);
-		WireApi.registerConnectorForRender("breaker_on", new ResourceLocation("immersiveengineering:block/connector/breaker_switch_on.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("e_meter", new ResourceLocation("immersiveengineering:block/connector/e_meter.obj"), null);
-		WireApi.registerConnectorForRender("redstone_breaker",
-				new ResourceLocation("immersiveengineering:block/connector/redstone_breaker.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("transformer_hv_left",
-				new ResourceLocation("immersiveengineering:block/connector/transformer_hv_left.obj"), null);
-		WireApi.registerConnectorForRender("transformer_hv_right",
-				new ResourceLocation("immersiveengineering:block/connector/transformer_hv_right.obj"), null);
-		WireApi.registerConnectorForRender("transformer_mv_left",
-				new ResourceLocation("immersiveengineering:block/connector/transformer_mv_left.obj"), null);
-		WireApi.registerConnectorForRender("transformer_mv_right",
-				new ResourceLocation("immersiveengineering:block/connector/transformer_mv_right.obj"), null);
-		WireApi.registerConnectorForRender("transformer_mv_post",
-				new ResourceLocation("immersiveengineering:block/connector/transformer_post.obj"), null);
-
-		WireApi.registerConnectorForRender("e_lantern",
-				new ResourceLocation("immersiveengineering:block/metal_device/e_lantern.obj"), null);
-		WireApi.registerConnectorForRender("e_lantern_on",
-				new ResourceLocation("immersiveengineering:block/metal_device/e_lantern.obj"),
-				ImmutableMap.of("#immersiveengineering:blocks/metal_device1_electric_lantern",
-						"immersiveengineering:blocks/metal_device1_electric_lantern_on"));
-		WireApi.registerConnectorForRender("floodlight",
-				new ResourceLocation("immersiveengineering:block/metal_device/floodlight.obj.ie"), null);
-		WireApi.registerConnectorForRender("floodlight_on",
-				new ResourceLocation("immersiveengineering:block/metal_device/floodlight.obj.ie"),
-				ImmutableMap.of("#immersiveengineering:blocks/metal_device1_floodlight",
-						"immersiveengineering:blocks/metal_device1_floodlight_on"));
-
-		WireApi.registerConnectorForRender("balloon",
-				new ResourceLocation("immersiveengineering:block/balloon.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("razor_wire",
-				new ResourceLocation("immersiveengineering:block/razor_wire.obj.ie"), null);
-
-		WireApi.registerConnectorForRender("feedthrough",
-				new ResourceLocation("immersiveengineering:block/smartmodel/feedthrough"), null);
-	}
 
 	@Override
 	public void onResourceManagerReload(@Nonnull IResourceManager resourceManager)
@@ -106,13 +48,15 @@ public class ConnLoader implements ICustomModelLoader
 	@Override
 	public boolean accepts(@Nonnull ResourceLocation modelLocation)
 	{
-		return modelLocation.getResourcePath().contains(RESOURCE_LOCATION);
+		return modelLocation.equals(DATA_BASED_LOC)||modelLocation.getResourcePath().contains(RESOURCE_LOCATION);
 	}
 
 	@Nonnull
 	@Override
 	public IModel loadModel(@Nonnull ResourceLocation modelLocation)
 	{
+		if(modelLocation.equals(DATA_BASED_LOC))
+			return new ConnModelBase(null);
 		String resourcePath = modelLocation.getResourcePath();
 		int pos = resourcePath.indexOf("conn_");
 		if(pos >= 0)
@@ -123,7 +67,7 @@ public class ConnLoader implements ICustomModelLoader
 			if(r!=null)
 			{
 				if(textureReplacements.containsKey(name))
-					return new ConnModelBase(r, textureReplacements.get(name));
+					return new ConnModelBase(r, textureReplacements.get(name), ImmutableMap.of());
 				else
 					return new ConnModelBase(r);
 			}
@@ -131,65 +75,139 @@ public class ConnLoader implements ICustomModelLoader
 		return ModelLoaderRegistry.getMissingModel();
 	}
 
-	private class ConnModelBase implements IModel
+	private static class ConnModelBase implements IModel
 	{
-		ResourceLocation base;
-		ImmutableMap<String, String> texReplace;
+		private static final ResourceLocation WIRE_LOC = new ResourceLocation(ImmersiveEngineering.MODID.toLowerCase(Locale.ENGLISH)+":blocks/wire");
+		@Nullable
+		final ResourceLocation base;
+		@Nonnull
+		final ImmutableMap<String, String> texReplace;
+		@Nonnull
+		final ImmutableMap<String, String> customBase;
+		@Nullable
+		private IModel baseModel;
 
-		public ConnModelBase(ResourceLocation b, ImmutableMap<String, String> t)
+		public ConnModelBase(@Nullable ResourceLocation b, @Nonnull ImmutableMap<String, String> t,
+							 @Nonnull ImmutableMap<String, String> customBase)
 		{
 			base = b;
 			texReplace = t;
+			this.customBase = customBase;
 		}
 
-		public ConnModelBase(ResourceLocation b)
+		public ConnModelBase(@Nullable ResourceLocation b)
 		{
-			this(b, ImmutableMap.of());
+			this(b, ImmutableMap.of(), ImmutableMap.of());
 		}
 
 		@Nonnull
 		@Override
 		public Collection<ResourceLocation> getDependencies()
 		{
-			return ImmutableList.of(base);
+			if(base==null)
+				return ImmutableList.of();
+			attemptToLoadBase(false);
+			if(baseModel!=null)
+			{
+				List<ResourceLocation> ret = new ArrayList<>(baseModel.getDependencies());
+				ret.add(0, base);
+				return ret;
+			}
+			else
+				return ImmutableList.of(base);
+		}
+
+		private void attemptToLoadBase(boolean throwError)
+		{
+			try
+			{
+				assert base!=null;
+				baseModel = ModelLoaderRegistry.getModel(base);
+				baseModel = baseModel.retexture(texReplace).process(customBase);
+			} catch(Exception e)
+			{
+				if(throwError)
+					throw new RuntimeException(e);
+			}
 		}
 
 		@Nonnull
 		@Override
 		public Collection<ResourceLocation> getTextures()
 		{
-			try
+			if(base==null)
+				return ImmutableList.of();
+			attemptToLoadBase(false);
+			if(baseModel!=null)
 			{
-				List<ResourceLocation> ret = new ArrayList<>(ModelLoaderRegistry.getModel(base).getTextures());
-				for(String tex : texReplace.values())
-					ret.add(new ResourceLocation(tex));
-				ret.add(new ResourceLocation(ImmersiveEngineering.MODID.toLowerCase(Locale.ENGLISH)+":blocks/wire"));
+				List<ResourceLocation> ret = new ArrayList<>(baseModel.getTextures());
+				ret.add(WIRE_LOC);
 				return ret;
-			} catch(Exception e)
-			{
-				throw new RuntimeException(e);
 			}
+			else
+				return ImmutableList.of(WIRE_LOC);
 		}
 
 		@Nonnull
 		@Override
 		public IBakedModel bake(@Nonnull IModelState state, @Nonnull VertexFormat format, @Nonnull Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter)
 		{
-			try
-			{
-				IModel model = ModelLoaderRegistry.getModel(base);
-				if(model instanceof OBJModel)
-				{
-					model = model.retexture(texReplace);
-					OBJModel obj = (OBJModel)model;
-					model = obj.process(ImmutableMap.of("flip-v", "true"));
-				}
-				return new ConnModelReal(model.bake(state, format, bakedTextureGetter));
-			} catch(Exception e)
-			{
-				throw new RuntimeException(e);
-			}
+			attemptToLoadBase(true);
+			assert baseModel!=null;
+			return new ConnModelReal(baseModel.bake(state, format, bakedTextureGetter));
 		}
 
+		private static final ImmutableSet<String> ownKeys = ImmutableSet.of("base", "custom", "textures");
+
+		@Nonnull
+		@Override
+		public IModel process(ImmutableMap<String, String> customData)
+		{
+			String baseLocStr = customData.get("base");
+			ResourceLocation baseLoc = this.base;
+			if(baseLocStr!=null)
+				baseLoc = new ResourceLocation(asString(baseLocStr));
+			ImmutableMap<String, String> customBase = ImmutableMap.of();
+			ImmutableMap<String, String> texReplacements = ImmutableMap.of();
+			if(customData.containsKey("custom"))
+			{
+				JsonObject obj = new JsonParser().parse(customData.get("custom")).getAsJsonObject();
+				customBase = asMap(obj);
+			}
+			ImmutableMap.Builder<String, String> leftoverKeys = ImmutableMap.builder();
+			for(Entry<String, String> e : customData.entrySet())
+				if(!ownKeys.contains(e.getKey()))
+					leftoverKeys.put(e);
+			leftoverKeys.putAll(customBase);
+			customBase = leftoverKeys.build();
+			if(customData.containsKey("textures"))
+			{
+				JsonObject obj = new JsonParser().parse(customData.get("textures")).getAsJsonObject();
+				texReplacements = asMap(obj);
+			}
+			if(!(baseLoc==base||baseLoc.equals(base))||!customBase.equals(this.customBase)
+					||!texReplacements.equals(this.texReplace))
+				return new ConnModelBase(baseLoc, texReplacements, customBase);
+			return this;
+		}
+
+		private ImmutableMap<String, String> asMap(JsonObject obj)
+		{
+			ImmutableMap.Builder<String, String> b = ImmutableMap.builder();
+			for(Entry<String, JsonElement> e : obj.entrySet())
+			{
+				JsonElement val = e.getValue();
+				if(val.isJsonPrimitive()&&val.getAsJsonPrimitive().isString())
+					b.put(e.getKey(), val.getAsString());
+				else
+					b.put(e.getKey(), val.toString());
+			}
+			return b.build();
+		}
+
+		private String asString(String json)
+		{
+			return new JsonParser().parse(json).getAsString();
+		}
 	}
 }
