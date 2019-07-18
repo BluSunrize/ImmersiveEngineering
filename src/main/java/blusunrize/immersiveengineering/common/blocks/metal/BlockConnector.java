ed63f6fba3fa4728ab3576515f1ed60343611e4c
MalkContent <patrick-riedel@gmx.net>
2019-07-18 11:55:17 +0200

    Wirepickpatch (#3389)
    * initial commit of extended ability to pick wires from connectors
    
    * removed strange new unused imports
    
    * made instanceof checks in wire picking routine consistent
    
    * removed 2 empty lines
    
    * made wirelist rigid and cycling reliable with multiple wire items
    
    * bug fix and code simplification

--- src/main/java/blusunrize/immersiveengineering/common/blocks/metal/BlockConnector.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/metal/BlockConnector.java
@@ -11,6 +11,8 @@ package blusunrize.immersiveengineering.common.blocks.metal;
 import blusunrize.immersiveengineering.api.IEProperties;
 import blusunrize.immersiveengineering.api.IPostBlock;
 import blusunrize.immersiveengineering.api.TargetingInfo;
+import blusunrize.immersiveengineering.api.energy.wires.IImmersiveConnectable;
+import blusunrize.immersiveengineering.api.energy.wires.IWireCoil;
 import blusunrize.immersiveengineering.api.energy.wires.TileEntityImmersiveConnectable;
 import blusunrize.immersiveengineering.api.energy.wires.WireType;
 import blusunrize.immersiveengineering.client.models.IOBJModelCallback;
@@ -25,10 +27,12 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
@@ -37,6 +41,7 @@ import net.minecraftforge.common.property.ExtendedBlockState;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.IUnlistedProperty;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BlockConnector extends BlockIETileProvider<BlockTypes_Connector>
@@ -177,21 +182,80 @@ public class BlockConnector extends BlockIETileProvider<BlockTypes_Connector>
 		if(player!=null&&player.isSneaking())
 		{
 			TileEntity te = world.getTileEntity(pos);
-			if(te instanceof TileEntityImmersiveConnectable)
+			if(te instanceof IImmersiveConnectable)
 			{
 				TargetingInfo subTarget = null;
 				if(target.hitVec!=null)
 					subTarget = new TargetingInfo(target.sideHit, (float)target.hitVec.x-pos.getX(), (float)target.hitVec.y-pos.getY(), (float)target.hitVec.z-pos.getZ());
 				else
 					subTarget = new TargetingInfo(target.sideHit, 0, 0, 0);
-				BlockPos masterPos = ((TileEntityImmersiveConnectable)te).getConnectionMaster(null, subTarget);
+				BlockPos masterPos = ((IImmersiveConnectable)te).getConnectionMaster(null, subTarget);
 				if(masterPos!=pos)
 					te = world.getTileEntity(masterPos);
-				if(te instanceof TileEntityImmersiveConnectable)
+				if(te instanceof IImmersiveConnectable)
 				{
-					WireType connected = ((TileEntityImmersiveConnectable)te).getCableLimiter(subTarget);
-					if(connected!=null)
-						return connected.getWireCoil();
+					IImmersiveConnectable connectable = (IImmersiveConnectable)te;
+					WireType wire = connectable.getCableLimiter(subTarget);
+					if(wire!=null)
+						return wire.getWireCoil();
+					ArrayList<ItemStack> applicableWires = new ArrayList<ItemStack>();
+					NonNullList<ItemStack> pInventory = player.inventory.mainInventory;
+					for(int i = 0; i < pInventory.size(); i++)
+					{
+						ItemStack s = pInventory.get(i);
+						if(s.getItem() instanceof IWireCoil)
+						{
+							IWireCoil coilItem = (IWireCoil)s.getItem();
+							wire = coilItem.getWireType(s);
+							if(connectable.canConnectCable(wire, subTarget, pos.subtract(masterPos)) && coilItem.canConnectCable(s, te))
+							{
+								ItemStack coil = wire.getWireCoil();
+								boolean unique = true;
+								int insertIndex = applicableWires.size();
+								for (int j = 0; j < applicableWires.size(); j++)
+								{
+									ItemStack priorWire = applicableWires.get(j);
+									if(coil.getItem() == priorWire.getItem()) //sort same item by metadata
+									{
+										if(coil.getMetadata() == priorWire.getMetadata())
+										{
+											unique = false;
+											break;
+										}
+										if(coil.getMetadata() < priorWire.getMetadata())
+										{
+											insertIndex = j;
+											break;
+										}
+									}
+									/*sort different item by itemID (can't guarantee a static list otherwise. switching items by pickBlock changes the order in which things are looked at,
+									making for scenarios in which applicable wires are possibly skipped when 3 or more wire Items are present)*/
+									else  
+									{
+										int coilID = Item.REGISTRY.getIDForObject(coil.getItem());
+										int priorID = Item.REGISTRY.getIDForObject(priorWire.getItem());
+										if(coilID < priorID)
+										{
+											insertIndex = j;
+											break;
+										}
+									}
+								}
+								if(unique)
+									applicableWires.add(insertIndex, coil);
+							}
+						}
+					}
+					if(applicableWires.size() > 0)
+					{
+						ItemStack heldItem = pInventory.get(player.inventory.currentItem);
+						if(heldItem.getItem() instanceof IWireCoil)
+							//cycle through to the next applicable wire, if currently held wire is already applicable
+							for (int i = 0; i < applicableWires.size(); i++)
+								if(heldItem.isItemEqual(applicableWires.get(i)))
+									return applicableWires.get((i+1) % applicableWires.size()); //wrap around on i+1 >= applicableWires.size()
+						return applicableWires.get(0);
+					}
 				}
 			}
 		}
