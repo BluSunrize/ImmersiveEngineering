9764549151e6f381c6f9dd57ac85bc3198250e03
JStocke12 <jstocke102@gmail.com>
2019-10-04 22:33:35 -0400

    Added Assembler to the manual

--- src/main/java/blusunrize/immersiveengineering/client/ClientProxy.java
+++ src/main/java/blusunrize/immersiveengineering/client/ClientProxy.java
@@ -531,6 +531,7 @@ public class ClientProxy extends CommonProxy
 		ieMan.addEntry(machinesCat, new ResourceLocation(MODID, "tesla_coil"));
 		ieMan.addEntry(machinesCat, new ResourceLocation(MODID, "razor_wire"));
 		ieMan.addEntry(machinesCat, new ResourceLocation(MODID, "turrets"));
+		ieMan.addEntry(machinesCat, new ResourceLocation(MODID, "assembler"));
 		ieMan.addEntry(heavyMachinesCat, new ResourceLocation(MODID, "refinery"));
 		String[][] table = formatToTable_ItemIntHashmap(ThermoelectricHandler.getThermalValuesSorted(true), "K");
 		ManualEntry.ManualEntryBuilder builder = new ManualEntry.ManualEntryBuilder(ManualHelper.getManual());
