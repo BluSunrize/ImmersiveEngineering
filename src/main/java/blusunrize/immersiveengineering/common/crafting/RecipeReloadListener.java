b51203cbdb0aa67aeb701a4071e2012ae1e26c9a
BluSunrize <danny.rad10@yahoo.de>
2020-05-16 23:50:58 +0200

    Fix recipe loading for server & client

--- src/main/java/blusunrize/immersiveengineering/common/crafting/RecipeReloadListener.java
+++ src/main/java/blusunrize/immersiveengineering/common/crafting/RecipeReloadListener.java
@@ -9,60 +9,78 @@
 
 package blusunrize.immersiveengineering.common.crafting;
 
+import blusunrize.immersiveengineering.ImmersiveEngineering;
 import blusunrize.immersiveengineering.api.crafting.*;
 import blusunrize.immersiveengineering.api.tool.ExcavatorHandler;
 import blusunrize.immersiveengineering.api.tool.ExcavatorHandler.MineralMix;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.resources.IResourceManager;
+import net.minecraft.resources.IResourceManagerReloadListener;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RecipesUpdatedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.common.thread.EffectiveSide;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
-import net.minecraftforge.resource.IResourceType;
-import net.minecraftforge.resource.ISelectiveResourceReloadListener;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class RecipeReloadListener implements ISelectiveResourceReloadListener
+public class RecipeReloadListener implements IResourceManagerReloadListener
 {
 	@Override
-	public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate)
+	public void onResourceManagerReload(IResourceManager resourceManager)
 	{
 		if(EffectiveSide.get().isServer())
 		{
-			Collection<IRecipe<?>> recipes = ServerLifecycleHooks.getCurrentServer().getRecipeManager().getRecipes();
-			AlloyRecipe.recipeList = filterRecipes(recipes, AlloyRecipe.class, AlloyRecipe.TYPE);
-			BlastFurnaceRecipe.recipeList = filterRecipes(recipes, BlastFurnaceRecipe.class, BlastFurnaceRecipe.TYPE);
-			BlastFurnaceFuel.blastFuels = filterRecipes(recipes, BlastFurnaceFuel.class, BlastFurnaceFuel.TYPE);
-			CokeOvenRecipe.recipeList = filterRecipes(recipes, CokeOvenRecipe.class, CokeOvenRecipe.TYPE);
-			ClocheRecipe.recipeList = filterRecipes(recipes, ClocheRecipe.class, ClocheRecipe.TYPE);
-
-			// Blueprint & MetalPress recipes are a little more invested
-			List<BlueprintCraftingRecipe> blueprintCraftingRecipes = filterRecipes(recipes, BlueprintCraftingRecipe.class, BlueprintCraftingRecipe.TYPE);
-			BlueprintCraftingRecipe.recipeList.clear();
-			for(BlueprintCraftingRecipe r : blueprintCraftingRecipes)
-				BlueprintCraftingRecipe.recipeList.put(r.blueprintCategory, r);
-			List<MetalPressRecipe> metalPressRecipes = filterRecipes(recipes, MetalPressRecipe.class, MetalPressRecipe.TYPE);
-			MetalPressRecipe.recipeList.clear();
-			for(MetalPressRecipe r : metalPressRecipes)
-				MetalPressRecipe.recipeList.put(r.mold, r);
-
-			ArcFurnaceRecipe.recipeList = filterRecipes(recipes, ArcFurnaceRecipe.class, ArcFurnaceRecipe.TYPE);
-			BottlingMachineRecipe.recipeList = filterRecipes(recipes, BottlingMachineRecipe.class, BottlingMachineRecipe.TYPE);
-			CrusherRecipe.recipeList = filterRecipes(recipes, CrusherRecipe.class, CrusherRecipe.TYPE);
-			FermenterRecipe.recipeList = filterRecipes(recipes, FermenterRecipe.class, FermenterRecipe.TYPE);
-			SqueezerRecipe.recipeList = filterRecipes(recipes, SqueezerRecipe.class, SqueezerRecipe.TYPE);
-			RefineryRecipe.recipeList = filterRecipes(recipes, RefineryRecipe.class, RefineryRecipe.TYPE);
-			MixerRecipe.recipeList = filterRecipes(recipes, MixerRecipe.class, MixerRecipe.TYPE);
-
-			List<MineralMix> mineralMixes = filterRecipes(recipes, MineralMix.class, MineralMix.TYPE);
-			ExcavatorHandler.mineralList.clear();
-			for(MineralMix r : mineralMixes)
-				ExcavatorHandler.mineralList.put(r.getId(), r);
+			buildRecipeLists(ServerLifecycleHooks.getCurrentServer().getRecipeManager());
 		}
+	}
+
+	@SubscribeEvent
+	public void onRecipesUpdated(RecipesUpdatedEvent event)
+	{
+		buildRecipeLists(event.getRecipeManager());
+	}
+
+	static void buildRecipeLists(RecipeManager recipeManager)
+	{
+		Collection<IRecipe<?>> recipes = recipeManager.getRecipes();
+
+		AlloyRecipe.recipeList = filterRecipes(recipes, AlloyRecipe.class, AlloyRecipe.TYPE);
+		BlastFurnaceRecipe.recipeList = filterRecipes(recipes, BlastFurnaceRecipe.class, BlastFurnaceRecipe.TYPE);
+		BlastFurnaceFuel.blastFuels = filterRecipes(recipes, BlastFurnaceFuel.class, BlastFurnaceFuel.TYPE);
+		CokeOvenRecipe.recipeList = filterRecipes(recipes, CokeOvenRecipe.class, CokeOvenRecipe.TYPE);
+		ClocheRecipe.recipeList = filterRecipes(recipes, ClocheRecipe.class, ClocheRecipe.TYPE);
+
+		// Blueprint & MetalPress recipes are a little more invested
+		List<BlueprintCraftingRecipe> blueprintCraftingRecipes = filterRecipes(recipes, BlueprintCraftingRecipe.class, BlueprintCraftingRecipe.TYPE);
+		BlueprintCraftingRecipe.recipeList.clear();
+		for(BlueprintCraftingRecipe r : blueprintCraftingRecipes)
+			BlueprintCraftingRecipe.recipeList.put(r.blueprintCategory, r);
+		List<MetalPressRecipe> metalPressRecipes = filterRecipes(recipes, MetalPressRecipe.class, MetalPressRecipe.TYPE);
+		MetalPressRecipe.recipeList.clear();
+		for(MetalPressRecipe r : metalPressRecipes)
+			MetalPressRecipe.recipeList.put(r.mold, r);
+
+		ArcFurnaceRecipe.recipeList = filterRecipes(recipes, ArcFurnaceRecipe.class, ArcFurnaceRecipe.TYPE);
+		BottlingMachineRecipe.recipeList = filterRecipes(recipes, BottlingMachineRecipe.class, BottlingMachineRecipe.TYPE);
+		CrusherRecipe.recipeList = filterRecipes(recipes, CrusherRecipe.class, CrusherRecipe.TYPE);
+		FermenterRecipe.recipeList = filterRecipes(recipes, FermenterRecipe.class, FermenterRecipe.TYPE);
+		SqueezerRecipe.recipeList = filterRecipes(recipes, SqueezerRecipe.class, SqueezerRecipe.TYPE);
+		RefineryRecipe.recipeList = filterRecipes(recipes, RefineryRecipe.class, RefineryRecipe.TYPE);
+		MixerRecipe.recipeList = filterRecipes(recipes, MixerRecipe.class, MixerRecipe.TYPE);
+
+		List<MineralMix> mineralMixes = filterRecipes(recipes, MineralMix.class, MineralMix.TYPE);
+		ExcavatorHandler.mineralList.clear();
+		for(MineralMix r : mineralMixes)
+			ExcavatorHandler.mineralList.put(r.getId(), r);
+
+		MixerRecipePotion.initPotionRecipes();
 	}
 
 	static <R extends IRecipe<?>> List<R> filterRecipes(Collection<IRecipe<?>> recipes, Class<R> recipeClass, IRecipeType<R> recipeType)
