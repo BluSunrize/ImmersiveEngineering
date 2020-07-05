89f67ab55f8765f075925aa43405ac8dc0c8f914
malte0811 <malte0811@web.de>
2020-07-05 18:09:11 +0200

    Use global loot modifiers for hemp drops from grass, closes #4046

---
+++ src/main/java/blusunrize/immersiveengineering/common/util/loot/GrassDrops.java
@@ -0,0 +1,65 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2020
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+package blusunrize.immersiveengineering.common.util.loot;
+
+import blusunrize.immersiveengineering.api.Lib;
+import blusunrize.immersiveengineering.common.items.IEItems.Misc;
+import com.google.gson.JsonObject;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.conditions.ILootCondition;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+@EventBusSubscriber(modid = Lib.MODID, bus = Bus.MOD)
+public class GrassDrops
+{
+	@SubscribeEvent
+	public static void registerModifiers(RegistryEvent.Register<GlobalLootModifierSerializer<?>> ev)
+	{
+		ev.getRegistry().register(
+				new GrassDropSerializer().setRegistryName(Lib.MODID, "hemp_seed_drops")
+		);
+	}
+
+	public static class GrassDropSerializer extends GlobalLootModifierSerializer<GrassDropModifier>
+	{
+
+		@Override
+		public GrassDropModifier read(ResourceLocation location, JsonObject object, ILootCondition[] ailootcondition)
+		{
+			return new GrassDropModifier(ailootcondition);
+		}
+	}
+
+	private static class GrassDropModifier extends LootModifier
+	{
+
+		protected GrassDropModifier(ILootCondition[] conditionsIn)
+		{
+			super(conditionsIn);
+		}
+
+		@Nonnull
+		@Override
+		protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context)
+		{
+			generatedLoot.add(new ItemStack(Misc.hempSeeds));
+			return generatedLoot;
+		}
+	}
+}
