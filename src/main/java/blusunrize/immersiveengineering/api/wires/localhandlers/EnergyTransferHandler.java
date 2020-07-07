BluSunrize/ImmersiveEngineering

commit d01b3b5d731fd84d95703d4b25154bfbf3878a5f

Move most methods in ApiUtils to smaller, more specific util classes (#4033)
* Move most methods in ApiUtils to smaller, more specific util classes

* Fix compile error from merge

src/main/java/blusunrize/immersiveengineering/common/items/VoltmeterItem.java
@@ -19,25 +19,21 @@ import blusunrize.immersiveengineering.api.wires.IImmersiveConnectable;
 import blusunrize.immersiveengineering.api.wires.LocalWireNetwork;
 import blusunrize.immersiveengineering.api.wires.localhandlers.EnergyTransferHandler;
 import blusunrize.immersiveengineering.api.wires.localhandlers.EnergyTransferHandler.Path;
+import blusunrize.immersiveengineering.api.wires.utils.WirecoilUtils;
+import blusunrize.immersiveengineering.api.wires.utils.WirecoilUtils.WireLink;
 import blusunrize.immersiveengineering.common.util.ChatUtils;
-import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
 import blusunrize.immersiveengineering.common.util.Utils;
-import jdk.nashorn.internal.runtime.regexp.joni.constants.TargetInfo;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.NBTUtil;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import org.apache.http.pool.ConnPool;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -53,12 +49,16 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
 	{
 		super.addInformation(stack, worldIn, tooltip, flagIn);
-		if(ItemNBTHelper.hasKey(stack, "linkingPos"))
+		if(WirecoilUtils.hasWireLink(stack))
 		{
-			CompoundNBT link = stack.getOrCreateTag().getCompound("linkingPos");
-			String dimension = stack.getOrCreateTag().getString("linkingDim");
-			ConnectionPoint cp = new ConnectionPoint(link);
-			tooltip.add(new TranslationTextComponent(Lib.DESC_INFO+"attachedToDim", cp.getX(), cp.getY(), cp.getZ(), dimension));
+			WirecoilUtils.WireLink link = WirecoilUtils.WireLink.readFromItem(stack);
+			tooltip.add(new TranslationTextComponent(
+					Lib.DESC_INFO+"attachedToDim",
+					link.cp.getX(),
+					link.cp.getY(),
+					link.cp.getZ(),
+					link.dimension.toString()
+			));
 		}
 	}
 
@@ -79,8 +79,8 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 		TileEntity tileEntity = world.getTileEntity(pos);
 		if((player==null||!player.isSneaking())&&(tileEntity instanceof IFluxReceiver||tileEntity instanceof IFluxProvider))
 		{
-			int max = 0;
-			int stored = 0;
+			int max;
+			int stored;
 			if(tileEntity instanceof IFluxReceiver)
 			{
 				max = ((IFluxReceiver)tileEntity).getMaxEnergyStored(side);
@@ -101,32 +101,30 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 				return ActionResultType.SUCCESS;
 			TargetingInfo targetingInfo = new TargetingInfo(context);
 			BlockPos masterPos = ((IImmersiveConnectable)tileEntity).getConnectionMaster(null, targetingInfo);
-			Vec3i delta = pos.subtract(masterPos);
+			BlockPos delta = pos.subtract(masterPos);
 			ConnectionPoint cp = ((IImmersiveConnectable)tileEntity).getTargetedPoint(targetingInfo, delta);
 			if(cp==null)
 				return ActionResultType.FAIL;
-			if(!ItemNBTHelper.hasKey(stack, "linkingPos"))
+			if(WirecoilUtils.hasWireLink(stack))
 			{
-				CompoundNBT nbt = stack.getOrCreateTag();
-				nbt.putString("linkingDim", world.getDimension().getType().getRegistryName().toString());
-				nbt.put("linkingPos", cp.createTag());
+				WireLink link = WireLink.create(cp, world, delta, targetingInfo);
+				link.writeToItem(stack);
 			}
 			else
 			{
-				String dim = ItemNBTHelper.getString(stack, "linkingDim");
-				if(dim.equals(world.getDimension().getType().getRegistryName().toString()))
+				WireLink link = WireLink.readFromItem(stack);
+				if(link.dimension.equals(world.getDimension().getType().getRegistryName()))
 				{
-					ConnectionPoint linkCP = new ConnectionPoint(stack.getOrCreateTag().getCompound("linkingPos"));
 					GlobalWireNetwork global = GlobalWireNetwork.getNetwork(world);
 					LocalWireNetwork netHere = global.getNullableLocalNet(cp);
-					LocalWireNetwork netLink = global.getNullableLocalNet(linkCP);
+					LocalWireNetwork netLink = global.getNullableLocalNet(link.cp);
 					if(netHere==netLink&&netHere!=null)
 					{
 						EnergyTransferHandler energyHandler = netHere.getHandler(EnergyTransferHandler.ID,
 								EnergyTransferHandler.class);
 						if(energyHandler!=null)
 						{
-							Path energyPath = energyHandler.getPath(linkCP, cp);
+							Path energyPath = energyHandler.getPath(link.cp, cp);
 							if(energyPath!=null)
 								player.sendMessage(new TranslationTextComponent(
 										Lib.CHAT_INFO+"averageLoss",
@@ -135,8 +133,7 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 						}
 					}
 				}
-				ItemNBTHelper.remove(stack, "linkingPos");
-				ItemNBTHelper.remove(stack, "linkingDim");
+				WirecoilUtils.clearWireLink(stack);
 			}
 			return ActionResultType.SUCCESS;
 		}

src/main/java/blusunrize/immersiveengineering/ImmersiveEngineering.java
@@ -13,6 +13,7 @@ import blusunrize.immersiveengineering.api.IEApi;
 import blusunrize.immersiveengineering.api.crafting.ArcFurnaceRecipe;
 import blusunrize.immersiveengineering.api.crafting.MetalPressRecipe;
 import blusunrize.immersiveengineering.api.shader.ShaderRegistry;
+import blusunrize.immersiveengineering.api.utils.TagUtils;
 import blusunrize.immersiveengineering.api.wires.WireType;
 import blusunrize.immersiveengineering.client.ClientProxy;
 import blusunrize.immersiveengineering.common.*;
@@ -142,12 +143,12 @@ public class ImmersiveEngineering
 				||stack.getItem()==Molds.moldUnpacking
 		);
 		// Blocks, Plates, Rods, Wires, Gears, Scaffoldings, Fences
-		ArcFurnaceRecipe.allowItemForRecycling(stack -> ApiUtils.isPlate(stack)
-				||ApiUtils.isMetalComponent(stack, "rods/")
-				||ApiUtils.isMetalComponent(stack, "wires/")
-				||ApiUtils.isMetalComponent(stack, "gears/")
-				||ApiUtils.isMetalComponent(stack, "scaffoldings/")
-				||ApiUtils.isMetalComponent(stack, "fences/"));
+		ArcFurnaceRecipe.allowItemForRecycling(stack -> TagUtils.isPlate(stack)
+				||TagUtils.isInPrefixedTag(stack, "rods/")
+				||TagUtils.isInPrefixedTag(stack, "wires/")
+				||TagUtils.isInPrefixedTag(stack, "gears/")
+				||TagUtils.isInPrefixedTag(stack, "scaffoldings/")
+				||TagUtils.isInPrefixedTag(stack, "fences/"));
 		// Prevent tools used during crafting to be recycled as components
 		ArcFurnaceRecipe.makeItemInvalidRecyclingOutput(stack -> stack.getItem() instanceof HammerItem
 				||stack.getItem() instanceof WirecutterItem||stack.getItem() instanceof ScrewdriverItem);

src/main/java/blusunrize/immersiveengineering/api/CapabilitySkyhookData.java
@@ -8,6 +8,7 @@
 
 package blusunrize.immersiveengineering.api;
 
+import blusunrize.immersiveengineering.api.utils.CapabilityUtils;
 import blusunrize.immersiveengineering.common.entities.SkylineHookEntity;
 import blusunrize.immersiveengineering.common.util.IELogger;
 import net.minecraft.nbt.INBT;
@@ -73,7 +74,7 @@ public class CapabilitySkyhookData
 	public static class SimpleSkyhookProvider implements ICapabilityProvider
 	{
 		SkyhookUserData data = new SkyhookUserData();
-		LazyOptional<SkyhookUserData> opt = ApiUtils.constantOptional(data);
+		LazyOptional<SkyhookUserData> opt = CapabilityUtils.constantOptional(data);
 
 		@Nonnull
 		@Override

src/main/java/blusunrize/immersiveengineering/api/ComparableItemStack.java
@@ -30,6 +30,18 @@ public class ComparableItemStack
 			copy();
 	}
 
+	public static ComparableItemStack create(ItemStack stack, boolean copy)
+	{
+		return create(stack, copy, stack.hasTag()&&!stack.getOrCreateTag().isEmpty());
+	}
+
+	public static ComparableItemStack create(ItemStack stack, boolean copy, boolean useNbt)
+	{
+		ComparableItemStack comp = new ComparableItemStack(stack, copy);
+		comp.setUseNBT(useNbt);
+		return comp;
+	}
+
 	public void copy()
 	{
 		stack = stack.copy();

src/main/java/blusunrize/immersiveengineering/api/IEApi.java
@@ -8,6 +8,7 @@
 
 package blusunrize.immersiveengineering.api;
 
+import blusunrize.immersiveengineering.api.utils.TagUtils;
 import com.google.common.collect.Lists;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
@@ -74,9 +75,9 @@ public class IEApi
 	{
 		return oreOutputPreference.computeIfAbsent(name, rl ->
 		{
-			if(ApiUtils.isNonemptyItemTag(name))
+			if(TagUtils.isNonemptyItemTag(name))
 				return new ItemStack(getPreferredElementbyMod(ItemTags.getCollection().get(name).getAllElements()));
-			else if(ApiUtils.isNonemptyBlockTag(name))
+			else if(TagUtils.isNonemptyBlockTag(name))
 				return new ItemStack(getPreferredElementbyMod(BlockTags.getCollection().get(name).getAllElements()));
 			else
 				return ItemStack.EMPTY;

src/main/java/blusunrize/immersiveengineering/api/crafting/BlueprintCraftingRecipe.java
@@ -8,8 +8,8 @@
 
 package blusunrize.immersiveengineering.api.crafting;
 
-import blusunrize.immersiveengineering.api.ApiUtils;
 import blusunrize.immersiveengineering.api.Lib;
+import blusunrize.immersiveengineering.api.utils.ItemUtils;
 import blusunrize.immersiveengineering.common.items.IEItems.Misc;
 import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
 import blusunrize.immersiveengineering.common.util.ListUtils;
@@ -152,7 +152,7 @@ public class BlueprintCraftingRecipe extends MultiblockRecipe
 					if(ingr.test(queryStack))
 					{
 						int taken = Math.min(queryStack.getCount(), inputSize);
-						consumed.add(ApiUtils.copyStackWithAmount(queryStack, taken));
+						consumed.add(ItemUtils.copyStackWithAmount(queryStack, taken));
 						if(taken >= queryStack.getCount()&&queryStack.getItem().hasContainerItem(queryStack))
 							query.set(i, queryStack.getItem().getContainerItem(queryStack));
 						else

src/main/java/blusunrize/immersiveengineering/api/crafting/BottlingMachineRecipe.java
@@ -8,8 +8,8 @@
 
 package blusunrize.immersiveengineering.api.crafting;
 
-import blusunrize.immersiveengineering.api.ApiUtils;
 import blusunrize.immersiveengineering.api.Lib;
+import blusunrize.immersiveengineering.api.utils.ItemUtils;
 import blusunrize.immersiveengineering.common.util.ListUtils;
 import com.google.common.collect.Lists;
 import net.minecraft.item.ItemStack;
@@ -60,7 +60,7 @@ public class BottlingMachineRecipe extends MultiblockRecipe
 	{
 		if(!input.isEmpty()&&fluid!=null)
 			for(BottlingMachineRecipe recipe : recipeList.values())
-				if(ApiUtils.stackMatchesObject(input, recipe.input)&&fluid.containsFluid(recipe.fluidInput))
+				if(ItemUtils.stackMatchesObject(input, recipe.input)&&fluid.containsFluid(recipe.fluidInput))
 					return recipe;
 		return null;
 	}

commit faa8960d64776aee94dfa47b28b3bbfa4192bb1a

Fix crash with connections with >=1 loss Fix voltmeter not working (broken since d01b3b5d731fd84d95703d4b25154bfbf3878a5f)

src/main/java/blusunrize/immersiveengineering/api/wires/localhandlers/EnergyTransferHandler.java
@@ -86,9 +86,9 @@ public class EnergyTransferHandler extends LocalNetworkHandler implements IWorld
 	public void update(World w)
 	{
 		transferPower();
-		burnOverloaded(w);
 		transferredLastTick = transferredNextTick;
 		transferredNextTick = new Object2DoubleOpenHashMap<>();
+		burnOverloaded(w);
 	}
 
 	/**
@@ -124,6 +124,9 @@ public class EnergyTransferHandler extends LocalNetworkHandler implements IWorld
 		return sources;
 	}
 
+	/**
+	 * @return shortest (w.r.t. base loss) path from source to sink. null if there is no path with base loss <1
+	 */
 	@Nullable
 	public Path getPath(ConnectionPoint source, ConnectionPoint sink)
 	{
@@ -208,13 +211,16 @@ public class EnergyTransferHandler extends LocalNetworkHandler implements IWorld
 			for(Entry<ConnectionPoint, EnergyConnector> sinkEntry : sinks.entrySet())
 			{
 				Path p = getPath(sourceCp, sinkEntry.getKey());
-				EnergyConnector sink = sinkEntry.getValue();
-				int requested = sink.getRequestedEnergy();
-				if(requested <= 0)
-					continue;
-				double requiredAtSource = Math.min(requested/(1-p.loss), available);
-				maxOut.put(p, requiredAtSource);
-				maxSum += requiredAtSource;
+				if(p!=null)
+				{
+					EnergyConnector sink = sinkEntry.getValue();
+					int requested = sink.getRequestedEnergy();
+					if(requested <= 0)
+						continue;
+					double requiredAtSource = Math.min(requested/(1-p.loss), available);
+					maxOut.put(p, requiredAtSource);
+					maxSum += requiredAtSource;
+				}
 			}
 			double allowedFactor = Math.min(1, available/maxSum);
 			for(Path p : maxOut.keySet())
@@ -250,9 +256,9 @@ public class EnergyTransferHandler extends LocalNetworkHandler implements IWorld
 	private void burnOverloaded(World world)
 	{
 		List<Pair<Connection, Double>> toBurn = new ArrayList<>();
-		for(Connection c : transferredNextTick.keySet())
+		for(Connection c : transferredLastTick.keySet())
 		{
-			double transferred = transferredNextTick.getDouble(c);
+			double transferred = transferredLastTick.getDouble(c);
 			if(c.type instanceof IEnergyWire&&((IEnergyWire)c.type).shouldBurn(c, transferred))
 				toBurn.add(new ImmutablePair<>(c, transferred));
 		}

src/main/java/blusunrize/immersiveengineering/api/wires/localhandlers/WireDamageHandler.java
@@ -73,8 +73,11 @@ public class WireDamageHandler extends LocalNetworkHandler implements ICollision
 			for(Object2IntMap.Entry<ConnectionPoint> entry : available.object2IntEntrySet())
 			{
 				Path path = energyHandler.getPath(entry.getKey(), target);
-				totalAvailable += entry.getIntValue()*(1-path.loss);
-				paths.put(entry.getKey(), path);
+				if(path!=null)
+				{
+					totalAvailable += entry.getIntValue()*(1-path.loss);
+					paths.put(entry.getKey(), path);
+				}
 			}
 			totalAvailable = Math.min(totalAvailable, shockWire.getTransferRate());
 

src/main/java/blusunrize/immersiveengineering/common/items/VoltmeterItem.java
@@ -105,7 +105,7 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 			ConnectionPoint cp = ((IImmersiveConnectable)tileEntity).getTargetedPoint(targetingInfo, delta);
 			if(cp==null)
 				return ActionResultType.FAIL;
-			if(WirecoilUtils.hasWireLink(stack))
+			if(!WirecoilUtils.hasWireLink(stack))
 			{
 				WireLink link = WireLink.create(cp, world, delta, targetingInfo);
 				link.writeToItem(stack);
@@ -125,11 +125,15 @@ public class VoltmeterItem extends IEBaseItem implements ITool
 						if(energyHandler!=null)
 						{
 							Path energyPath = energyHandler.getPath(link.cp, cp);
+							double loss;
 							if(energyPath!=null)
-								player.sendMessage(new TranslationTextComponent(
-										Lib.CHAT_INFO+"averageLoss",
-										Utils.formatDouble(energyPath.loss*100, "###.000")
-								));
+								loss = energyPath.loss;
+							else
+								loss = 1;
+							player.sendMessage(new TranslationTextComponent(
+									Lib.CHAT_INFO+"averageLoss",
+									Utils.formatDouble(loss*100, "###.000")
+							));
 						}
 					}
 				}