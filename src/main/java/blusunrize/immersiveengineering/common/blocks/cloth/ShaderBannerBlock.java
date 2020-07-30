96300bd2076e66ea47e50a1b869388eb56d6e8ce
Spencer Brown <TeamSpen210@users.noreply.github.com>
2020-07-31 07:08:09 +1000

    Fix a crash if wall Shader Banners are placed directly without NBT, and fix collisions (#4137)
    * Remove ShaderBannerTileEntity.wall, fixing a crash if NBT isn't present
    
    * Make shader banner shapes consistent with vanilla
    
    Also move methods to the block, since they don't need the TE's data.
    
    * Document what the block check is doing.

--- src/main/java/blusunrize/immersiveengineering/common/blocks/cloth/ShaderBannerBlock.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/cloth/ShaderBannerBlock.java
@@ -17,6 +17,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.state.IProperty;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nonnull;
@@ -24,6 +28,8 @@ import javax.annotation.Nullable;
 
 public abstract class ShaderBannerBlock extends IETileProviderBlock
 {
+	private static VoxelShape SHAPE = Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 16.0D, 12.0D);
+
 	public ShaderBannerBlock(String name, IProperty... stateProps)
 	{
 		super(name, Block.Properties.create(Material.WOOL).hardnessAndResistance(1.0F).sound(SoundType.CLOTH).doesNotBlockMovement(), (b, p) -> null, stateProps);
@@ -35,6 +41,18 @@ public abstract class ShaderBannerBlock extends IETileProviderBlock
 	public TileEntity createTileEntity(@Nonnull BlockState state, @Nonnull IBlockReader world)
 	{
 		return new ShaderBannerTileEntity();
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context)
+	{
+		return SHAPE;
+	}
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context)
+	{
+		return VoxelShapes.empty();
 	}
 
 	@Nonnull
