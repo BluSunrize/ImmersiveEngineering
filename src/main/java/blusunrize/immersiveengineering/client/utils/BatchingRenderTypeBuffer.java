01c38a6ba62fccfdb575f94c77e2e2439744c5a1
malte0811 <malte0811@web.de>
2020-08-08 15:38:36 +0200

    Fix crash when holding a drill with a damaged head, closes #4179 Fix and improve bottling machine render

---
+++ src/main/java/blusunrize/immersiveengineering/client/utils/BatchingRenderTypeBuffer.java
@@ -0,0 +1,37 @@
+package blusunrize.immersiveengineering.client.utils;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class BatchingRenderTypeBuffer implements IRenderTypeBuffer
+{
+	private final Map<RenderType, CollectingVertexBuilder> builders = new HashMap<>();
+
+	@Nonnull
+	@Override
+	public IVertexBuilder getBuffer(@Nonnull RenderType type)
+	{
+		return builders.computeIfAbsent(type, s -> new CollectingVertexBuilder());
+	}
+
+	public void pipe(IRenderTypeBuffer out)
+	{
+		// Glint uses GL_EQUAL for depth, so it needs to be drawn after everything else
+		Collection<RenderType> delay = ImmutableList.of(RenderType.getGlint(), RenderType.getEntityGlint());
+		for(Entry<RenderType, CollectingVertexBuilder> e : builders.entrySet())
+			if(!delay.contains(e.getKey()))
+				e.getValue().pipeAndClear(out.getBuffer(e.getKey()));
+		for(RenderType rt : delay)
+			if(builders.containsKey(rt))
+				builders.get(rt).pipeAndClear(out.getBuffer(rt));
+		builders.clear();
+	}
+}
--- src/main/java/blusunrize/immersiveengineering/client/utils/InvertingVertexBuffer.java
+++ src/main/java/blusunrize/immersiveengineering/client/utils/InvertingVertexBuffer.java
@@ -1,85 +1,26 @@
 package blusunrize.immersiveengineering.client.utils;
 
-import com.google.common.base.Preconditions;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
-import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-
-public class InvertingVertexBuffer implements IVertexBuilder
+public class InvertingVertexBuffer extends CollectingVertexBuilder
 {
 	private final int verticesPerPrimitive;
 	private final IVertexBuilder baseBuilder;
-	private List<List<Consumer<IVertexBuilder>>> vertices = new ArrayList<>();
-	private List<Consumer<IVertexBuilder>> nextVertex = new ArrayList<>();
 
 	public InvertingVertexBuffer(int verticesPerPrimitive, IVertexBuilder baseBuilder)
 	{
 		this.verticesPerPrimitive = verticesPerPrimitive;
-		this.baseBuilder = Preconditions.checkNotNull(baseBuilder);
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder pos(double x, double y, double z)
-	{
-		nextVertex.add(vb -> vb.pos(x, y, z));
-		return this;
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder color(int red, int green, int blue, int alpha)
-	{
-		nextVertex.add(vb -> vb.color(red, green, blue, alpha));
-		return this;
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder tex(float u, float v)
-	{
-		nextVertex.add(vb -> vb.tex(u, v));
-		return this;
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder overlay(int u, int v)
-	{
-		nextVertex.add(vb -> vb.overlay(u, v));
-		return this;
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder lightmap(int u, int v)
-	{
-		nextVertex.add(vb -> vb.lightmap(u, v));
-		return this;
-	}
-
-	@Nonnull
-	@Override
-	public IVertexBuilder normal(float x, float y, float z)
-	{
-		nextVertex.add(vb -> vb.normal(x, y, z));
-		return this;
+		this.baseBuilder = baseBuilder;
 	}
 
 	@Override
 	public void endVertex()
 	{
-		nextVertex.add(IVertexBuilder::endVertex);
-		vertices.add(nextVertex);
-		nextVertex = new ArrayList<>();
 		if(vertices.size() >= verticesPerPrimitive)
 		{
 			for(int i = vertices.size()-1; i >= 0; --i)
 				vertices.get(i).forEach(c -> c.accept(baseBuilder));
-			vertices = new ArrayList<>();
+			vertices.clear();
 		}
 	}
 }
