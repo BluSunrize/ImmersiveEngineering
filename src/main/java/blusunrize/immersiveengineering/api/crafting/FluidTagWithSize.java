dbac83f14f185948976f5130f74a24e2bed3a33d
BluSunrize <danny.rad10@yahoo.de>
2020-08-17 19:38:06 +0200

    Add support for NBT on sized fluid tags

--- src/main/java/blusunrize/immersiveengineering/api/crafting/FluidTagWithSize.java
+++ src/main/java/blusunrize/immersiveengineering/api/crafting/FluidTagWithSize.java
@@ -12,7 +12,10 @@ package blusunrize.immersiveengineering.api.crafting;
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.Tag;
@@ -30,12 +33,18 @@ public class FluidTagWithSize implements Predicate<FluidStack>
 {
 	protected final Tag<Fluid> fluidTag;
 	protected final int amount;
+	protected final CompoundNBT nbtTag;
 
-
-	public FluidTagWithSize(Tag<Fluid> fluidTag, int count)
+	public FluidTagWithSize(Tag<Fluid> fluidTag, int amount, CompoundNBT nbtTag)
 	{
 		this.fluidTag = fluidTag;
-		this.amount = count;
+		this.amount = amount;
+		this.nbtTag = nbtTag;
+	}
+
+	public FluidTagWithSize(Tag<Fluid> fluidTag, int amount)
+	{
+		this(fluidTag, amount, null);
 	}
 
 	public static FluidTagWithSize deserialize(JsonElement input)
@@ -44,35 +53,48 @@ public class FluidTagWithSize implements Predicate<FluidStack>
 		JsonObject jsonObject = input.getAsJsonObject();
 		ResourceLocation resourcelocation = new ResourceLocation(JSONUtils.getString(jsonObject, "tag"));
 		Tag<Fluid> tag = FluidTags.getCollection().get(resourcelocation);
-		return new FluidTagWithSize(tag, JSONUtils.getInt(jsonObject, "amount"));
+		if(!JSONUtils.hasField(jsonObject, "nbt"))
+			return new FluidTagWithSize(tag, JSONUtils.getInt(jsonObject, "amount"));
+		try
+		{
+			CompoundNBT nbt = JsonToNBT.getTagFromJson(JSONUtils.getString(jsonObject, "nbt"));
+			return new FluidTagWithSize(tag, JSONUtils.getInt(jsonObject, "amount"), nbt);
+		} catch(CommandSyntaxException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static FluidTagWithSize read(PacketBuffer input)
 	{
 		Tag<Fluid> tag = FluidTags.getCollection().get(input.readResourceLocation());
-		return new FluidTagWithSize(tag, input.readInt());
+		int amount = input.readInt();
+		CompoundNBT nbt = input.readBoolean()?input.readCompoundTag(): null;
+		return new FluidTagWithSize(tag, amount, nbt);
 	}
 
 	@Override
 	public boolean test(@Nullable FluidStack fluidStack)
 	{
-		if(fluidStack==null)
-			return false;
-		return fluidTag.contains(fluidStack.getFluid())&&fluidStack.getAmount() >= this.amount;
+		return testIgnoringAmount(fluidStack)&&fluidStack.getAmount() >= this.amount;
 	}
 
 	public boolean testIgnoringAmount(@Nullable FluidStack fluidStack)
 	{
 		if(fluidStack==null)
 			return false;
-		return fluidTag.contains(fluidStack.getFluid());
+		if(!fluidTag.contains(fluidStack.getFluid()))
+			return false;
+		if(this.nbtTag!=null)
+			return fluidStack.hasTag()&&fluidStack.getTag().equals(this.nbtTag);
+		return true;
 	}
 
 	@Nonnull
 	public List<FluidStack> getMatchingFluidStacks()
 	{
 		return this.fluidTag.getAllElements().stream()
-				.map(fluid -> new FluidStack(fluid, FluidTagWithSize.this.amount))
+				.map(fluid -> new FluidStack(fluid, FluidTagWithSize.this.amount, FluidTagWithSize.this.nbtTag))
 				.collect(Collectors.toList());
 	}
 
@@ -82,6 +104,8 @@ public class FluidTagWithSize implements Predicate<FluidStack>
 		JsonObject jsonObject = new JsonObject();
 		jsonObject.addProperty("tag", this.fluidTag.getId().toString());
 		jsonObject.addProperty("amount", this.amount);
+		if(this.nbtTag!=null)
+			jsonObject.addProperty("nbt", this.nbtTag.toString());
 		return jsonObject;
 	}
 
@@ -100,5 +124,8 @@ public class FluidTagWithSize implements Predicate<FluidStack>
 	{
 		out.writeResourceLocation(this.fluidTag.getId());
 		out.writeInt(this.amount);
+		out.writeBoolean(this.nbtTag!=null);
+		if(this.nbtTag!=null)
+			out.writeCompoundTag(this.nbtTag);
 	}
 }
