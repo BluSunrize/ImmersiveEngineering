3c14cf567980b35b0573f8c378e52b8957a9f7f8
MalkContent <patrick-riedel@gmx.net>
2020-09-03 17:40:36 +0200

    make transport time dependent on conveyor animation to visibly transport items with the conveyor belt instead of them sliding along at different speeds halved processTime to better fit the animation (and indirectly buffing the MetalPress, which I felt was running slow anyways. Please don't be mad, revert at your discretion)

--- src/main/java/blusunrize/immersiveengineering/client/render/tile/MetalPressRenderer.java
+++ src/main/java/blusunrize/immersiveengineering/client/render/tile/MetalPressRenderer.java
@@ -34,9 +34,10 @@ public class MetalPressRenderer extends TileEntityRenderer<MetalPressTileEntity>
 {
 	public static DynamicModel<Void> PISTON;
 
-	private static final float standardTransportTime = 52.5f;
+	private static final float translationDistance = 2.5f;
+	private static final float standardTransportTime = 16f*(translationDistance/2); //16 frames in conveyor animation, 1 frame/tick, 2.5 blocks of total translation distance, halved because transport time just affects half the distance
 	private static final float standardPressTime = 3.75f;
-	private static final float minCycleTime = 120f; //set >= 2*(standardPressTime+standardTransportTime)
+	private static final float minCycleTime = 60f; //set >= 2*(standardPressTime+standardTransportTime)
 
 	public MetalPressRenderer(TileEntityRendererDispatcher rendererDispatcherIn)
 	{
@@ -73,7 +74,7 @@ public class MetalPressRenderer extends TileEntityRenderer<MetalPressTileEntity>
 			float fProcess = process.processTick;
 
 			if(fProcess < transportTime)
-				shift[i] = fProcess/transportTime*.5f;
+				shift[i] = .5f*fProcess/transportTime;
 			else if(fProcess < (processMaxTicks-transportTime))
 				shift[i] = .5f;
 			else
@@ -117,7 +118,7 @@ public class MetalPressRenderer extends TileEntityRenderer<MetalPressTileEntity>
 			if(!(process instanceof PoweredMultiblockTileEntity.MultiblockProcessInWorld))
 				continue;
 			matrixStack.push();
-			matrixStack.translate(0, 0, -2.5*shift[i]);
+			matrixStack.translate(0, 0, -translationDistance*shift[i]);
 			if(piston > .92)
 				matrixStack.translate(0, .92-piston, 0);
 
