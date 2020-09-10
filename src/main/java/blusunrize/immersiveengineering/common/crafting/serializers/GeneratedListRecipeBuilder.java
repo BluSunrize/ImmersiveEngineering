ee01de9d3c46f8be2dfb12c4499b0168310c80a8
malte0811 <malte0811@web.de>
2020-09-10 08:58:09 +0200

    Generate recipe JSONs for generated list recipes

---
+++ src/main/java/blusunrize/immersiveengineering/common/crafting/serializers/GeneratedListRecipeBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2020
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ *
+ */
+
+package blusunrize.immersiveengineering.common.crafting.serializers;
+
+import blusunrize.immersiveengineering.common.crafting.GeneratedListRecipe;
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonObject;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+
+public class GeneratedListRecipeBuilder
+{
+	public GeneratedListRecipeBuilder()
+	{
+	}
+
+	public static void build(Consumer<IFinishedRecipe> consumerIn, final ResourceLocation id)
+	{
+		Preconditions.checkArgument(GeneratedListRecipe.LIST_GENERATORS.containsKey(id));
+		consumerIn.accept(new IFinishedRecipe()
+		{
+			public void serialize(@Nonnull JsonObject json)
+			{
+			}
+
+			@Nonnull
+			public IRecipeSerializer<?> getSerializer()
+			{
+				return GeneratedListRecipe.SERIALIZER.get();
+			}
+
+			@Nonnull
+			public ResourceLocation getID()
+			{
+				return id;
+			}
+
+			@Nullable
+			public JsonObject getAdvancementJson()
+			{
+				return null;
+			}
+
+			public ResourceLocation getAdvancementID()
+			{
+				return new ResourceLocation("");
+			}
+		});
+	}
+}
