e28f2d6d45da58c2b98eba2cd9180f06d54afedb
BluSunrize <danny.rad10@yahoo.de>
2020-09-22 12:37:07 +0200

    Make JEI handler nicer *again*

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/sawmill/SawmillRecipeCategory.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/jei/sawmill/SawmillRecipeCategory.java
@@ -31,20 +31,23 @@ import java.util.Arrays;
 public class SawmillRecipeCategory extends IERecipeCategory<SawmillRecipe>
 {
 	public static final ResourceLocation UID = new ResourceLocation(Lib.MODID, "sawmill");
-	private final IDrawableAnimated arrow;
-	private final IDrawableAnimated arrow_split;
+	private final IDrawableStatic middle;
+	private final IDrawableAnimated arrowNormal;
+	private final IDrawableAnimated arrowSplit;
 
 	public SawmillRecipeCategory(IGuiHelper helper)
 	{
 		super(SawmillRecipe.class, helper, UID, "block.immersiveengineering.sawmill");
 		setBackground(helper.drawableBuilder(
-				JEIHelper.JEI_GUI, 0, 0, 104, 26).setTextureSize(128, 128).addPadding(2, 36, 3, 13).build()
+				JEIHelper.JEI_GUI, 0, 0, 114, 26).setTextureSize(128, 128).addPadding(2, 36, 2, 12).build()
 		);
-		IDrawableStatic arrowStatic = helper.drawableBuilder(JEIHelper.JEI_GUI, 0, 26, 46, 16).setTextureSize(128, 128).build();
-		this.arrow = helper.createAnimatedDrawable(arrowStatic, 80, IDrawableAnimated.StartDirection.LEFT, false);
-		arrowStatic = helper.drawableBuilder(JEIHelper.JEI_GUI, 46, 26, 46, 16).setTextureSize(128, 128).build();
-		this.arrow_split = helper.createAnimatedDrawable(arrowStatic, 80, IDrawableAnimated.StartDirection.LEFT, false);
 		setIcon(new ItemStack(IEBlocks.Multiblocks.sawmill));
+
+		this.middle =  helper.drawableBuilder(JEIHelper.JEI_GUI, 0, 26, 29, 16).setTextureSize(128, 128).build();
+		IDrawableStatic arrowStatic = helper.drawableBuilder(JEIHelper.JEI_GUI, 29, 26, 66, 16).setTextureSize(128, 128).build();
+		this.arrowSplit = helper.createAnimatedDrawable(arrowStatic, 80, IDrawableAnimated.StartDirection.LEFT, false);
+		arrowStatic = helper.drawableBuilder(JEIHelper.JEI_GUI, 29, 42, 66, 17).setTextureSize(128, 128).build();
+		this.arrowNormal = helper.createAnimatedDrawable(arrowStatic, 80, IDrawableAnimated.StartDirection.LEFT, false);
 	}
 
 	@Override
@@ -63,24 +66,24 @@ public class SawmillRecipeCategory extends IERecipeCategory<SawmillRecipe>
 	public void setRecipe(IRecipeLayout recipeLayout, SawmillRecipe recipe, IIngredients ingredients)
 	{
 		IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
-		guiItemStacks.init(0, true, 3, 6);
+		guiItemStacks.init(0, true, 2, 6);
 		guiItemStacks.set(0, Arrays.asList(recipe.input.getMatchingStacks()));
 
 		int slot = 1;
 		if(!recipe.stripped.isEmpty())
 		{
-			guiItemStacks.init(slot, false, 40, 6);
+			guiItemStacks.init(slot, false, 46, 6);
 			guiItemStacks.set(slot, recipe.stripped);
 			guiItemStacks.setBackground(slot++, JEIHelper.slotDrawable);
 		}
 
-		guiItemStacks.init(slot, false, 85, 6);
+		guiItemStacks.init(slot, false, 94, 6);
 		guiItemStacks.set(slot++, recipe.output);
 
 		int i = 0;
 		for(ItemStack out : recipe.secondaryStripping)
 		{
-			guiItemStacks.init(slot, false, 40+i%2*18, 28+i/2*18);
+			guiItemStacks.init(slot, false, 46+i%2*18, 28+i/2*18);
 			guiItemStacks.set(slot, out);
 			guiItemStacks.setBackground(slot++, JEIHelper.slotDrawable);
 			i++;
@@ -89,7 +92,7 @@ public class SawmillRecipeCategory extends IERecipeCategory<SawmillRecipe>
 		i = 0;
 		for(ItemStack out : recipe.secondaryOutputs)
 		{
-			guiItemStacks.init(slot, false, 81+i%2*18, 28+i/2*18);
+			guiItemStacks.init(slot, false, 90+i%2*18, 28+i/2*18);
 			guiItemStacks.set(slot, out);
 			guiItemStacks.setBackground(slot++, JEIHelper.slotDrawable);
 			i++;
@@ -100,9 +103,12 @@ public class SawmillRecipeCategory extends IERecipeCategory<SawmillRecipe>
 	public void draw(SawmillRecipe recipe, double mouseX, double mouseY)
 	{
 		if(recipe.stripped.isEmpty())
-			this.arrow.draw(28, 6);
+		{
+			this.middle.draw(36,7);
+			this.arrowNormal.draw(22, 6);
+		}
 		else
-			this.arrow_split.draw(28, 6);
+			this.arrowSplit.draw(22, 6);
 	}
 
 }
