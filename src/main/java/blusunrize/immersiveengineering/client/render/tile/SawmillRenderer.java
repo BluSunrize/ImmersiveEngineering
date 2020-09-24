468ba5e16c9b5b995447e9b730fd502a5e8e7b0e
malte0811 <malte0811@web.de>
2020-09-24 17:41:45 +0200

    Fix compile error

--- src/main/java/blusunrize/immersiveengineering/client/render/tile/SawmillRenderer.java
+++ src/main/java/blusunrize/immersiveengineering/client/render/tile/SawmillRenderer.java
@@ -15,9 +15,6 @@ import blusunrize.immersiveengineering.common.blocks.metal.SawmillTileEntity.Saw
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Quaternion;
 import net.minecraft.client.renderer.RenderType;
@@ -27,7 +24,6 @@ import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.client.model.data.EmptyModelData;
 
 public class SawmillRenderer extends TileEntityRenderer<SawmillTileEntity>
 {
@@ -73,8 +69,8 @@ public class SawmillRenderer extends TileEntityRenderer<SawmillTileEntity>
 				spin += 36f*partialTicks;
 			matrixStack.rotate(new Quaternion(0, 0, spin, true));
 			ClientUtils.renderModelTESRFast(
-					BLADE.get(Direction.NORTH).getQuads(state, null, te.getWorldNonnull().rand, EmptyModelData.INSTANCE),
-					solidBuilder, matrixStack, combinedLightIn);
+					BLADE.getNullQuads(Direction.NORTH, state),
+					solidBuilder, matrixStack, combinedLightIn, combinedOverlayIn);
 			matrixStack.pop();
 		}
 
