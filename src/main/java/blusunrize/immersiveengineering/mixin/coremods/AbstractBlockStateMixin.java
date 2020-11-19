1aa445ad6d96bf92dfee2460af1f510b72fabe48
malte0811 <malte0811@web.de>
2020-11-19 13:54:33 +0100

    Use a more compatible (if slightly less reliable) injection point for wire collisions

--- src/main/java/blusunrize/immersiveengineering/mixin/coremods/AbstractBlockStateMixin.java
+++ src/main/java/blusunrize/immersiveengineering/mixin/coremods/AbstractBlockStateMixin.java
@@ -10,27 +10,21 @@
 package blusunrize.immersiveengineering.mixin.coremods;
 
 import blusunrize.immersiveengineering.common.wires.WireCollisions;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Entity.class)
-public class EntityMixin
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public class AbstractBlockStateMixin
 {
-	@Redirect(
-			method = "doBlockCollisions",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/block/BlockState;onEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V"
-			)
-	)
-	public void onBlockCollision(BlockState abstractBlockState, World worldIn, BlockPos pos, Entity entityIn)
+	@Inject(method = "onEntityCollision", at = @At(value = "HEAD"))
+	public void onBlockCollision(World worldIn, BlockPos pos, Entity entityIn, CallbackInfo ci)
 	{
-		abstractBlockState.onEntityCollision(worldIn, pos, entityIn);
 		WireCollisions.handleEntityCollision(pos, entityIn);
 	}
 }
