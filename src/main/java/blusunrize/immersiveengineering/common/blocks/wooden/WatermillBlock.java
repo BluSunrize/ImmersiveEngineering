17da6b0c721703587e8b6a937684c56c0f550447
malte0811 <malte0811@web.de>
2020-12-31 21:28:17 +0100

    Fix waterwheel not placing at certain angles, and do not place it inside the player, closes #4538

--- src/main/java/blusunrize/immersiveengineering/common/blocks/wooden/WatermillBlock.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/wooden/WatermillBlock.java
@@ -15,11 +15,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Direction.Axis;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
@@ -46,14 +48,18 @@ public class WatermillBlock extends IETileProviderBlock
 	{
 		BlockPos center = context.getPos();
 		World world = context.getWorld();
-		Direction facing = context.getNearestLookingDirection();//TODO is this the correct one?
+		Direction facing = context.getPlacementHorizontalFacing();
+		PlayerEntity player = context.getPlayer();
+		ISelectionContext selectionCtx = player==null?ISelectionContext.dummy(): ISelectionContext.forEntity(player);
+		BlockState stateToPlace = getDefaultState();
 		for(int hh = -2; hh <= 2; hh++)
 			for(int ww = -2; ww <= 2; ww++)
 				if(((hh > -2&&hh < 2)||(ww > -2&&ww < 2))&&(hh!=0||ww!=0))
 				{
 					BlockPos pos2 = center.add(facing.getAxis()==Axis.Z?ww: 0, hh, facing.getAxis()==Axis.Z?0: ww);
 					BlockState state = world.getBlockState(pos2);
-					if(!state.isReplaceable(BlockItemUseContext.func_221536_a(context, pos2, facing)))
+					if(!state.isReplaceable(BlockItemUseContext.func_221536_a(context, pos2, facing))||
+							!world.placedBlockCollides(stateToPlace, pos2, selectionCtx))
 						return false;
 				}
 		return true;
