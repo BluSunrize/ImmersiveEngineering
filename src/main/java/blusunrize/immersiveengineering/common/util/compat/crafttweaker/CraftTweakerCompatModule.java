1a21d30abfa22e71705bb99f19e995d73ef3cf6b
Jared <jaredlll08@gmail.com>
2021-01-23 22:57:03 +0200

    year++

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/crafttweaker/CraftTweakerCompatModule.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/crafttweaker/CraftTweakerCompatModule.java
@@ -1,3 +1,10 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2021
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
 package blusunrize.immersiveengineering.common.util.compat.crafttweaker;
 
 import blusunrize.immersiveengineering.common.util.compat.IECompatModule;
--- src/main/java/blusunrize/immersiveengineering/common/util/compat/crafttweaker/CrafttweakerEventHandlers.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/crafttweaker/CrafttweakerEventHandlers.java
@@ -1,6 +1,6 @@
 /*
  * BluSunrize
- * Copyright (c) 2020
+ * Copyright (c) 2021
  *
  * This code is licensed under "Blu's License of Common Sense"
  * Details can be found in the license file in the root folder of this project
