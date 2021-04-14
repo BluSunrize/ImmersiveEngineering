af68d1690e69f51de92aadaa1e617dd5268264f3
malte0811 <malte0811@web.de>
2021-04-14 08:56:10 +0200

    Add copyright statements to computer compat code

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/computers/generic/owners/FloodlightCallbacks.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/computers/generic/owners/FloodlightCallbacks.java
@@ -1,3 +1,11 @@
+/*
+ *  BluSunrize
+ *  Copyright (c) 2021
+ *
+ *  This code is licensed under "Blu's License of Common Sense"
+ *  Details can be found in the license file in the root folder of this project
+ */
+
 package blusunrize.immersiveengineering.common.util.compat.computers.generic.owners;
 
 import blusunrize.immersiveengineering.common.blocks.metal.FloodlightTileEntity;
