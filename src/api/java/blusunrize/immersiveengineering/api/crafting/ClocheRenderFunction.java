e303e2bf49d103bfa86bffd59d575cd0579566e8
InfinityRaider <InfinityRaider@users.noreply.github.com>
2021-05-26 13:44:53 +0100

    Allow more dynamic recipes in the Garden Cloche (#4814)
    * Modifications to the Cloche for more dynamic recipes
    
    * Addition of javadoc and comment cleanup

--- src/api/java/blusunrize/immersiveengineering/api/crafting/ClocheRenderFunction.java
+++ src/api/java/blusunrize/immersiveengineering/api/crafting/ClocheRenderFunction.java
@@ -23,6 +23,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public interface ClocheRenderFunction
@@ -30,6 +31,27 @@ public interface ClocheRenderFunction
 	float getScale(ItemStack seed, float growth);
 
 	Collection<Pair<BlockState, TransformationMatrix>> getBlocks(ItemStack stack, float growth);
+
+	/**
+	 * Method to inject quads into the Cloche's plant rendering.
+	 * Any quads passed to the consumer will be included in the plant rendering.
+	 *
+	 *
+	 * Immersive Engineering will not cache any quads injected by this method,
+	 * therefore it is up to the implementation to make sure quads are properly cached.
+	 *
+	 * Additionally, even though this method is only called client side, the containing class exists on both sides.
+	 * As a result, it is imperative that implementations of this method do not contain direct references to client-only
+	 * code.
+	 *
+	 * @param stack the stack containing the seed for which the plant is being rendered
+	 * @param growth the current growth progress of the plant which is being rendered
+	 * @param quadConsumer the consumer to pass quads to
+	 */
+	default void injectQuads(ItemStack stack, float growth, Consumer<?> quadConsumer)
+	{
+
+	}
 
 
 	/**
