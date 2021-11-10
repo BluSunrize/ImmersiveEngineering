924839becf76e3018f92ade81f286db309adfe7f
malte0811 <malte0811@web.de>
2021-11-10 17:15:58 +0100

    Inline reverseDirection in moveItemStackToWithMayPlace The behavior for reverseDirection=true does not match the vanilla behavior one would expect from the method name, and since it is always called with reverseDirection=false this is the cleanest fix.

--- src/main/java/blusunrize/immersiveengineering/common/gui/IEBaseContainer.java
+++ src/main/java/blusunrize/immersiveengineering/common/gui/IEBaseContainer.java
@@ -113,7 +113,7 @@ public class IEBaseContainer<T extends BlockEntity> extends AbstractContainerMen
 					return ItemStack.EMPTY;
 				}
 			}
-			else if(!this.moveItemStackToWithMayPlace(itemstack1, 0, slotCount, false))
+			else if(!this.moveItemStackToWithMayPlace(itemstack1, 0, slotCount))
 			{
 				return ItemStack.EMPTY;
 			}
@@ -131,7 +131,7 @@ public class IEBaseContainer<T extends BlockEntity> extends AbstractContainerMen
 		return itemstack;
 	}
 
-	protected boolean moveItemStackToWithMayPlace(ItemStack pStack, int pStartIndex, int pEndIndex, boolean pReverseDirection)
+	protected boolean moveItemStackToWithMayPlace(ItemStack pStack, int pStartIndex, int pEndIndex)
 	{
 		boolean inAllowedRange = true;
 		int allowedStart = pStartIndex;
@@ -140,7 +140,7 @@ public class IEBaseContainer<T extends BlockEntity> extends AbstractContainerMen
 			boolean mayplace = this.slots.get(i).mayPlace(pStack);
 			if(inAllowedRange&&(!mayplace||i==pEndIndex-1))
 			{
-				if(moveItemStackTo(pStack, allowedStart, i, pReverseDirection))
+				if(moveItemStackTo(pStack, allowedStart, i, false))
 					return true;
 				inAllowedRange = false;
 			}
