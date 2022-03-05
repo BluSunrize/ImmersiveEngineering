dc8bf31727f8e4e1b2a00bc8e589261f277a4905
malte0811 <malte0811@web.de>
2022-03-05 19:03:54 +0100

    Make hammer crushing recipes properly lazy

---
+++ src/main/java/blusunrize/immersiveengineering/common/crafting/LazyShapelessRecipe.java
@@ -0,0 +1,56 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2022
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+package blusunrize.immersiveengineering.common.crafting;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.ShapelessRecipe;
+import net.minecraftforge.common.util.Lazy;
+
+import javax.annotation.Nonnull;
+
+public class LazyShapelessRecipe extends ShapelessRecipe
+{
+	private final Lazy<ItemStack> result;
+	private final RecipeSerializer<LazyShapelessRecipe> serializer;
+
+	public LazyShapelessRecipe(
+			ResourceLocation id, String groups, Lazy<ItemStack> result, NonNullList<Ingredient> ingredients, RecipeSerializer<LazyShapelessRecipe> serializer
+	)
+	{
+		super(id, groups, ItemStack.EMPTY, ingredients);
+		this.result = result;
+		this.serializer = serializer;
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack getResultItem()
+	{
+		return result.get();
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack assemble(@Nonnull CraftingContainer p_44260_)
+	{
+		return result.get();
+	}
+
+	@Nonnull
+	@Override
+	public RecipeSerializer<LazyShapelessRecipe> getSerializer()
+	{
+		return serializer;
+	}
+}
--- src/main/java/blusunrize/immersiveengineering/common/crafting/serializers/HammerCrushingRecipeSerializer.java
+++ src/main/java/blusunrize/immersiveengineering/common/crafting/serializers/HammerCrushingRecipeSerializer.java
@@ -10,6 +10,7 @@
 package blusunrize.immersiveengineering.common.crafting.serializers;
 
 import blusunrize.immersiveengineering.api.crafting.IERecipeSerializer;
+import blusunrize.immersiveengineering.common.crafting.LazyShapelessRecipe;
 import blusunrize.immersiveengineering.common.register.IEItems;
 import com.google.gson.JsonObject;
 import net.minecraft.core.NonNullList;
@@ -18,14 +19,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.ShapelessRecipe;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.common.util.Lazy;
 
 import javax.annotation.Nonnull;
 
-public class HammerCrushingRecipeSerializer extends IERecipeSerializer<ShapelessRecipe>
+public class HammerCrushingRecipeSerializer extends IERecipeSerializer<LazyShapelessRecipe>
 {
 	@Override
 	public ItemStack getIcon()
@@ -35,29 +35,28 @@ public class HammerCrushingRecipeSerializer extends IERecipeSerializer<Shapeless
 
 	@Nonnull
 	@Override
-	public ShapelessRecipe readFromJson(@Nonnull ResourceLocation recipeId, @Nonnull JsonObject json)
+	public LazyShapelessRecipe readFromJson(@Nonnull ResourceLocation recipeId, @Nonnull JsonObject json)
 	{
 		Lazy<ItemStack> output = readOutput(json.get("result"));
 		Ingredient input = Ingredient.fromJson(GsonHelper.getAsJsonObject(json, "input"));
 		NonNullList<Ingredient> ingredients = NonNullList.of(Ingredient.EMPTY, input, Ingredient.of(IEItems.Tools.HAMMER));
-		// TODO make non-tagbased or add a lazy shapeless recipe?
-		return new ShapelessRecipe(recipeId, "", output.get(), ingredients);
+		return new LazyShapelessRecipe(recipeId, "", output, ingredients, this);
 	}
 
 	@Nonnull
 	@Override
-	public ShapelessRecipe fromNetwork(@Nonnull ResourceLocation recipeId, @Nonnull FriendlyByteBuf buffer)
+	public LazyShapelessRecipe fromNetwork(@Nonnull ResourceLocation recipeId, @Nonnull FriendlyByteBuf buffer)
 	{
 		int count = buffer.readInt();
 		NonNullList<Ingredient> ingredients = NonNullList.withSize(count, Ingredient.EMPTY);
 		for(int i = 0; i < count; i++)
 			ingredients.set(i, Ingredient.fromNetwork(buffer));
-		ItemStack output = buffer.readItem();
-		return new ShapelessRecipe(recipeId, "", output, ingredients);
+		Lazy<ItemStack> output = readLazyStack(buffer);
+		return new LazyShapelessRecipe(recipeId, "", output, ingredients, this);
 	}
 
 	@Override
-	public void toNetwork(@Nonnull FriendlyByteBuf buffer, @Nonnull ShapelessRecipe recipe)
+	public void toNetwork(@Nonnull FriendlyByteBuf buffer, @Nonnull LazyShapelessRecipe recipe)
 	{
 		NonNullList<Ingredient> ingredients = recipe.getIngredients();
 		buffer.writeInt(ingredients.size());
