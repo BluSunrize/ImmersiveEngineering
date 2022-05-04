912dc73afda60a110857fc6e71541b3d11fbd256
malte0811 <malte0811@web.de>
2022-05-04 09:13:19 +0200

    Make template chunk behavior more consistent when querying via sections vs querying directly This fixes an issue with Lithium ports where the multiblock preview just doesn't show

--- src/main/java/blusunrize/immersiveengineering/common/util/fakeworld/TemplateChunk.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/fakeworld/TemplateChunk.java
@@ -1,60 +1,49 @@
 package blusunrize.immersiveengineering.common.util.fakeworld;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.EmptyLevelChunk;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.StructureBlockInfo;
 import net.minecraft.world.level.material.FluidState;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 
-public class TemplateChunk extends EmptyLevelChunk
+public class TemplateChunk extends LevelChunk
 {
-	private final Map<BlockPos, StructureBlockInfo> blocksInChunk;
-	private final Map<BlockPos, BlockEntity> tiles;
 	private final Predicate<BlockPos> shouldShow;
+	private final Holder<Biome> biome;
 
 	public TemplateChunk(Level worldIn, ChunkPos chunkPos, List<StructureBlockInfo> blocksInChunk, Predicate<BlockPos> shouldShow)
 	{
-		super(worldIn, chunkPos, worldIn.getUncachedNoiseBiome(0, 0, 0));
+		super(worldIn, chunkPos);
+		Registry<Biome> biomeRegistry = worldIn.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY);
+		for(int i = 0; i < getSections().length; ++i)
+			getSections()[i] = new TemplateChunkSection(i, biomeRegistry, shouldShow);
 		this.shouldShow = shouldShow;
-		this.blocksInChunk = new HashMap<>();
-		tiles = new HashMap<>();
+		this.biome = worldIn.getUncachedNoiseBiome(0, 0, 0);
 		for(StructureBlockInfo info : blocksInChunk)
 		{
-			this.blocksInChunk.put(info.pos, info);
+			actuallSetBlockState(info.pos, info.state);
 			if(info.nbt!=null)
 			{
 				BlockEntity tile = BlockEntity.loadStatic(info.pos, info.state, info.nbt);
 				if(tile!=null)
 				{
 					tile.setLevel(worldIn);
-					tiles.put(info.pos, tile);
+					getBlockEntities().put(info.pos, tile);
 				}
 			}
 		}
-	}
-
-	@Nonnull
-	@Override
-	public BlockState getBlockState(@Nonnull BlockPos pos)
-	{
-		if(shouldShow.test(pos))
-		{
-			StructureBlockInfo result = blocksInChunk.get(pos);
-			if(result!=null)
-				return result.state;
-		}
-		return Blocks.VOID_AIR.defaultBlockState();
 	}
 
 	@Nonnull
@@ -71,6 +60,71 @@ public class TemplateChunk extends EmptyLevelChunk
 	{
 		if(!shouldShow.test(pos))
 			return null;
-		return tiles.get(pos);
+		return getBlockEntities().get(pos);
+	}
+
+	@Nullable
+	@Override
+	public BlockState setBlockState(@Nonnull BlockPos pos, @Nonnull BlockState state, boolean isMoving)
+	{
+		return null;
+	}
+
+	public void actuallSetBlockState(@Nonnull BlockPos pos, @Nonnull BlockState state)
+	{
+		final int sectionIndex = getSectionIndex(pos.getY());
+		final int sectionX = pos.getX()&15;
+		final int sectionY = pos.getY()&15;
+		final int sectionZ = pos.getZ()&15;
+		final TemplateChunkSection section = (TemplateChunkSection)getSection(sectionIndex);
+		section.actuallySetBlockState(sectionX, sectionY, sectionZ, state);
+	}
+
+	@Override
+	public int getLightEmission(@Nonnull BlockPos pos)
+	{
+		return 0;
+	}
+
+	@Override
+	public void addAndRegisterBlockEntity(@Nonnull BlockEntity blockEntity)
+	{
+	}
+
+	@Override
+	public void setBlockEntity(@Nonnull BlockEntity blockEntity)
+	{
+	}
+
+	@Override
+	public void removeBlockEntity(@Nonnull BlockPos pos)
+	{
+	}
+
+	// Not always correct, but hopefully "good enough"
+	@Override
+	public boolean isEmpty()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isYSpaceEmpty(int startY, int endY)
+	{
+		return false;
+	}
+
+	@Nonnull
+	@Override
+	public ChunkHolder.FullChunkStatus getFullStatus()
+	{
+		return ChunkHolder.FullChunkStatus.BORDER;
+	}
+
+	@Nonnull
+	@Override
+	public Holder<Biome> getNoiseBiome(int p_204426_, int p_204427_, int p_204428_)
+	{
+		return this.biome;
 	}
 }
---
+++ src/main/java/blusunrize/immersiveengineering/common/util/fakeworld/TemplateChunkSection.java
@@ -0,0 +1,51 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2022
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+package blusunrize.immersiveengineering.common.util.fakeworld;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+
+import javax.annotation.Nonnull;
+import java.util.function.Predicate;
+
+public class TemplateChunkSection extends LevelChunkSection
+{
+	private final Predicate<BlockPos> shouldShow;
+
+	public TemplateChunkSection(int sectionY, Registry<Biome> biomeRegistry, Predicate<BlockPos> shouldShow)
+	{
+		super(sectionY, biomeRegistry);
+		this.shouldShow = shouldShow;
+	}
+
+	@Nonnull
+	@Override
+	public BlockState setBlockState(int x, int y, int z, @Nonnull BlockState state, boolean lock)
+	{
+		return getBlockState(x, y, z);
+	}
+
+	@Nonnull
+	public BlockState actuallySetBlockState(int x, int y, int z, @Nonnull BlockState state)
+	{
+		return super.setBlockState(x, y, z, state, false);
+	}
+
+	@Override
+	public BlockState getBlockState(int x, int y, int z)
+	{
+		if(!shouldShow.test(new BlockPos(x, y, z)))
+			return Blocks.AIR.defaultBlockState();
+		return super.getBlockState(x, y, z);
+	}
+}
