76a64710fea089338f08d737264eb62b9da2fdc5
malte0811 <malte0811@web.de>
2022-05-27 09:02:26 +0200

    Add package-info.java for icon datagen to document who wrote what code

---
+++ src/datagen/java/blusunrize/immersiveengineering/data/icon/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2022
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+/**
+ * The history of this package is not fully visible on git since it moved between projects twice:
+ * malte0811, Immersive Engineering: Initial demo version, able to render vanilla models but not OBJ and similar
+ * OrionOnline, Chisels&Bits: Fully working version, able to render all models
+ * malte0811, Immersive Engineering: Port from 1.16.5 to 1.18.2
+ */
+package blusunrize.immersiveengineering.data.icon;
