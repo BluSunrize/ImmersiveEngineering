01a26ece95fe257af3ea0f726b1ff821ccfb503f
malte0811 <malte0811@web.de>
2022-06-10 12:40:06 +0200

    Fix Mixin warning due to a Forge added field that used to be vanilla

--- src/main/java/blusunrize/immersiveengineering/mixin/accessors/FurnaceTEAccess.java
+++ src/main/java/blusunrize/immersiveengineering/mixin/accessors/FurnaceTEAccess.java
@@ -13,15 +13,19 @@ import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public interface FurnaceTEAccess
 {
-	@Accessor
+	// This is a Forge-added field in 1.19, so it does not need to be remapped
+	@Accessor(remap = false)
+	@Final
 	RecipeType<? extends AbstractCookingRecipe> getRecipeType();
 
 	@Accessor
+	@Final
 	ContainerData getDataAccess();
 }
