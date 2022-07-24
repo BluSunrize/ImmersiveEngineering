0d6501a8e7e3700eaa1f583b2e8e2878224bbe92
malte0811 <malte0811@web.de>
2022-07-24 10:23:16 +0200

    Exclude crate contents from item share tag

--- src/main/java/blusunrize/immersiveengineering/common/blocks/BlockItemIE.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/BlockItemIE.java
@@ -153,9 +153,9 @@ public class BlockItemIE extends BlockItem
 
 	public static class BlockItemIENoInventory extends BlockItemIE
 	{
-		public BlockItemIENoInventory(Block b, Properties props)
+		public BlockItemIENoInventory(Block b)
 		{
-			super(b, props);
+			super(b);
 		}
 
 		@Nullable
@@ -166,7 +166,7 @@ public class BlockItemIE extends BlockItem
 			if(ret!=null)
 			{
 				ret = ret.copy();
-				ret.remove("inventory");
+				ret.remove("Items");
 			}
 			return ret;
 		}
