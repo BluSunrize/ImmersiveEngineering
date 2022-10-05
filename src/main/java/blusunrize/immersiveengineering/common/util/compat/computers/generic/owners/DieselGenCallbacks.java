479f02cd521a38b5e83921653f3a48e04d355915
Pespinho <38698281+pespinho@users.noreply.github.com>
2022-10-05 16:40:32 +0200

    Fix problems with CC: Tweaked compatibility and diesel generators and… (#5448)
    * Fix problems with CC: Tweaked compatibility and diesel generators and add the new computer callable method getEnabled()
    
    Fixed a problem causing Lua exceptions when methods isRunning() and setEnabled() were called for a diesel generator by:
    - Correcting the parameter type for setEnabled() in MultiblockCallbacks.java.
    - Moving isRunning() to PoweredMBCallbacks.java.
    - Adding a specific version of isRunning() for diesel generators in DieselGenCallbacks.java.
    
    Added a method getEnabled() to MultiblockCallbacks.java so that the enabled status can be also queried.
    
    Made some changes for setting the RS block as CallbackEnvironment for Computer Calls of MultiblockCallbackOwners:
    - changed preprocess() accordingly, so that it returns the RS block entity.
    - changed the isRunning() method, so that it first retrieves the master block before querying.
    
    * Use new method beforePreprocess() for accessing the RS BE.
    
    Co-authored-by: Pespinho <pespinho@users.noreply.github.com>

--- src/main/java/blusunrize/immersiveengineering/common/util/compat/computers/generic/owners/DieselGenCallbacks.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/compat/computers/generic/owners/DieselGenCallbacks.java
@@ -9,11 +9,19 @@
 package blusunrize.immersiveengineering.common.util.compat.computers.generic.owners;
 
 import blusunrize.immersiveengineering.common.blocks.metal.DieselGeneratorBlockEntity;
+import blusunrize.immersiveengineering.common.util.compat.computers.generic.CallbackEnvironment;
+import blusunrize.immersiveengineering.common.util.compat.computers.generic.ComputerCallable;
 import blusunrize.immersiveengineering.common.util.compat.computers.generic.impl.MultiblockCallbacks;
 import blusunrize.immersiveengineering.common.util.compat.computers.generic.impl.TankCallbacks;
 
 public class DieselGenCallbacks extends MultiblockCallbackOwner<DieselGeneratorBlockEntity>
 {
+	@ComputerCallable
+	public boolean isRunning(CallbackEnvironment<DieselGeneratorBlockEntity> env)
+	{
+		return env.object().active;
+	}
+
 	public DieselGenCallbacks()
 	{
 		super(DieselGeneratorBlockEntity.class, "diesel_generator");
