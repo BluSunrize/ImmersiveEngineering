344648b948e95cb46a685bfd719fd94ea4e660f0
malte0811 <malte0811@web.de>
2023-01-06 15:39:33 +0100

    Add missing copyright headers

--- src/main/java/blusunrize/immersiveengineering/common/CommonProxy.java
+++ src/main/java/blusunrize/immersiveengineering/common/CommonProxy.java
@@ -8,6 +8,14 @@
 
 /*
  * BluSunrize
+ * Copyright (c) 2023
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ */
+
+/*
+ * BluSunrize
  * Copyright (c) 2020
  *
  * This code is licensed under "Blu's License of Common Sense"
