6e12f797847ff588bc5dcb7e19114f232e2eef91
BluSunrize <danny.rad10@yahoo.de>
2023-03-19 14:44:07 +0100

    Remove a lot of custom handling from IE projectiles
    Among many other things, this fixes piercing on sawblades, closes #5622

--- src/main/java/blusunrize/immersiveengineering/common/entities/IEProjectileEntity.java
+++ src/main/java/blusunrize/immersiveengineering/common/entities/IEProjectileEntity.java
@@ -10,11 +10,7 @@ package blusunrize.immersiveengineering.common.entities;
 
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtUtils;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -24,21 +20,12 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.level.ClipContext.Block;
-import net.minecraft.world.level.ClipContext.Fluid;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.phys.*;
-import net.minecraft.world.phys.HitResult.Type;
-import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -47,10 +34,6 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 	private static final EntityDataAccessor<Optional<UUID>> SHOOTER_PARAMETER =
 			SynchedEntityData.defineId(IEProjectileEntity.class, EntityDataSerializers.OPTIONAL_UUID);
 
-	protected BlockPos stuckIn = null;
-	protected BlockState inBlockState;
-	public boolean inGround;
-	public int ticksInGround;
 	public int ticksInAir;
 	protected IntSet piercedEntities;
 	@Nullable
@@ -142,175 +125,56 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 		return ItemStack.EMPTY;
 	}
 
+	public boolean isInGround()
+	{
+		return this.inGround;
+	}
+
 	@Override
 	public void tick()
 	{
 		if(this.getOwner()==null&&this.level.isClientSide)
 			this.shooterUUID = getShooterSynced();
 
-		this.baseTick();
-		BlockState localState;
-		if(stuckIn!=null)
-			localState = this.level.getBlockState(stuckIn);
-		else
-			localState = Blocks.AIR.defaultBlockState();
+		// store previous movement
+		Vec3 delta = this.getDeltaMovement().add(0, 0, 0);
+		float xRotPrev = this.getXRot();
+		float yRotPrev = this.getYRot();
+		float xRot0Prev = this.xRotO;
+		float yRot0Prev = this.yRotO;
+		// disable vanilla gravity
+		this.setNoGravity(true);
 
-		//TODO better air check
-		if(localState.getMaterial()!=Material.AIR)
-		{
-			VoxelShape shape = localState.getCollisionShape(this.level, stuckIn);
-			for(AABB subbox : shape.toAabbs())
-				if(subbox.contains(this.getX(), this.getY(), this.getZ()))
-				{
-					inGround = true;
-					break;
-				}
-		}
+		// perform vanilla tick
+		super.tick();
 
-		if(this.inGround)
-		{
-			if(localState==inBlockState)
-			{
-				++this.ticksInGround;
-				if(this.ticksInGround >= getMaxTicksInGround())
-					this.discard();
-			}
-			else
-			{
-				this.inGround = false;
-				setDeltaMovement(getDeltaMovement().scale(this.random.nextFloat()/5));
-				this.ticksInGround = 0;
-				this.ticksInAir = 0;
-			}
-		}
-		else
-		{
-			++this.ticksInAir;
+		// enable gravity
+		this.setNoGravity(false);
+		// restore rotations
+		this.setXRot(xRotPrev);
+		this.setYRot(yRotPrev);
+		this.xRotO = xRot0Prev;
+		this.yRotO = yRot0Prev;
 
-			if(ticksInAir >= tickLimit)
-			{
-				this.discard();
-				return;
-			}
-
-			Vec3 currentPos = new Vec3(this.getX(), this.getY(), this.getZ());
-			Vec3 nextPos = new Vec3(this.getX(), this.getY(), this.getZ()).add(getDeltaMovement());
-			HitResult mop = this.level.clip(new ClipContext(currentPos, nextPos, Block.COLLIDER,
-					Fluid.NONE, this));
-
-			if(mop.getType()==Type.BLOCK)
-				nextPos = mop.getLocation();
-
-			if(mop.getType()!=Type.ENTITY)
-			{
-				Entity entity = null;
-				List<Entity> list = this.level.getEntities(this, this.getBoundingBox().expandTowards(getDeltaMovement()).inflate(1), Entity::isPickable);
-				double d0 = 0.0D;
-				for(Entity entity1 : list)
-				{
-					if(entity1.isPickable()&&(!entity1.getUUID().equals(this.shooterUUID)||this.ticksInAir > 5))
-					{
-						float f = 0.3F;
-						AABB axisalignedbb = entity1.getBoundingBox().inflate((double)f, (double)f, (double)f);
-						Optional<Vec3> movingobjectposition1 = axisalignedbb.clip(currentPos, nextPos);
-
-						if(movingobjectposition1.isPresent())
-						{
-							double d1 = currentPos.distanceTo(movingobjectposition1.get());
-							if(d1 < d0||d0==0.0D)
-							{
-								entity = entity1;
-								d0 = d1;
-							}
-						}
-					}
-				}
-				if(entity!=null)
-					mop = new EntityHitResult(entity);
-			}
-
-			if(mop.getType()!=Type.MISS)
-			{
-				if(mop.getType()==Type.ENTITY)
-				{
-					EntityHitResult entityHit = (EntityHitResult)mop;
-					if(!this.isOnFire()&&this.canIgnite()&&entityHit.getEntity().isOnFire())
-						this.setSecondsOnFire(3);
-					boolean allowHit = true;
-					if(shooterUUID!=null)
-					{
-						Player shooter = level.getPlayerByUUID(shooterUUID);
-						if(shooter!=null&&entityHit.getEntity() instanceof Player)
-							allowHit = shooter.canHarmPlayer((Player)entityHit.getEntity());
-					}
-					if(allowHit)
-						this.onHit(mop);
-					this.handlePiecing(entityHit.getEntity());
-				}
-				else if(mop.getType()==Type.BLOCK)
-				{
-					BlockHitResult blockHit = (BlockHitResult)mop;
-					this.onHit(blockHit);
-					this.stuckIn = blockHit.getBlockPos();
-					this.inBlockState = this.level.getBlockState(blockHit.getBlockPos());
-					setDeltaMovement(blockHit.getLocation().subtract(position()));
-					float f2 = (float)getDeltaMovement().length();
-					Vec3 motion = getDeltaMovement();
-					this.setPos(
-							this.getX()-motion.x/(double)f2*0.05,
-							this.getY()-motion.y/(double)f2*0.05,
-							this.getZ()-motion.z/(double)f2*0.05
-					);
-
-					this.inGround = true;
-					if(this.inBlockState.getMaterial()!=Material.AIR)
-						this.inBlockState.entityInside(this.level, blockHit.getBlockPos(), this);
-				}
-			}
-
-			this.setPos(
-					this.getX()+getDeltaMovement().x,
-					this.getY()+getDeltaMovement().y,
-					this.getZ()+getDeltaMovement().z
-			);
-
-			float absMotion = (float)getDeltaMovement().length();
-			this.setYRot((float)(Math.atan2(getDeltaMovement().x, getDeltaMovement().z)*180.0D/Math.PI));
-			this.setXRot((float)(Math.atan2(getDeltaMovement().y, absMotion)*180.0D/Math.PI));
-			while(this.getXRot()-this.xRotO < -180.0F)
-				this.xRotO -= 360.0F;
-			while(this.getXRot()-this.xRotO >= 180.0F)
-				this.xRotO += 360.0F;
-			while(this.getYRot()-this.yRotO < -180.0F)
-				this.yRotO -= 360.0F;
-			while(this.getYRot()-this.yRotO >= 180.0F)
-				this.yRotO += 360.0F;
-			this.setXRot(this.xRotO+(this.getXRot()-this.xRotO)*0.2F);
-			this.setYRot(this.yRotO+(this.getYRot()-this.yRotO)*0.2F);
-
-
-			float movementDecay = getMotionDecayFactor();
-
-			if(this.isInWater())
-			{
-				for(int j = 0; j < 4; ++j)
-				{
-					float f3 = 0.25F;
-					this.level.addParticle(ParticleTypes.BUBBLE,
-							this.getX()-getDeltaMovement().x*(double)f3,
-							this.getY()-getDeltaMovement().y*(double)f3,
-							this.getZ()-getDeltaMovement().z*(double)f3,
-							getDeltaMovement().x,
-							getDeltaMovement().y,
-							getDeltaMovement().z);
-				}
-				movementDecay *= 0.8F;
-			}
-			if(movementDecay > 0)
-				setDeltaMovement(getDeltaMovement().scale(movementDecay).add(0, -getGravity(), 0));
-			this.setPos(this.getX(), this.getY(), this.getZ());
-			this.checkInsideBlocks();
-		}
+		// perform custom movement changes
+		float absMotion = (float)delta.length();
+		this.setYRot((float)(Math.atan2(delta.x, delta.z)*180.0D/Math.PI));
+		this.setXRot((float)(Math.atan2(delta.y, absMotion)*180.0D/Math.PI));
+		while(this.getXRot()-this.xRotO < -180.0F)
+			this.xRotO -= 360.0F;
+		while(this.getXRot()-this.xRotO >= 180.0F)
+			this.xRotO += 360.0F;
+		while(this.getYRot()-this.yRotO < -180.0F)
+			this.yRotO -= 360.0F;
+		while(this.getYRot()-this.yRotO >= 180.0F)
+			this.yRotO += 360.0F;
+		this.setXRot(this.xRotO+(this.getXRot()-this.xRotO)*0.2F);
+		this.setYRot(this.yRotO+(this.getYRot()-this.yRotO)*0.2F);
+		float movementDecay = getMotionDecayFactor();
+		if(this.isInWater())
+			movementDecay *= 0.8F;
+		if(movementDecay > 0)
+			setDeltaMovement(delta.scale(movementDecay).add(0, -getGravity(), 0));
 	}
 
 	@Override
@@ -351,14 +215,6 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 			this.discard();
 	}
 
-	@Override
-	public boolean shouldRenderAtSqrDistance(double distSq)
-	{
-		double d1 = this.getBoundingBox().getSize()*4.0D;
-		d1 *= 64.0D;
-		return distSq < d1*d1;
-	}
-
 	public double getGravity()
 	{
 		return 0.05F;
@@ -374,14 +230,6 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 		return 100;
 	}
 
-	public abstract void onHit(HitResult mop);
-
-	protected void onHitBlock(BlockHitResult mop)
-	{
-		BlockState blockstate = this.level.getBlockState(mop.getBlockPos());
-		blockstate.onProjectileHit(this.level, blockstate, mop, this);
-	}
-
 	protected float getMotionDecayFactor()
 	{
 		return 0.99F;
@@ -391,12 +239,6 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 	public void addAdditionalSaveData(CompoundTag nbt)
 	{
 		super.addAdditionalSaveData(nbt);
-		if(inBlockState!=null)
-		{
-			nbt.put("inPos", NbtUtils.writeBlockPos(stuckIn));
-			nbt.put("inTile", NbtUtils.writeBlockState(inBlockState));
-		}
-		nbt.putByte("inGround", (byte)(this.inGround?1: 0));
 		if(this.shooterUUID!=null)
 			nbt.putUUID("Owner", this.shooterUUID);
 
@@ -406,17 +248,6 @@ public abstract class IEProjectileEntity extends AbstractArrow//Yes I have to ex
 	public void readAdditionalSaveData(CompoundTag nbt)
 	{
 		super.readAdditionalSaveData(nbt);
-		if(nbt.contains("inTile", Tag.TAG_COMPOUND))
-		{
-			inBlockState = NbtUtils.readBlockState(nbt.getCompound("inTile"));
-			stuckIn = NbtUtils.readBlockPos(nbt.getCompound("inPos"));
-		}
-		else
-		{
-			inBlockState = null;
-			stuckIn = null;
-		}
-		this.inGround = nbt.getByte("inGround")==1;
 		if(nbt.contains("Owner"))
 			this.shooterUUID = nbt.getUUID("Owner");
 		else
--- src/main/java/blusunrize/immersiveengineering/common/entities/SawbladeEntity.java
+++ src/main/java/blusunrize/immersiveengineering/common/entities/SawbladeEntity.java
@@ -15,16 +15,13 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 
 import javax.annotation.Nonnull;
 
@@ -99,36 +96,33 @@ public class SawbladeEntity extends IEProjectileEntity
 	}
 
 	@Override
-	public void onHit(HitResult mop)
+	protected void onHitEntity(EntityHitResult result)
 	{
-		if(!this.level.isClientSide&&!getAmmo().isEmpty())
-		{
-			if(mop instanceof EntityHitResult)
-			{
-				Entity hit = ((EntityHitResult)mop).getEntity();
-				Entity shooter = getOwner();
-				// todo: make this configurable?
-				hit.hurt(IEDamageSources.causeSawbladeDamage(this, shooter), 12.0f);
-			}
-			int dmg = Math.round(getAmmo().getMaxDamage()*.05f);
-			Entity shooter = getOwner();
-			if(getAmmo().hurt(dmg, level.random, shooter instanceof ServerPlayer?(ServerPlayer)shooter: null))
-				this.discard();
-			if(mop instanceof BlockHitResult)
-				this.onHitBlock((BlockHitResult)mop);
-		}
+		Entity shooter = getOwner();
+		Entity target = result.getEntity();
+		// todo: make this configurable?
+		target.hurt(IEDamageSources.causeSawbladeDamage(this, shooter), 12.0f);
+		this.handlePiecing(target);
 	}
 
 	@Override
 	protected void handlePiecing(Entity target)
 	{
 		super.handlePiecing(target);
-		if(this.piercedEntities.size() >= 3 && getShooterUUID() != null)
+		if(this.piercedEntities.size() >= 3&&getShooterUUID()!=null)
 		{
 			Player shooter = level.getPlayerByUUID(this.getShooterUUID());
 			if(shooter!=null)
 				Utils.unlockIEAdvancement(shooter, "tools/secret_ravenholm");
 		}
+	}
+
+	@Override
+	protected boolean canHitEntity(Entity target)
+	{
+		if(piercedEntities!=null&&piercedEntities.contains(target.getId()))
+			return false;
+		return !target.isSpectator()&&target.isAlive()&&target.isPickable();
 	}
 
 	@Override
