fc79b842ac1bceba655a15f1f30b4140d6b2423c
malte0811 <malte0811@web.de>
2023-08-13 17:15:15 +0200

    Fix 2 Level reference leaks

--- src/main/java/blusunrize/immersiveengineering/common/util/SpawnInterdictionHandler.java
+++ src/main/java/blusunrize/immersiveengineering/common/util/SpawnInterdictionHandler.java
@@ -22,6 +22,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.EntityTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -51,6 +52,17 @@ public class SpawnInterdictionHandler
 			event.setResult(Event.Result.DENY);
 	}
 
+	@SubscribeEvent
+	public static void onWorldUnload(LevelEvent.Unload event)
+	{
+		if(event.getLevel().isClientSide()||!(event.getLevel() instanceof Level realLevel))
+			return;
+		synchronized(interdictionTiles)
+		{
+			interdictionTiles.remove(realLevel.dimension());
+		}
+	}
+
 	private static boolean shouldCancel(Entity entity)
 	{
 		if(entity.getType().getCategory()!=MobCategory.MONSTER)
@@ -64,9 +76,8 @@ public class SpawnInterdictionHandler
 			while(it.hasNext())
 			{
 				ISpawnInterdiction interdictor = it.next();
-				if(interdictor instanceof BlockEntity)
+				if(interdictor instanceof BlockEntity interdictorTE)
 				{
-					BlockEntity interdictorTE = (BlockEntity)interdictor;
 					if(interdictorTE.isRemoved()||interdictorTE.getLevel()==null)
 						it.remove();
 					else if(SafeChunkUtils.isChunkSafe(interdictorTE.getLevel(), interdictorTE.getBlockPos()))
@@ -84,19 +95,21 @@ public class SpawnInterdictionHandler
 	public static <T extends BlockEntity & ISpawnInterdiction>
 	void removeFromInterdictionTiles(T tile)
 	{
-		synchronized(interdictionTiles)
-		{
-			Set<ISpawnInterdiction> inDimension = interdictionTiles.get(tile.getLevel().dimension());
-			if(inDimension!=null)
-				inDimension.remove(tile);
-		}
+		Level level = tile.getLevel();
+		if(level!=null&&!level.isClientSide)
+			synchronized(interdictionTiles)
+			{
+				Set<ISpawnInterdiction> inDimension = interdictionTiles.get(level.dimension());
+				if(inDimension!=null)
+					inDimension.remove(tile);
+			}
 	}
 
 	public static <T extends BlockEntity & ISpawnInterdiction>
 	void addInterdictionTile(T tile)
 	{
 		Level world = tile.getLevel();
-		if(world!=null&&IEServerConfig.MACHINES.floodlight_spawnPrevent.get())
+		if(world!=null&&!world.isClientSide()&&IEServerConfig.MACHINES.floodlight_spawnPrevent.get())
 			synchronized(interdictionTiles)
 			{
 				Set<ISpawnInterdiction> forDim = interdictionTiles.computeIfAbsent(
