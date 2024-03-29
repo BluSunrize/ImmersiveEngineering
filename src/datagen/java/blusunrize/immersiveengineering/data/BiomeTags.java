9ae8af9a9061bb1fe0d45f38fb2cdb6db90d205c
BluSunrize <danny.rad10@yahoo.de>
2024-03-29 18:13:05 +0100

    Add biome tags for specific IE veins

---
+++ src/datagen/java/blusunrize/immersiveengineering/data/BiomeTags.java
@@ -0,0 +1,60 @@
+/*
+ * BluSunrize
+ * Copyright (c) 2022
+ *
+ * This code is licensed under "Blu's License of Common Sense"
+ * Details can be found in the license file in the root folder of this project
+ *
+ */
+
+package blusunrize.immersiveengineering.data;
+
+import blusunrize.immersiveengineering.api.IETags;
+import blusunrize.immersiveengineering.api.Lib;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.BiomeTagsProvider;
+import net.minecraft.world.level.biome.Biomes;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+
+public class BiomeTags extends BiomeTagsProvider
+{
+
+	public BiomeTags(PackOutput output, CompletableFuture<Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper)
+	{
+		super(output, lookupProvider, Lib.MODID, existingFileHelper);
+	}
+
+	@Override
+	protected void addTags(Provider p_255894_)
+	{
+		// deserts, plains, mesas, savannas, ice plains
+		tag(IETags.generateClaypan)
+				.add(Biomes.DESERT)
+				.add(Biomes.PLAINS)
+				.add(Biomes.SUNFLOWER_PLAINS)
+				.add(Biomes.BADLANDS)
+				.add(Biomes.SAVANNA)
+				.add(Biomes.SNOWY_PLAINS);
+		// swamps, beaches, coral reefs, stony shore
+		tag(IETags.generateSeabed)
+				.add(Biomes.SWAMP)
+				.add(Biomes.MANGROVE_SWAMP)
+				.add(Biomes.BEACH)
+				.add(Biomes.SNOWY_BEACH)
+				.add(Biomes.WARM_OCEAN)
+				.add(Biomes.STONY_SHORE);
+	}
+
+
+	@Nonnull
+	@Override
+	public String getName()
+	{
+		return "IE biome tags";
+	}
+}
