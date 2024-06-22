d3d47779da331b2338bef731e0086254d003d9a1
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2024-06-22 00:23:09 -0400

    Update metal ladder behavior

--- src/main/java/blusunrize/immersiveengineering/common/blocks/metal/MetalLadderBlock.java
+++ src/main/java/blusunrize/immersiveengineering/common/blocks/metal/MetalLadderBlock.java
@@ -8,15 +8,28 @@
 
 package blusunrize.immersiveengineering.common.blocks.metal;
 
+import blusunrize.immersiveengineering.api.IEProperties;
+import blusunrize.immersiveengineering.api.IETags;
 import blusunrize.immersiveengineering.api.utils.DirectionUtils;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IHammerInteraction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -29,6 +42,7 @@ import java.util.Map;
 public class MetalLadderBlock extends LadderBlock
 {
 	private static final Map<Direction, VoxelShape> FRAMES = new EnumMap<>(Direction.class);
+	private static final Map<Direction, VoxelShape> FRAMES_OPEN = new EnumMap<>(Direction.class);
 
 	static
 	{
@@ -44,6 +58,21 @@ public class MetalLadderBlock extends LadderBlock
 			if(dir!=Direction.WEST)
 				forDir = merge(forDir, new AABB(.9375, 0, 0, 1, 1, 1));
 			FRAMES.put(dir, forDir);
+		}
+		for(Direction dir : DirectionUtils.BY_HORIZONTAL_INDEX)
+		{
+			VoxelShape forDir = Shapes.empty();
+			if(dir.getAxis()==Axis.Z)
+			{
+				forDir = merge(forDir, new AABB(0, 0, .9375, 1, 1, 1));
+				forDir = merge(forDir, new AABB(.9375, 0, 0, 1, 1, 1));
+			}
+			else
+			{
+				forDir = merge(forDir, new AABB(0, 0, 0, .0625, 1, 1));
+				forDir = merge(forDir, new AABB(0, 0, 0, 1, 1, .0625));
+			}
+			FRAMES_OPEN.put(dir, forDir);
 		}
 	}
 
@@ -69,7 +98,7 @@ public class MetalLadderBlock extends LadderBlock
 		else
 		{
 			Direction ladderSide = state.getValue(LadderBlock.FACING);
-			return Shapes.joinUnoptimized(base, FRAMES.get(ladderSide), BooleanOp.OR);
+			return state.getValue(IEProperties.ACTIVE) ? Shapes.joinUnoptimized(base, FRAMES_OPEN.get(ladderSide), BooleanOp.OR) : Shapes.joinUnoptimized(base, FRAMES.get(ladderSide), BooleanOp.OR);
 		}
 	}
 
@@ -78,7 +107,9 @@ public class MetalLadderBlock extends LadderBlock
 	public BlockState getStateForPlacement(BlockPlaceContext ctx)
 	{
 		BlockState baseState = super.getStateForPlacement(ctx);
-		if(type==CoverType.NONE||baseState==null)
+		if(baseState==null) return baseState;
+		baseState = baseState.setValue(IEProperties.ACTIVE, false);
+		if(type==CoverType.NONE)
 			return baseState;
 		else
 			return baseState.setValue(LadderBlock.FACING, Direction.fromYRot(ctx.getRotation()).getOpposite());
@@ -91,6 +122,29 @@ public class MetalLadderBlock extends LadderBlock
 			return super.canSurvive(state, world, pos);
 		else
 			return true;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
+		state.add(FACING, WATERLOGGED, IEProperties.ACTIVE);
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
+	{
+		ItemStack activeStack = player.getItemInHand(hand);
+		if(type!=CoverType.NONE&&activeStack.is(IETags.hammers))
+		{
+			System.out.println(state.getValue(IEProperties.ACTIVE));
+			boolean b = world.setBlockAndUpdate(pos, state.setValue(IEProperties.ACTIVE, !state.getValue(IEProperties.ACTIVE)));
+			if(b)
+				return InteractionResult.SUCCESS;
+			else
+				return InteractionResult.FAIL;
+		}
+		else
+			return super.use(state, world, pos, player, hand, hit);
 	}
 
 	public enum CoverType
