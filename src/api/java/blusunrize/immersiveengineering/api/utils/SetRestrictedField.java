0c6e88c929f1d7333a30045d947fc302184965e9
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2025-02-19 22:27:30 +0800

    Fix a rare issue caused by multithread mod loading

--- src/api/java/blusunrize/immersiveengineering/api/utils/SetRestrictedField.java
+++ src/api/java/blusunrize/immersiveengineering/api/utils/SetRestrictedField.java
@@ -89,7 +89,7 @@ public class SetRestrictedField<T>
 		private final List<Pair<Exception, SetRestrictedField<?>>> fields = new ArrayList<>();
 		private TrackerState state = TrackerState.OPEN;
 
-		<T> SetRestrictedField<T> make()
+		synchronized <T> SetRestrictedField<T> make()
 		{
 			Preconditions.checkState(state!=TrackerState.LOCKED);
 			SetRestrictedField<T> result = new SetRestrictedField<>(this);
